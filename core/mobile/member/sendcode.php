bda03e2ef4cd4ae51c1c9d83ddac2bb5c7b818d8
YMG <yangmingymg@163.com>
2016-03-07 17:28:45 +0800

    修改微信端无法发送信息bug

--- core/mobile/member/sendcode.php
+++ core/mobile/member/sendcode.php
@@ -48,7 +48,30 @@ if($op == 'sendcode'){
     $_SESSION['code_mobile'] = $mobile;
     $content = "您的安全码是：". $code ."。请不要把安全码泄露给其他人。如非本人操作，可不用理会！";
     $issendsms = $this->sendSms($mobile, $content);
-    show_json(1);  
+    show_json(1);
+}else if ($op == 'forgetcode'){
+    $mobile = $_GPC['mobile'];
+    if(empty($mobile)){
+        show_json(0, '请填入手机号');
+    }
+    $info = pdo_fetch('select * from ' . tablename('sz_yi_member') . ' where mobile=:mobile and pwd!="" and uniacid=:uniacid limit 1', array(
+                ':uniacid' => $_W['uniacid'],
+                ':mobile' => $mobile
+            ));
+    //print_r($info);
+    
+    $code = rand(1000, 9999);
+    $_SESSION['codetime'] = time();
+    $_SESSION['code'] = $code;
+    $_SESSION['code_mobile'] = $mobile;
+    $content = "您的安全码是：". $code ."。请不要把安全码泄露给其他人。如非本人操作，可不用理会！";
+    $issendsms = $this->sendSms($mobile, $content);
+    if(empty($info))
+    {
+        show_json(0);
+    }else{
+        show_json(1);
+    }
 }else if ($op == 'checkcode'){
     $code = $_GPC['code']; 
 
