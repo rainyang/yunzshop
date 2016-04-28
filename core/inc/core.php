3295a6d965b3be8551b15fed90a23ea1dcb81ec0
zhouguiyu <576216559@qq.com>
2016-04-28 23:38:50 +0800

    完善

--- core/inc/core.php
+++ core/inc/core.php
@@ -21,7 +21,14 @@ class Core extends WeModuleSite
     public function __construct()
     {
         global $_W, $_GPC;
+        
+        $set=m('common')->getSysset('shop');
 
+        if(strstr($_SERVER['REQUEST_URI'],'app')){
+            if($set['ispc']==0 && !isMobile()){
+                message('抱歉，PC版暂时关闭，请用微信打开!','','error');
+            }   
+        }
         if (is_weixin()) {
             m('member')->checkMember();
         }
