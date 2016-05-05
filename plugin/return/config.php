5bbf5162bc2c6aeee9579afe867d0bd4cba6055f
LuckyStar.D <18646546800@163.com>
2016-05-05 16:23:55 +0800

    添加全返插件

---
+++ plugin/return/config.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('IN_IA')) {
+    exit('Access Denied');
+}
+return array(
+    'version' => '1.0',
+    'id' => 'return',
+    'name' => '全返'
+);
---
+++ plugin/return/web.php
@@ -0,0 +1,39 @@
+<?php
+if (!defined('IN_IA')) {
+    exit('Access Denied');
+}
+class ReturnWeb extends Plugin
+{
+	protected $set = null;
+
+	public function __construct()
+	{
+		parent::__construct('return');
+		$this->set = $this->getSet();
+	}
+
+    public function index()
+    {
+        global $_W;
+
+        header('location: ' . $this->createPluginWebUrl('return/set'));
+
+    }
+
+	public function upgrade()
+    {
+        $this->_exec_plugin(__FUNCTION__);
+    }
+    public function set()
+    {
+        $this->_exec_plugin(__FUNCTION__);
+    }
+    public function return_tj()
+    {
+
+        $this->_exec_plugin(__FUNCTION__);
+    }
+  
+
+  
+}
