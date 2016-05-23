60f0e280810e82ae7bf2c16ccf640240af8b655b
rayyang <php_yanglei@163.com>
2016-05-23 16:25:04 +0800

    注册时使用推荐码 获得余额积分 发送通知

--- core/mobile/member/register.php
+++ core/mobile/member/register.php
@@ -5,10 +5,11 @@ if (!defined('IN_IA')) {
 global $_W, $_GPC;
 $preUrl = $_COOKIE['preUrl'];
 $operation = !empty($_GPC['op']) ? $_GPC['op'] : 'display';
-//echo "<pre>"; print_r($this->yzShopSet);exit;
+
 session_start();
 if ($_W['isajax']) {
     if ($_W['ispost']) {
+
         $mobile = !empty($_GPC['mobile']) ? $_GPC['mobile'] : show_json(0, '手机号不能为空！');
         $password = !empty($_GPC['password']) ? $_GPC['password'] : show_json(0, '密码不能为空！');
         $code = !empty($_GPC['code']) ? $_GPC['code'] : show_json(0, '验证码不能为空！');
@@ -29,11 +30,28 @@ if ($_W['isajax']) {
         if(!empty($member)){
             show_json(0, '该手机号已被注册！');
         }
+        //使用推荐码 是否开启
+        $isreferraltrue = false;
+        if($this->yzShopSet['isreferral'] == 1 && !empty($_GPC['referral']))
+        {
+            $referral = pdo_fetch('select * from ' . tablename('sz_yi_member') . ' where referralsn=:referralsn and uniacid=:uniacid limit 1', array(
+                        ':uniacid' => $_W['uniacid'],
+                        ':referralsn' => $_GPC['referral']
+                    ));
+            if(!$referral)
+            {
+                show_json(0, '推荐码无效！');
+            }else{
+                $isreferraltrue = true;
+            }
+        }
+        
         $openid = pdo_fetchcolumn('select openid from ' . tablename('sz_yi_member') . ' where mobile=:mobile and uniacid=:uniacid limit 1', array(
                         ':uniacid' => $_W['uniacid'],
                         ':mobile' => $mobile
                     ));
         if(empty($openid)){
+
             $member_data = array(
                 'uniacid' => $_W['uniacid'],
                 'uid' => 0,
@@ -44,6 +62,7 @@ if ($_W['isajax']) {
                 'status' => 0,
                 'regtype' => 2,
             );
+
             pdo_insert('sz_yi_member', $member_data);
             $openid = $member_data['openid'];
         }else{
@@ -53,6 +72,29 @@ if ($_W['isajax']) {
                 'isbindmobile' => 1
             );
             pdo_update('sz_yi_member', $member_data, array("mobile" => $mobile, "uniacid" => $_W['uniacid']));
+        }
+        
+        //使用推荐码 SH20160520172508468878
+        if($isreferraltrue)
+        {
+             $member = pdo_fetch('select * from ' . tablename('sz_yi_member') . ' where mobile=:mobile and pwd!="" and uniacid=:uniacid limit 1', array(
+                        ':uniacid' => $_W['uniacid'],
+                        ':mobile' => $mobile
+                    )); 
+                   
+            if(!$member['agentid'])
+            { 
+                $m_data = array(
+                    'agentid' => $referral['id'],
+                    'agenttime' => time(),
+                    'status' => 1,
+                    'isagent' => 1
+                );
+
+                pdo_update('sz_yi_member', $m_data, array("mobile" => $mobile, "uniacid" => $_W['uniacid']));
+                m('member')->responseReferral($this->yzShopSet,$referral,$member);
+            }
+            
         }
 
         $lifeTime = 24 * 3600 * 3;
