93b7db6fa29df0b440f17e638c9505675de86761
wujingyu <827127436@qq.com>
2016-05-25 10:21:55 +0800

    新数据库

--- core/web/order/newsql.php
+++ core/web/order/newsql.php
@@ -44,6 +44,29 @@ CREATE TABLE IF NOT EXISTS ".tablename('sz_yi_exhelper_senduser')." (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 
+CREATE TABLE IF NOT EXISTS ".tablename('sz_yi_category2')." (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uniacid` int(11) DEFAULT '0' COMMENT '所属帐号',
+  `name` varchar(50) DEFAULT NULL COMMENT '分类名称',
+  `thumb` varchar(255) DEFAULT NULL COMMENT '分类图片',
+  `parentid` int(11) DEFAULT '0' COMMENT '上级分类ID,0为第一级',
+  `isrecommand` int(10) DEFAULT '0',
+  `description` varchar(500) DEFAULT NULL COMMENT '分类介绍',
+  `displayorder` tinyint(3) unsigned DEFAULT '0' COMMENT '排序',
+  `enabled` tinyint(1) DEFAULT '1' COMMENT '是否开启',
+  `ishome` tinyint(3) DEFAULT '0',
+  `advimg` varchar(255) DEFAULT '',
+  `advurl` varchar(500) DEFAULT '',
+  `level` tinyint(3) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_uniacid` (`uniacid`),
+  KEY `idx_displayorder` (`displayorder`),
+  KEY `idx_enabled` (`enabled`),
+  KEY `idx_parentid` (`parentid`),
+  KEY `idx_isrecommand` (`isrecommand`),
+  KEY `idx_ishome` (`ishome`)
+) ENGINE=MyISAM AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS ".tablename('sz_yi_exhelper_sys')." (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uniacid` int(11) NOT NULL DEFAULT '0',
