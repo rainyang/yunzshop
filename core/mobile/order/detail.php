d79ada250a8f1617e3ebd9eb9313fa6c6d119467
鲸鱼不会飞 <827127436@qq.com>
2016-06-17 23:29:20 +0800

    修复会员中心订单列表和订单详情都没有商家logo的问题

--- core/mobile/order/detail.php
+++ core/mobile/order/detail.php
@@ -14,8 +14,8 @@ $order          = pdo_fetch('select * from ' . tablename('sz_yi_order') . ' wher
     ':uniacid' => $uniacid,
     ':openid' => $openid
 ));
-if(p('cashier')){
-    $order['name'] = set_medias(pdo_fetch('select cs.name,cs.thumb,cs.contact,cs.mobile,cs.address from ' .tablename('sz_yi_cashier_store'). 'cs '.'left join ' .tablename('sz_yi_cashier_order'). ' co on cs.id = co.cashier_store_id where co.order_id=:orderid and co.uniacid=:uniacid', array(':orderid' => $orderid,':uniacid'=>$_W['uniacid'])), 'thumb');
+if(p('cashier') && $order['cashier'] == 1){
+    $order['name'] = set_medias(pdo_fetch('select * from ' .tablename('sz_yi_cashier_store'). ' where id=:id and uniacid=:uniacid', array(':id' => $order['cashierid'],':uniacid'=>$_W['uniacid'])), 'thumb');
 }
 
 if (!empty($order)) {
@@ -168,7 +168,4 @@ if ($_W['isajax']) {
         'set' => $set
     ));
 }
-include $this->template('order/detail');   
-
-
-
+include $this->template('order/detail');
