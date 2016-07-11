0e18b6a928dbc29668dd51f2954ae39b5f38a896
Y.yang <yang_0458@126.com>
2016-07-11 16:44:08 +0800

    【渠道商】申请渠道商支持自定义表单完成

--- plugin/channel/core/web/upgrade.php
+++ plugin/channel/core/web/upgrade.php
@@ -11,4 +11,7 @@ if(empty($result)){
     $sql = "INSERT INTO " . tablename('sz_yi_plugin') . " (`displayorder`,`identity`,`name`,`version`,`author`,`status`,`category`) VALUES(". $displayorder .",'channel','渠道商','1.0','官方','1','biz');";
   pdo_fetchall($sql);
 }
+if(!pdo_fieldexists('sz_yi_af_channel', 'status')) {
+  pdo_query("ALTER TABLE ".tablename('sz_yi_af_channel')." ADD `status` tinyint(2) NOT NULL COMMENT '0为申请1为通过';");
+}
 message('渠道商插件安装成功', $this->createPluginWebUrl('channel/index'), 'success');
