ce491d0a34e67d353ad8c6041dea44b8c118ae4d
杨雷 <heiangdao@sina.com>
2016-07-15 14:02:38 +0800

    全返插件 增加按周返现

--- plugin/return/web.php
+++ plugin/return/web.php
@@ -28,6 +28,10 @@ class ReturnWeb extends Plugin
     {
         $this->_exec_plugin(__FUNCTION__);
     }
+    public function level()
+    {
+        $this->_exec_plugin(__FUNCTION__);
+    }
     public function return_tj()
     {
 
