90c7ff04f7e90df5d6d2fbfde5b3900e8707d67e
杨雷 <heiangdao@sina.com>
2016-07-16 15:53:59 +0800

    修改发送消息通知处理 大括号

--- core/model/message.php
+++ core/model/message.php
@@ -25,39 +25,39 @@ class Sz_DYi_Message
         return $account->sendTplNotice($touser, $template_id, $postdata, $url);
     }
     public function sendCustomNotice($openid, $msg, $url = '', $account = null)
-    { {
-            if (!$account) {
-                $account = m('common')->getAccount();
-            }
-            if (!$account) {
-                return;
-            }
-            $content = "";
-            if (is_array($msg)) {
-                foreach ($msg as $key => $value) {
-                    if (!empty($value['title'])) {
-                        $content .= $value['title'] . ":" . $value['value'] . "\n";
-                    } else {
-                        $content .= $value['value'] . "\n";
-                        if ($key == 0) {
-                            $content .= "\n";
-                        }
+    { 
+        if (!$account) {
+            $account = m('common')->getAccount();
+        }
+        if (!$account) {
+            return;
+        }
+        $content = "";
+        if (is_array($msg)) {
+            foreach ($msg as $key => $value) {
+                if (!empty($value['title'])) {
+                    $content .= $value['title'] . ":" . $value['value'] . "\n";
+                } else {
+                    $content .= $value['value'] . "\n";
+                    if ($key == 0) {
+                        $content .= "\n";
                     }
                 }
-            } else {
-                $content = $msg;
             }
-            if (!empty($url)) {
-                $content .= "<a href='{$url}'>点击查看详情</a>";
-            }
-            return $account->sendCustomNotice(array(
-                "touser" => $openid,
-                "msgtype" => "text",
-                "text" => array(
-                    'content' => urlencode($content)
-                )
-            ));
+        } else {
+            $content = $msg;
         }
+        if (!empty($url)) {
+            $content .= "<a href='{$url}'>点击查看详情</a>";
+        }
+        return $account->sendCustomNotice(array(
+            "touser" => $openid,
+            "msgtype" => "text",
+            "text" => array(
+                'content' => urlencode($content)
+            )
+        ));
+        
     }
     public function sendImage($openid, $mediaid)
     {
