8c8787b8d7bfdea5958ea3a65d10cdd7ac8a5473
dingran80s <dingran@zongji.in>
2016-07-20 19:26:51 +0800

    [增加] app首页接口文件

--- core/mobile/api/index.php
+++ core/mobile/api/index.php
@@ -10,6 +10,7 @@ global $_W, $_GPC;
 
 $uniacid   = $_W['uniacid'];
 
+//轮播图
 $advs = pdo_fetchall('select id,advname,link,thumb,thumb_pc from ' . tablename('sz_yi_adv') . ' where uniacid=:uniacid and enabled=1 order by displayorder desc', array(':uniacid' => $uniacid));
 foreach($advs as $key => $adv){
     if(!empty($advs[$key]['thumb'])){
@@ -21,11 +22,28 @@ foreach($advs as $key => $adv){
 }
 $advs = set_medias($advs, 'thumb,thumb_pc');
 
+//推荐分类
+$category = pdo_fetchall('select id,name,thumb,parentid,level from ' . tablename('sz_yi_category') . ' where uniacid=:uniacid and ishome=1 and enabled=1 order by displayorder desc', array(':uniacid' => $uniacid));
+$category = set_medias($category, 'thumb');
+
+foreach ($category as &$c) {
+    $c['thumb'] = tomedia($c['thumb']);
+    if ($c['level'] == 3) {
+        $c['url'] = $this->createMobileUrl('shop/list', array('tcate' => $c['id']));
+    } else if ($c['level'] == 2) {
+        $c['url'] = $this->createMobileUrl('shop/list', array('ccate' => $c['id']));
+    }
+}
+
+//推荐宝贝
+$args = array('page' => $_GPC['page'], 'pagesize' => 6, 'isrecommand' => 1, 'order' => 'displayorder desc,createtime desc', 'by' => '');
+$goods = m('goods')->getList($args);
+
 //echo '<pre>';print_r($advs);exit;
 $app_interface = new InterfaceController();
 $res = array(
     'advs' => $advs,
-    'category' => array(),
-    'goods' => array()
+    'category' => $category,
+    'goods' => $goods
 );
 $app_interface->checkResultAndReturn($res);
