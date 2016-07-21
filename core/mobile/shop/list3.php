13e06436348f15c2a9e07c3de48475bcf6752ffa
dingran80s <dingran@zongji.in>
2016-07-21 18:09:48 +0800

    [增加] app原生增加推荐宝贝全部显示页面

--- core/mobile/shop/list3.php
+++ core/mobile/shop/list3.php
@@ -6,4 +6,22 @@
  * Time: 下午4:37
  */
 
-echo 3;exit;
+global $_W, $_GPC;
+
+$operation = !empty($_GPC['op']) ? $_GPC['op'] : 'index';
+$openid    = m('user')->getOpenid();
+$uniacid   = $_W['uniacid'];
+$shopset   = m('common')->getSysset('shop');
+
+if ($_W['isajax']) {
+    if ($operation == 'index') {
+        $type = $_GPC['type'];
+        $args = array('page' => $_GPC['page'], 'pagesize' => 6, 'isrecommand' => 1, 'order' => 'displayorder desc,createtime desc', 'by' => '');
+        $goods = m('goods')->getList($args);
+
+        show_json(1, array('goods' => $goods, 'pagesize' => $args['pagesize']));
+    }
+}
+
+$this->setHeader();
+include $this->template('shop/list3');
