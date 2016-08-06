3ba5bfc3abf8a2a8c06a5e93fc797df5a43523f6
YMG <yangmingymg@163.com>
2016-08-06 19:46:22 +0800

    【修改】合利宝添加消费为完成天数计算，并不计算收银台订单

--- plugin/commission/model.php
+++ plugin/commission/model.php
@@ -179,6 +179,7 @@ if (!class_exists('CommissionModel')) {
 			$_var_23 = $this->getLevel($openid);
 			$_var_24 = time();
 			$_var_25 = intval($set['settledays']) * 3600 * 24;
+			$withdraw_day = intval($set['withdraw_day']) * 3600 * 24;
 			$_var_26 = 0;
 			$_var_27 = 0;
 			$_var_28 = 0;
@@ -504,7 +505,11 @@ if (!class_exists('CommissionModel')) {
 			}
 			//Author:ym Date:2016-04-07 Content:自购完成订单
 			if (in_array('myorder', $_var_21)) {
-				$myorder = pdo_fetch('select sum(og.realprice) as ordermoney,count(distinct og.orderid) as ordercount from ' . tablename('sz_yi_order') . ' o ' . ' left join  ' . tablename('sz_yi_order_goods') . ' og on og.orderid=o.id ' . ' where o.openid=:openid and o.status>=3 and o.status<>6 and o.status<>4 and o.status<>5 and o.uniacid=:uniacid limit 1', array(':uniacid' => $_W['uniacid'], ':openid' => $member['openid']));
+				$myorder_where = "";
+				if(p('love')){
+					$myorder_where .= " and ({$_var_24} - o.finishtime <= {$withdraw_day}) and cashier=0";
+				}
+				$myorder = pdo_fetch('select sum(og.realprice) as ordermoney,count(distinct og.orderid) as ordercount from ' . tablename('sz_yi_order') . ' o ' . ' left join  ' . tablename('sz_yi_order_goods') . ' og on og.orderid=o.id ' . ' where o.openid=:openid and o.status>=3 and o.status<>6 and o.status<>4 and o.status<>5 and o.uniacid=:uniacid', array(':uniacid' => $_W['uniacid'], ':openid' => $member['openid']));
 				//Author:ym Date:2016-04-07 Content:自购订单金额
 				$myordermoney = $myorder['ordermoney'];
 				//Author:ym Date:2016-04-07 Content:自购订单数量
