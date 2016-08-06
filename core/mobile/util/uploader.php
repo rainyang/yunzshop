a2ede0314a958210356b0e7cb561894cfdd27676
rainyang <RainYang2012@qq.com>
2016-08-06 21:05:37 +0800

    修改部分代码.变量等

--- core/mobile/util/uploader.php
+++ core/mobile/util/uploader.php
@@ -4,22 +4,22 @@ if (!defined('IN_IA')) {
 }
 global $_W, $_GPC;
 load()->func('file');
-$operation = !empty($_GPC['op']) ? $_GPC['op'] : 'upload';
-if ($operation == 'upload') {
+$op = !empty($_GPC['op']) ? $_GPC['op'] : 'upload';
+if ($op == 'upload') {
 	$field = $_GPC['file'];
 	if (!empty($_FILES[$field]['name'])) {
 		if ($_FILES[$field]['error'] != 0) {
-			$result['message'] = '上传失败，请重试！';
+			$result['message'] = '图片上传失败，请重试！';
 			exit(json_encode($result));
 		}
 		$path = '/images/sz_yi/' . $_W['uniacid'];
 		if (!is_dir(ATTACHMENT_ROOT . $path)) {
 			mkdirs(ATTACHMENT_ROOT . $path);
 		}
-		$_W['uploadsetting'] = array();
-		$_W['uploadsetting']['image']['folder'] = $path;
-		$_W['uploadsetting']['image']['extentions'] = $_W['config']['upload']['image']['extentions'];
-		$_W['uploadsetting']['image']['limit'] = $_W['config']['upload']['image']['limit'];
+		$_W['uploadset'] = array();
+		$_W['uploadset']['image']['folder'] = $path;
+		$_W['uploadset']['image']['extentions'] = $_W['config']['upload']['image']['extentions'];
+		$_W['uploadset']['image']['limit'] = $_W['config']['upload']['image']['limit'];
 		$file = file_upload($_FILES[$field], 'image');
 		if (is_error($file)) {
 			$result['message'] = $file['message'];
@@ -40,10 +40,10 @@ if ($operation == 'upload') {
 		pdo_insert('core_attachment', array('uniacid' => $_W['uniacid'], 'uid' => $_W['member']['uid'], 'filename' => $_FILES[$field]['name'], 'attachment' => $result['filename'], 'type' => 1, 'createtime' => TIMESTAMP,));
 		exit(json_encode($result));
 	} else {
-		$result['message'] = '请选择要上传的图片！';
+		$result['message'] = '请选择要上传的图片';
 		exit(json_encode($result));
 	}
-} elseif ($operation == 'remove') {
+} elseif ($op == 'remove') {
     $file = $_GPC['file'];
     file_delete($file);
     show_json(1);
