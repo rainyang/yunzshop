10fcd48fb732b00b26af4a3e28f2106b915db358
杨雷 <heiangdao@sina.com>
2016-08-14 22:19:51 +0800

    余额转让记录增表 加转让人当前余额 受让人当前余额

--- core/mobile/member/transfer.php
+++ core/mobile/member/transfer.php
@@ -27,55 +27,77 @@ if($operation == 'assigns'){
     }
 }elseif($operation == 'submit'){
     if ($_W['isajax']) {
-        $money = floatval($_GPC['money']);
-        if ($money <= 0 || $member['credit2'] < $money){
-            show_json(0,'转让金额不正确');
+        //创建文件锁
+        $tmpdir = IA_ROOT . "/addons/sz_yi/tmp/member";
+        $file   = $tmpdir."/filelock.txt";
+        if (!is_dir($tmpdir)) {
+            mkdirs($tmpdir);
         }
-        $assigns_id = intval($_GPC['assigns']);
-        $assigns = pdo_fetch("select * from " . tablename('sz_yi_member') . " where uniacid=:uniacid and id = :assigns_id ", array(
-            ':uniacid'=> $uniacid,
-            ':assigns_id'=>$assigns_id
-        ));
-        if ($assigns) {
-            $mc_assigns = m('member')->getMember($assigns['openid']);
-            m('member')->setCredit($member['openid'],'credit2',-$money);
-            $messages = array(
-                'keyword1' => array(
-                    'value' => '转增通知', 
-                    'color' => '#73a68d'
-                ),
-                'keyword2' => array(
-                    'value' => '你向'.$assigns['nickname'].'转增金额'.$money."元！",
-                    'color' => '#73a68d'
-                )
-            );
-            m('message')->sendCustomNotice($member['openid'], $messages);
-            m('member')->setCredit($assigns['openid'],'credit2',$money, array(0, '会员余额转让所得：' . $money . " 元"));
-            $messages = array(
-                'keyword1' => array(
-                    'value' => '转增通知', 
-                    'color' => '#73a68d'
-                ),
-                'keyword2' => array(
-                    'value' => $member['nickname'].'向你转增金额'.$money."元！",
-                    'color' => '#73a68d'
-                )
-            );
-            m('message')->sendCustomNotice($assigns['openid'], $messages);
-            $member_data = array(
-                'uniacid'       => $_W['uniacid'],
-                'openid'        => $openid,
-                'tosell_id'     => $member['id'],
-                'assigns_id'     => $assigns_id,
-                'createtime'    => time(),
-                'status'        => 1,
-                'money'         => $money
-            );
-            pdo_insert('sz_yi_member_transfer_log', $member_data); 
-            show_json(1);
-        } else {
-            show_json(0,'受让人不存在！');
+        if (!file_exists($file)) {
+            touch($file);
+            $money = floatval($_GPC['money']);
+            if ($money <= 0 || $member['credit2'] < $money){
+                show_json(0,'转让金额不正确');
+            }
+            $assigns_id = intval($_GPC['assigns']);
+            $assigns = pdo_fetch("select * from " . tablename('sz_yi_member') . " where uniacid=:uniacid and id = :assigns_id ", array(
+                ':uniacid'=> $uniacid,
+                ':assigns_id'=>$assigns_id
+            ));
+            if ($assigns) {
+                $mc_assigns = m('member')->getMember($assigns['openid']);
+                m('member')->setCredit($member['openid'],'credit2',-$money);
+                $messages = array(
+                    'keyword1' => array(
+                        'value' => '转增通知', 
+                        'color' => '#73a68d'
+                    ),
+                    'keyword2' => array(
+                        'value' => '你向'.$assigns['nickname'].'转增金额'.$money."元！",
+                        'color' => '#73a68d'
+                    )
+                );
+                m('message')->sendCustomNotice($member['openid'], $messages);
+                m('member')->setCredit($assigns['openid'],'credit2',$money, array(0, '会员余额转让所得：' . $money . " 元"));
+                $messages = array(
+                    'keyword1' => array(
+                        'value' => '转增通知', 
+                        'color' => '#73a68d'
+                    ),
+                    'keyword2' => array(
+                        'value' => $member['nickname'].'向你转增金额'.$money."元！",
+                        'color' => '#73a68d'
+                    )
+                );
+                m('message')->sendCustomNotice($assigns['openid'], $messages);
+                $member_data = array(
+                    'uniacid'       => $_W['uniacid'],
+                    'openid'        => $openid,
+                    'tosell_id'     => $member['id'],
+                    'assigns_id'     => $assigns_id,
+                    'createtime'    => time(),
+                    'status'        => 1,
+                    'money'         => $money,
+                    'tosell_current_credit'     => $member['credit2'] - $money,
+                    'assigns_current_credit'    => $assigns['credit2'] + $money,
+                );
+                pdo_insert('sz_yi_member_transfer_log', $member_data); 
+                @unlink ($file); 
+                show_json(1);
+            } else {
+                @unlink ($file); 
+                show_json(0,'受让人不存在！');
+            }
+            
         }
     }
+}
+
+function mkdirs($path) {
+    if (!is_dir($path)) {
+        mkdirs(dirname($path));
+        mkdir($path);
+    }
+    return is_dir($path);
 }
 include $this->template('member/transfer');
