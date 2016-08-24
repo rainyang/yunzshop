df38dea2fb0093947417cbb1a0cdec1272fe6758
Y.yang <yang_0458@126.com>
2016-08-24 17:55:16 +0800

    【帮助中心】

---
+++ plugin/helper/model.php
@@ -0,0 +1,10 @@
+ <?php
+if (!defined('IN_IA')) {
+    exit('Access Denied');
+}
+if (!class_exists('HelperModel')) {
+    class HelperModel extends PluginModel
+    {
+        
+    }    
+}        
