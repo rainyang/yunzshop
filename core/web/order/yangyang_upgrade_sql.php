cc1211cb150d7eb57deb567872f0835f5e45b39b
Y.yang <yang_0458@126.com>
2016-08-25 10:02:40 +0800

    【帮助中心sql】

---
+++ core/web/order/yangyang_upgrade_sql.php
@@ -0,0 +1,8 @@
+<?php
+if(!pdo_fieldexists('sz_yi_article', 'is_helper')) {
+  pdo_fetchall("ALTER TABLE ".tablename('sz_yi_article')." ADD `is_helper` int(11) NOT NULL DEFAULT '0';");
+}
+
+if(!pdo_fieldexists('sz_yi_article_category', 'is_helper')) {
+  pdo_fetchall("ALTER TABLE ".tablename('sz_yi_article_category')." ADD `is_helper` int(11) NOT NULL DEFAULT '0';");
+}
