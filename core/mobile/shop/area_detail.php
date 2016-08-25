a4318d71772ea96d24fd51a3a4b87d7d6476247a
吴敬宇 <827127436@qq.com>
2016-08-25 18:03:25 +0800

    样式调试完成

--- core/mobile/shop/area_detail.php
+++ core/mobile/shop/area_detail.php
@@ -17,8 +17,7 @@ if ($operation == 'display') {
             and uniacid=:uniacid order by displayorder DESC', array(
             ':id' => intval($_GPC['tcate_area']), ':uniacid' => $_W['uniacid']
         )),'thumb');
-        $times = $category['times'] + 1;
-        pdo_update('sz_yi_category_area', array('times' => $times), array('id' => $category['id'], 'uniacid' => $_W['uniacid']));
+        
        
     } else if (!empty($_GPC['ccate_area'])) {
         $category = set_medias(pdo_fetch('select * from ' . tablename('sz_yi_category_area') . ' where id=:id 
@@ -26,11 +25,27 @@ if ($operation == 'display') {
             ':id' => intval($_GPC['ccate_area']),
             ':uniacid' => $_W['uniacid']
         )),'thumb');
-        $times = $category['times'] + 1;
-        pdo_update('sz_yi_category_area', array('times' => $times), array('id' => $category['id'], 'uniacid' => $_W['uniacid']));
-
-
+        
     }
-    
+
+    $times = $category['times'] + 1;
+    pdo_update('sz_yi_category_area', array('times' => $times), array('id' => $category['id'], 'uniacid' => $_W['uniacid']));
+    $html = $category['detail'];
+    preg_match_all("/<img.*?src=[\'| \"](.*?(?:[\.gif|\.jpg]?))[\'|\"].*?[\/]?>/", $html, $imgs);
+    if (isset($imgs[1])) {
+        foreach ($imgs[1] as $img) {
+            $im       = array(
+                "old" => $img,
+                "new" => tomedia($img)
+            );
+            $images[] = $im;
+        }
+        if (isset($images)) {
+            foreach ($images as $img) {
+                $html = str_replace($img['old'], $img['new'], $html);
+            }
+        }
+        $category['detail'] = $html;
+    }
 }    
 include $this->template('shop/area_detail');
