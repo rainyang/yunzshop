25f04d2d27c5eb9667f237c4f483a7b08f37ed49
rainyang <RainYang2012@qq.com>
2016-08-29 00:09:28 +0800

    修改函数重复问题

--- plugin/yunbi/core/mobile/task.php
+++ plugin/yunbi/core/mobile/task.php
@@ -129,12 +129,16 @@ if (!file_exists($file)) {
     @unlink ($file);
     echo "返现任务执行完成!";
 }
-function mkdirs($path) {
-    if (!is_dir($path)) {
-        mkdirs(dirname($path));
-        mkdir($path);
+
+
+if (!function_exists('mkdirs')) {
+    function mkdirs($path) {
+        if (!is_dir($path)) {
+            mkdirs(dirname($path));
+            mkdir($path);
+        }
+        return is_dir($path);
     }
-    return is_dir($path);
 }
 
 
