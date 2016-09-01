2c2dcb8b02ff3a48729cacf31554bbb90f05b358
吴敬宇 <827127436@qq.com>
2016-09-01 20:56:49 +0800

    修改默认值

--- core/web/order/wujingyu_upgrade_sql.php
+++ core/web/order/wujingyu_upgrade_sql.php
@@ -5,7 +5,7 @@ if(!pdo_fieldexists('sz_yi_cashier_store', 'centercan')) {
 }
 //优惠券指定供应商新加字段
 if(!pdo_fieldexists('sz_yi_coupon', 'getsupplier')) {
-    pdo_fetchall("ALTER TABLE ".tablename('sz_yi_coupon')." ADD    `getsupplier` tinyint(1) DEFAULT '1';");
+    pdo_fetchall("ALTER TABLE ".tablename('sz_yi_coupon')." ADD    `getsupplier` tinyint(1) DEFAULT '0';");
 }
 if(!pdo_fieldexists('sz_yi_coupon', 'supplierids')) {
     pdo_fetchall("ALTER TABLE ".tablename('sz_yi_coupon')." ADD    `supplierids` text(0) DEFAULT '1';");
