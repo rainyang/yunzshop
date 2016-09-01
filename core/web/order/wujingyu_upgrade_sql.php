311f239c79806db6e1e0145f369478b5ba521563
吴敬宇 <827127436@qq.com>
2016-09-01 20:57:22 +0800

    修改默认值

--- core/web/order/wujingyu_upgrade_sql.php
+++ core/web/order/wujingyu_upgrade_sql.php
@@ -8,8 +8,8 @@ if(!pdo_fieldexists('sz_yi_coupon', 'getsupplier')) {
     pdo_fetchall("ALTER TABLE ".tablename('sz_yi_coupon')." ADD    `getsupplier` tinyint(1) DEFAULT '0';");
 }
 if(!pdo_fieldexists('sz_yi_coupon', 'supplierids')) {
-    pdo_fetchall("ALTER TABLE ".tablename('sz_yi_coupon')." ADD    `supplierids` text(0) DEFAULT '1';");
+    pdo_fetchall("ALTER TABLE ".tablename('sz_yi_coupon')." ADD    `supplierids` text(0) DEFAULT '';");
 }
 if(!pdo_fieldexists('sz_yi_coupon', 'suppliernames')) {
-    pdo_fetchall("ALTER TABLE ".tablename('sz_yi_coupon')." ADD    `suppliernames` text(0) DEFAULT '1';");
+    pdo_fetchall("ALTER TABLE ".tablename('sz_yi_coupon')." ADD    `suppliernames` text(0) DEFAULT '';");
 }
