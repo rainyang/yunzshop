5980241672e4771949f8aeafa428dbf4fe18b8de
dingran80s <dingran@zongji.in>
2016-09-05 14:37:49 +0800

    [修复] 后台excel表格导出数据不完整问题

--- core/model/excel.php
+++ core/model/excel.php
@@ -151,12 +151,19 @@ class Sz_DYi_Excel
             $len = count($params['columns']);
             for ($i = 0; $i < $len; $i++) {
                 $value = $row[$params['columns'][$i]['field']];
-                if ($params['columns'][$i]['field'] == 'nickname') {
+                $value = @iconv("utf-8", "gbk", $value);
+                $value = @iconv("gbk", "utf-8", $value);
+               /* if ($params['columns'][$i]['field'] == 'nickname') {
                     $value = @iconv("utf-8", "gbk", $value);
                     $value = @iconv("gbk", "utf-8", $value);
                 }
 
                 if ($params['columns'][$i]['field'] == 'realname') {
+                    $value = @iconv("utf-8", "gbk", $value);
+                    $value = @iconv("gbk", "utf-8", $value);
+                }
+
+                if ($params['columns'][$i]['field'] == 'mrealname') {
                     $value = @iconv("utf-8", "gbk", $value);
                     $value = @iconv("gbk", "utf-8", $value);
                 }
@@ -174,7 +181,7 @@ class Sz_DYi_Excel
                 if ($params['columns'][$i]['field'] == 'salerinfo') {
                     $value = @iconv("utf-8", "gbk", $value);
                     $value = @iconv("gbk", "utf-8", $value);
-                }
+                }*/
 
 
                 $sheet->setCellValueExplicit($this->column($i, $rownum), $value, PHPExcel_Cell_DataType::TYPE_STRING);
