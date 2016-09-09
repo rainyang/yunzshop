4c506ca6b1054f24e9bbc0f3f99a91dead23f557
YMG <yangmingymg@163.com>
2016-09-09 11:04:46 +0800

    【添加】bonus表中缺少字段

--- core/web/order/yangming_upgrade_sql.php
+++ core/web/order/yangming_upgrade_sql.php
@@ -2,3 +2,7 @@
 if(!pdo_fieldexists('sz_yi_bonus_log', 'type')) {
   pdo_fetchall("ALTER TABLE ".tablename('sz_yi_bonus_log')." ADD `type` tinyint(1) DEFAULT '0';");
 }
+if(!pdo_fieldexists('sz_yi_bonus', 'bonus_area')) {
+  pdo_fetchall("ALTER TABLE ".tablename('sz_yi_bonus')." ADD `bonus_area` tinyint(1) DEFAULT '0';");
+}
+echo 1;
