b72d508e63b4f255c5d683089f71093f682351c1
YMG <yangmingymg@163.com>
2016-09-13 16:43:27 +0800

    【添加】充值记录表中添加字段，该条记录正在执行，不允许重复执行。

--- core/web/order/yangming_upgrade_sql.php
+++ core/web/order/yangming_upgrade_sql.php
@@ -5,4 +5,9 @@ if(!pdo_fieldexists('sz_yi_bonus_log', 'type')) {
 if(!pdo_fieldexists('sz_yi_bonus', 'bonus_area')) {
   pdo_fetchall("ALTER TABLE ".tablename('sz_yi_bonus')." ADD `bonus_area` tinyint(1) DEFAULT '0';");
 }
+//9.13添加
+//充值记录表中添加进行充值中的记录
+if (!pdo_fieldexists('sz_yi_member_log', 'underway')) {
+    pdo_fetchall("ALTER TABLE ".tablename('sz_yi_member_log')." ADD `underway` tinyint(1) DEFAULT '0';");
+}
 echo 1;
