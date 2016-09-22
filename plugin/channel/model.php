1cffbc4a280ba6b4de3a8f2344180ba910fe92d4
杨洋 <yang_0458@126.com>
2016-09-22 12:07:42 +0800

    [渠道商]循环升级bug，渠道商金额计算bug

--- plugin/channel/model.php
+++ plugin/channel/model.php
@@ -115,7 +115,7 @@ if (!class_exists('ChannelModel')) {
 					$lower_openids[] = "'".$value['lower_openid']."'";
 				}
 				$lower_openids = implode(',', $lower_openids);
-				$lower_order_money = pdo_fetchcolumn("SELECT sum(og.price) FROM " . tablename('sz_yi_order') . " o LEFT JOIN " . tablename('sz_yi_order_goods') . " og on og.orderid=o.id WHERE o.uniacid=:uniacid AND o.status>=3 AND o.iscmas=0 AND og.ischannelpay=1 AND o.openid in ({$lower_openids})", array(':uniacid' => $_W['uniacid']));
+				$lower_order_money = pdo_fetchcolumn("SELECT sum(o.goodsprice) FROM " . tablename('sz_yi_order') . " o LEFT JOIN " . tablename('sz_yi_order_goods') . " og on og.orderid=o.id WHERE o.uniacid=:uniacid AND o.status>=3 AND o.iscmas=0 AND og.ischannelpay=1 AND o.openid in ({$lower_openids})", array(':uniacid' => $_W['uniacid']));
 				$lower_order_money = number_format($lower_order_money*$set['setprofitproportion']/100,2);
 			} else {
 				$lower_openids = 0;
@@ -135,7 +135,7 @@ if (!class_exists('ChannelModel')) {
             //零售金额
             $retail_commission = pdo_fetchcolumn("SELECT ifnull(sum(ogp.profit),0) FROM " . tablename('sz_yi_channel_order_goods_profit') . " ogp LEFT JOIN " . tablename('sz_yi_order_goods') . " og ON og.id=ogp.order_goods_id LEFT JOIN " .tablename('sz_yi_order') . " o ON (o.id=og.orderid) WHERE o.uniacid=:uniacid AND og.channel_id=:channel_id AND o.status=3 AND og.channel_apply_status=0 AND og.ischannelpay=0 ", array(':uniacid' => $_W['uniacid'], ':channel_id' => $member['id']));
             //下级采购金额
-            $purchase_commission = pdo_fetchcolumn("SELECT ifnull(sum(og.price),0) FROM " . tablename('sz_yi_order_goods') . " og left join " .tablename('sz_yi_order') . " o on (o.id=og.orderid) WHERE o.uniacid=:uniacid AND og.channel_id=:channel_id AND o.status=3 AND og.channel_apply_status=0 AND og.ischannelpay=1 ", array(':uniacid' => $_W['uniacid'], ':channel_id' => $member['id']));
+            $purchase_commission = pdo_fetchcolumn("SELECT sum(o.goodsprice) FROM " . tablename('sz_yi_order_goods') . " og left join " .tablename('sz_yi_order') . " o on (o.id=og.orderid) WHERE o.uniacid=:uniacid AND og.channel_id=:channel_id AND o.status=3 AND og.channel_apply_status=0 AND og.ischannelpay=1 ", array(':uniacid' => $_W['uniacid'], ':channel_id' => $member['id']));
             
             $channel_info['channel']['commission_ok'] = $retail_commission + $purchase_commission;
 
@@ -641,6 +641,9 @@ if (!class_exists('ChannelModel')) {
 			$my_level = $this->getLevel($openid);
 			$up_level_num = $my_level['level_num'] + 1;
 			$channel_level = pdo_fetch("SELECT * FROM " . tablename('sz_yi_channel_level') . " WHERE uniacid={$_W['uniacid']} AND level_num={$up_level_num}");
+			if (empty($channel_level)) {
+				return;
+			}
 			$status = array();
             if ($set['become'] == 2) {//其他方式
             	if (in_array(1,$set['become_other'])) {//团队总人数
