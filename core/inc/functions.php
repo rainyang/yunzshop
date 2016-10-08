bdd01c4171fd3d84ca3ffc9a6197e92393f85701
吴敬宇 <827127436@qq.com>
2016-10-08 12:22:17 +0800

    防止发验证码短信报错

--- core/inc/functions.php
+++ core/inc/functions.php
@@ -153,7 +153,7 @@ function curl_download($url, $dir)
     return $res;
 }
 
-function send_sms($account, $pwd, $mobile, $code, $type = 'check', $name, $title, $total, $tel)
+function send_sms($account, $pwd, $mobile, $code, $type = 'check', $name = '', $title = '', $total = '', $tel = '')
 {
     if ($type == 'check') {
         $content = "您的验证码是：" . $code . "。请不要把验证码泄露给其他人。如非本人操作，可不用理会！";
