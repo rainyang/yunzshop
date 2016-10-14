6e7ba514c86757c4fd7088bab27500be31bf17dc
杨洋 <yang_0458@126.com>
2016-10-14 21:32:31 +0800

    [云打印]

--- plugin/yunprint/model.php
+++ plugin/yunprint/model.php
@@ -218,8 +218,11 @@ if (!class_exists('YunprintModel')) {
                     $orderinfo .= "优惠项目：           {$print_order['couponprice']}<BR>";
                 }
                 if ($offers['dispatchprice'] == 1) {
-                    $orderinfo .= "运费：                {$print_order['dispatchprice']}<BR>";
+                    $orderinfo .= "运费：               {$print_order['dispatchprice']}<BR>";
                 }
+            }
+            $orderinfo .= "实际支付：            {$print_order['price']}";
+            if (!empty($offers)) {
                 if ($offers['url'] == 1) {
                     $orderinfo .= "
                                 ================================<BR>
