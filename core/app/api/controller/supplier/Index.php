094b54f71d143f1fe63f60b007452ac78868d8ad
杨洋 <yang_0458@126.com>
2016-10-14 10:58:52 +0800

    no message

--- core/app/api/controller/supplier/Index.php
+++ core/app/api/controller/supplier/Index.php
@@ -7,46 +7,67 @@ use yii\helpers\ArrayHelper;
 
 class Index extends YZ
 {
+    private $json;
 
     public function __construct()
     {
         parent::__construct();
+        $result = $this->callPlugin('supplier/orderj');
+        $result['json']['checkmoney'] = "订单完成{$result['json']['set']['apply_day']}天后可提现{}元";
+        $this->json = $result;
     }
 
     public function index()
     {
-        $result = $this->callPlugin('supplier/orderj');
-        //$this->variable = $result['variable'];
-        $this->returnSuccess($result);
+        echo '<pre>';print_r($this->json);
+        $block_list = $this->_getSupplierBlockList();
+        订单完成{$set['apply_day']}天后可提现：{php echo $expect_money} 元 <br>可提现金额（元）（订单完成后可获得）</div>
     }
 
-    public function order()
+    private function _getSupplierBlockList()
     {
-        $result = $this->callPlugin('supplier/orderj/order');
-        //$this->variable = $result['variable'];
-        $this->returnSuccess($result);
-    }
+        $member = $this->json['member'];
+        $set = p('commission')->getSet();
+        $list = [
+            [
+                'id' => 1,
+                'icon' => '',
+                'title' => $set['texts']['commission1'],
+                'value' => $member['commission_total'],
+                'unit' => '元'
+            ], [
+                'id' => 2,
+                'icon' => '',
+                'title' => $set['texts']['order'],
+                'value' => $member['ordercount0'],
+                'unit' => '个'
+            ], [
+                'id' => 3,
+                'icon' => '',
+                'title' => $set['texts']['commission_detail'],
+                'value' => '',
+                'unit' => $set['texts']['commission_detail']
+            ], [
+                'id' => 4,
+                'icon' => '',
+                'title' => $set['texts']['myteam'],
+                'value' => $member['agentcount'],
+                'unit' => '个'
+            ], [
+                'id' => 5,
+                'icon' => '',
+                'title' => $set['texts']['mycustomer'],
+                'value' => $member['customercount'],
+                'unit' => '人'
+            ], [
+                'id' => 6,
+                'icon' => '',
+                'title' => '二维码',
+                'value' => '',
+                'unit' => '推广二维码'
+            ],
+        ];
 
-    public function logg()
-    {
-        $result = $this->callPlugin('supplier/logg');
-        //$this->variable = $result['variable'];
-        $this->returnSuccess($result);
-    }
-
-    public function applyg()
-    {
-        $result = $this->callPlugin('supplier/applyg');
-        //$this->variable = $result['variable'];
-        $this->returnSuccess($result);
-    }
-
-    public function detail()
-    {
-        $result = $this->callPlugin('supplier/detail');
-
-        echo '<pre>';print_r($result);exit;
-        //$this->variable = $result['variable'];
-        $this->returnSuccess($result);
+        return $list;
     }
 }
