e93401b7443947165b5757c2c7a16d4399afa858
吴敬宇 <827127436@qq.com>
2016-10-19 11:10:42 +0800

    添加定位功能

--- plugin/choose/core/mobile/list_goods.php
+++ plugin/choose/core/mobile/list_goods.php
@@ -82,7 +82,7 @@ if($_W['isajax']){
     } elseif (!empty($page['isstore']) || !empty($_GPC['storeid'])) {
         $args['isverify'] = 2;
     }
-
+    $args['pagesize'] = 50;
     $goods = m('goods')->getList($args);
     //替换门店商品库存（没有规格的商品）
     if ($page['isstore'] == 1) {
