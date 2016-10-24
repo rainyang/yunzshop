8a9e1a3409014665d00d828188e674c30c714a29
杨雷 <heiangdao@sina.com>
2016-10-24 18:19:26 +0800

    no message

--- core/app/api/controller/member/Log.php
+++ core/app/api/controller/member/Log.php
@@ -23,6 +23,31 @@ class Log extends YZ
     public function index()
     {
         $result = $this->callMobile('member/log');
+        foreach ($result['json']['list'] as $key => &$value) {
+            if ($value['status'] == '0') {
+                if ($value['type'] == '0') {
+                    $value['status_name'] = "未充值";
+                }else{
+                    $value['status_name'] = "申请中";
+                }
+            } elseif ($value['status'] == '1') {
+                if ($value['type'] == '0') {
+                    $value['status_name'] = "充值成功";
+                }elseif ($value['type'] == '1') {
+                    $value['status_name'] = "提现成功";
+                }else{
+                    $value['status_name'] = "打款成功";
+                }
+            } elseif ($value['status'] == '-1') {
+                if ($value['type'] == '1') {
+                    $value['status_name'] = "提现失败";
+                }
+            } elseif ($value['status'] == '3') {
+                if ($value['type'] == '0') {
+                    $value['status_name'] = "充值退款";
+                } 
+            }
+        }
         $this->returnSuccess($result);
 
 
