5c03586d7ac55251a87f03bcb934e269a025b07d
YMG <yangmingymg@163.com>
2016-11-09 11:58:42 +0800

    【调整】分销商注册接口调整

--- core/app/api/controller/commission/Register.php
+++ core/app/api/controller/commission/Register.php
@@ -29,19 +29,19 @@ class Register extends YZ
         $result['json']['footer']['vshop_commission'] = "微店卖出商品，您可以获得" . $result['json']['set']['texts']['commission'];
         $result['json']['footer']['centent'] = $result['json']['set']['texts']['agent'] . "的商品销售统一由厂家直接收款、直接发货，并提供产品的售后服务，" . $result['json']['set']['texts']['commission'] . "由厂家统一设置。";
 
-        if ($result['json']['status'] == 1) {
+        if ($result['json']['set']['become'] == 2) {
             $result['json']['centent'] = "本店累计消费满 " . $result['json']['status'] . " 次，
                        才可成为<" . $result['json']['shop_set']['name']  . ">购物中心" . $result['json']['set']['texts']['agent'] . "，您已消费 " . $result['json']['order'] . " 次，请继续努力！";
             $result['json']['button'] = "继续去购物";
-        } elseif ($result['json']['status'] == 2) {
+        } elseif ($result['json']['set']['become'] == 3) {
             $result['json']['centent'] = "本店累计消费满 " . $result['json']['moneycount'] . " 元，
                        才可成为<" . $result['json']['shop_set']['name']  . ">购物中心" . $result['json']['set']['texts']['agent'] . "，您已消费 " . $result['json']['money'] . " 元，请继续努力！";
             $result['json']['button'] = "继续去购物";
-        } elseif ($result['json']['centent'] = 3) {
+        } elseif ($result['json']['set']['become'] == 4) {
             $result['json']['centent'] = "本店需购买商品【" . $result['json']['goods']['title']  . "】才可成为<" . $result['json']['shop_set']['name']  . ">;购物中心" . $result['json']['set']['texts']['agent'] . "，请现在去购买吧！";
             $result['json']['button'] = "现在就去购买";
             //$result['json']['goods']['id'] = "现在就去购买";
-        } elseif ($result['json']['centent'] = 0) {
+        } elseif ($result['json']['set']['become'] == 1) {
             $result['json']['centent']['title'] = "欢迎加入" . $result['json']['shop_set']['name']  . "，请填写申请信息";
             $yname = $result['json']['agent'] ? $result['json']['agent']['nickname'] : "总店";
             $result['json']['centent']['desc'] = "邀请人：" . $yname . " (请核对)";
