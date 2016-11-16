4ae15565ea8a9f45337b62086b2251c713d4d64c
沈阳 <564345292@qq.com>
2016-11-16 09:32:11 +0800

    no message

--- core/app/api/controller/goods/Display.php
+++ core/app/api/controller/goods/Display.php
@@ -4,7 +4,7 @@ namespace app\api\controller\goods;
 use app\api\YZ;
 use app\api\Request;
 
-class Detail extends YZ
+class Display extends YZ
 {
     private $json;
     private $variable;
@@ -12,40 +12,28 @@ class Detail extends YZ
     public function __construct()
     {
         parent::__construct();
-        global $_W;
-        $_W['ispost']= true;
-        $result = $this->callMobile('shop/detail');
+        global $_W,$_GPC;
+        $_W['ispost'] = true;
+        $_GPC['pagesize'] = 10;
+        $result = $this->callMobile('shop/list');
         //dump($result);exit;
-        if($result['code'] == -1){
+        if ($result['code'] == -1) {
             $this->returnError($result['json']);
         }
         $this->variable = $result['variable'];
         $this->json = $result['json'];
     }
-    private function _getShareInfo()
-    {
-        global $_W;
-        $result = array(
-            'title' => $_W['shopshare']['title'],
-            'webUrl' => $_W['shopshare']['link'] . '&access=app',
-            'imageUrl' => $_W['shopshare']['imgUrl'],
-            'content' => $_W['shopshare']['desc']
-        );
-        return $result;
-    }
+
     public function index()
     {
+        global $_GPC;
         $res = $this->json;
-        $res['share'] = $this->_getShareInfo();
-
+        foreach ($res['goods'] as &$good) {
+            unset($good['content']);
+        }
+        $res['page'] = $_GPC['page'] ?: 1;
         $this->returnSuccess($res);
     }
-    public function _goodsPrice(){
-        /*<%if goods.minprice !=goods.maxprice%>
-                <%goods.minprice%> - <%goods.maxprice%>
-                <%else%>
-                <%goods.marketprice%>
-                <%/if%>*/
-    }
+
 }
 
