c8d5dff2a0e37d7ed54f6e40adb57ceed2994f14
陈凯照 <patiencing@outlook.com>
2016-11-29 01:27:45 +0800

    [修改]可自定义每页显示的通知个数,默认是20条

--- core/app/api/controller/member/MessageList.php
+++ core/app/api/controller/member/MessageList.php
@@ -17,7 +17,7 @@ class MessageList extends YZ
         $openid = m('user')->getOpenid();
         
         $page = max(1, intval($_GPC['page']));
-        $psize = 20;
+        $psize = !empty($_GPC['psize']) ? $_GPC['psize'] : 20;
 
         $list = pdo_fetchall("SELECT * FROM " . tablename('sz_yi_message') . " WHERE `openid` = '" . $openid . "' ORDER BY `id` DESC LIMIT ".($page - 1)*$psize . ',' . $psize);
         $result = show_json(1, array('list'=>$list));
