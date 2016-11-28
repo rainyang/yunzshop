510da90f4b2523ee24a0f1f9ddd649140fdfdda9
陈凯照 <patiencing@outlook.com>
2016-11-29 01:13:33 +0800

    [修改]给通知列表API增加"分页"功能

--- core/app/api/controller/member/MessageList.php
+++ core/app/api/controller/member/MessageList.php
@@ -15,8 +15,11 @@ class MessageList extends YZ
         }
 
         $openid = m('user')->getOpenid();
+        
+        $page = max(1, intval($_GPC['page']));
+        $psize = 20;
 
-        $list = pdo_fetchall("SELECT * FROM " . tablename('sz_yi_message') . " WHERE `openid` = '" . $openid . "' ORDER BY `id` DESC");
+        $list = pdo_fetchall("SELECT * FROM " . tablename('sz_yi_message') . " WHERE `openid` = '" . $openid . "' ORDER BY `id` DESC LIMIT ".($page - 1)*$psize . ',' . $psize);
         $result = show_json(1, array('list'=>$list));
 
         $this->returnSuccess($result);
