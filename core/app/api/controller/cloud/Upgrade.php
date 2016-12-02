a6f7269d6a8dc7920334d04dfe69f8a3ef270e76
yitianli <livsyitian@163.com>
2016-12-02 16:35:55 +0800

    【初步测试load()->func();】

--- core/app/api/controller/cloud/Upgrade.php
+++ core/app/api/controller/cloud/Upgrade.php
@@ -8,7 +8,17 @@ class Upgrade extends YZ
 
     public function index()
     {
+    	define('CLOUD_UPGRADE_URL', 'http://customer.liyitian.com/web/index.php?c=account&a=upgradetest1');
+    	load()->func('communication');
 
+    	$version = "1.3";
+    	$resp = ihttp_post(CLOUD_UPGRADE_URL, array(
+            'type' => 'download',
+            'signature' => 'sz_cloud_register',
+            'domain' => $_SERVER['HTTP_HOST'],
+            'version' => $version
+        ));
+        echo "<pre>"; print_r($resp);exit;
         return $this->returnSuccess();
     }
 
