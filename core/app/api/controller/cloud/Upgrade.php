d72e20a83ad2595cffdcff03cf75105cb36a172d
yitianli <livsyitian@163.com>
2016-12-05 14:43:35 +0800

    【备份，切换分支】

--- core/app/api/controller/cloud/Upgrade.php
+++ core/app/api/controller/cloud/Upgrade.php
@@ -8,19 +8,27 @@ class Upgrade extends YZ
 
     public function index()
     {
-    	define('CLOUD_UPGRADE_URL', 'http://customer.liyitian.com/web/index.php?c=account&a=upgradetest1');
-    	load()->func('communication');
+        $op = empty($_GPC['op']) ? 'display' : $_GPC['op'];
+        if ($op == 'display') {
 
-    	$version = "1.3";
-    	$resp = ihttp_post(CLOUD_UPGRADE_URL, array(
+        }
+        return $this->returnSuccess();
+    }
+
+    function detection()
+    {
+        echo "<pre>"; print_r(456);exit;
+    }
+
+}
+/*        define('CLOUD_UPGRADE_URL', 'http://lyt.yunzshop.com/web/index.php?c=account&a=upgradetest1');
+        load()->func('communication');
+
+        $version = "1.3";
+        $resp = ihttp_post(CLOUD_UPGRADE_URL, array(
             'type' => 'download',
             'signature' => 'sz_cloud_register',
             'domain' => $_SERVER['HTTP_HOST'],
             'version' => $version
         ));
-        echo "<pre>"; print_r($resp);exit;
-        return $this->returnSuccess();
-    }
-
-}
-
+        echo "<pre>"; print_r($resp);exit;*/
