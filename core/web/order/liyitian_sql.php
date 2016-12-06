2cdb003ef09379e8b7ebad3bd13a0531ea0547f2
yitianli <livsyitian@163.com>
2016-12-06 11:24:25 +0800

    【修改liyitian_sql语法错误】

--- core/web/order/liyitian_sql.php
+++ core/web/order/liyitian_sql.php
@@ -6,7 +6,7 @@ if (!pdo_fieldexists('sz_yi_member', 'membermobile')) {
     pdo_query($sql);
 }
 //12.2增加app库APK信息数据表
-$sql = "CREATE TABLE IF NOT EXISTS" . tablename(`sz_yi_appinfo`). "(
+$sql = "CREATE TABLE IF NOT EXISTS " . tablename('sz_yi_appinfo'). "(
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `apkname` varchar(255) DEFAULT NULL COMMENT 'apk包名',
   `apktype` varchar(255) DEFAULT NULL COMMENT 'apk类型',
@@ -24,10 +24,10 @@ $sql = "CREATE TABLE IF NOT EXISTS" . tablename(`sz_yi_appinfo`). "(
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='app升级包APK信息记录表【客户端】';
 ";
-pdo_fetchall($sql);
+pdo_query($sql);
 
 //云端数据库增加APK信息表【只限云端使用，使用时去请粘贴出去独立运行】
-/*$sql = "CREATE TABLE IF NOT EXISTS" . tablename(`sz_yi_client_app`). "(
+/*$sql = "CREATE TABLE IF NOT EXISTS " . tablename('sz_yi_client_app'). "(
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `clientid` int(11) DEFAULT NULL COMMENT '客户ID',
   `apkname` varchar(255) DEFAULT NULL COMMENT 'apk包名',
@@ -51,22 +51,3 @@ pdo_query($sql);*/
 
 
 echo  "运行成功";
-
-/*
-CREATE TABLE IF NOT EXISTS `ims_sz_yi_appinfo`(
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `apkname` varchar(255) DEFAULT NULL COMMENT 'apk包名',
-  `apktype` varchar(255) DEFAULT NULL COMMENT 'apk类型',
-  `apksize` varchar(255) DEFAULT NULL COMMENT 'apk文件大小',
-  `createtime` int(11) DEFAULT NULL COMMENT '创建apk时间',
-  `appname` varchar(255) DEFAULT NULL COMMENT 'APP应用名称',
-  `package` varchar(255) DEFAULT NULL COMMENT 'APP应用包名',
-  `version_name` varchar(255) DEFAULT NULL COMMENT 'app版本',
-  `version_code` varchar(255) DEFAULT NULL COMMENT '版本代码，用来判断版本',
-  `app_icon` varchar(255) DEFAULT NULL COMMENT '图片路径',
-  `apkpath` varchar(255) DEFAULT NULL COMMENT '文件储存路径',
-  `apkremark` text COMMENT '更新日志',
-  `downloadurl` varchar(255) NOT NULL COMMENT '采集地址',
-  `clientdownload` varchar(255) NOT NULL COMMENT '客户下载地址',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='app升级包APK信息记录表【客户端】';*/
