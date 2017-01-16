7b28e239fe9bd3a7001c2fc952d4e6551bd4deec
dingran80s <dingran@zongji.in>
2017-01-16 16:33:31 +0800

    修改app广告图查询SQ语句L

--- core/mobile/shop/interface.php
+++ core/mobile/shop/interface.php
@@ -22,7 +22,7 @@ if ($operation == 'result') {
 } elseif ($operation == 'adv') { //引导广告位图片
     $qiniu_domain = "http://7xwyfd.com1.z0.glb.clouddn.com/";
 
-    $banner = pdo_fetch("SELECT * FROM " . tablename('sz_yi_banner') . " WHERE uniacid=:uniacid  ORDER BY `id` DESC" ,array(":uniacid" => $_W['uniacid']));
+    $banner = pdo_fetch("SELECT * FROM " . tablename('sz_yi_banner') . " WHERE uniacid=:uniacid AND enabled=1 ORDER BY `id` DESC" ,array(":uniacid" => $_W['uniacid']));
 
     $file_info = pathinfo($banner['thumb']);
 
