6d2c72f8febb0d97c9ee9444682def04ed113ff6
kaizhao <patiencing@outlook.com>
2017-02-06 13:47:36 +0800

    更新"获取sign"的API

--- core/mobile/live/room.php
+++ core/mobile/live/room.php
@@ -8,6 +8,7 @@ $room_id = $_GPC['room_id']; //必须的参数
 $page = $_GPC['page'];
 $page_size = $_GPC['pagesize'];
 $openid = m('user')->getOpenid();
+$domain = $_SERVER['HTTP_HOST'];
 
 if ($operation == 'display'){
 
@@ -42,7 +43,7 @@ if ($operation == 'display'){
 
     //获取sig
     if(empty($_GPC['sig'])){
-        $result_02 = ihttp_get('http://live.tbw365.cn/shop_live.php?api=IM/Get/sign&openid='.$openid);
+        $result_02 = ihttp_get('http://live.tbw365.cn/shop_live.php?api=IM/Get/sign&openid='.$openid.'&domain='.$domain);
         $result_02_array = json_decode($result_02['content'], true);
         $sig = $result_02_array['data']['sign'];
     }
