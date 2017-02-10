d0b8e34c5efe33fc39f702b97894af0132ac5dfb
kaizhao <patiencing@outlook.com>
2017-02-10 10:15:52 +0800

    修改注释(订单的主播引流标识应该是云端的主播ID)

--- core/mobile/order/confirm.php
+++ core/mobile/order/confirm.php
@@ -1811,7 +1811,7 @@ if ($_W['isajax']) {
 
     }
     elseif ($operation == 'create' && $_W['ispost']) {
-        $fromanchor = empty($_GPC['fromanchor']) ? NULL : intval($_GPC['fromanchor']); //主播id(用于后面生成订单时标识是从哪个主播的直播引流过来的)
+        $fromanchor = empty($_GPC['fromanchor']) ? NULL : intval($_GPC['fromanchor']); //云端的主播ID(用于后面生成订单时标识是从哪个主播引流过来的)
         $ischannelpay = intval($_GPC['ischannelpay']);
         $ischannelpick = intval($_GPC['ischannelpick']);
         //$isyunbipay = intval($_GPC['isyunbipay']);
