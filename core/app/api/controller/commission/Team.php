8e891b3fbd1795cf4d8c63c10bcadd6e249fcad6
沈阳 <564345292@qq.com>
2017-02-14 16:16:52 +0800

    分销 团队接口

--- core/app/api/controller/commission/Team.php
+++ core/app/api/controller/commission/Team.php
@@ -14,12 +14,12 @@ class Team extends YZ
     public function index()
     {
         $result = $this->callPlugin('commission/team');
-        $result['json']['set']['texts']['myteam'] = $result['json']['set']['texts']['myteam'] . "(" . $result['json']['total'] . ")";
-        $result['json']['set']['texts']['c1'] = $result['json']['set']['texts']['c1'] . "(" . $result['json']['level1'] . ")";
-		$result['json']['set']['texts']['c2'] = $result['json']['set']['texts']['c2'] . "(" . $result['json']['level2'] . ")";
-		$result['json']['set']['texts']['c3'] = $result['json']['set']['texts']['c3'] . "(" . $result['json']['level3'] . ")";
-		$result['json']['set']['texts']['commission_team'] = "TA的" . $result['json']['set']['texts']['commission'] . "/成员";
-        $result['json']['pageon'] = ceil($result['json']['total'] / $result['json']['pagesize']);
+        $result['json']['set']['texts']['myteam'] = $result['variable']['set']['texts']['myteam'] . "(" . $result['variable']['total'] . ")";
+        $result['json']['set']['texts']['c1'] = $result['variable']['set']['texts']['c1'] . "(" . $result['variable']['level1'] . ")";
+		$result['json']['set']['texts']['c2'] = $result['variable']['set']['texts']['c2'] . "(" . $result['variable']['level2'] . ")";
+		$result['json']['set']['texts']['c3'] = $result['variable']['set']['texts']['c3'] . "(" . $result['variable']['level3'] . ")";
+		$result['json']['set']['texts']['commission_team'] = "TA的" . $result['variable']['set']['texts']['commission'] . "/成员";
+        $result['json']['pageon'] = ceil($result['variable']['total'] / $result['json']['pagesize']);
         foreach ($result['json']['list'] as $key => &$val) {
         	$val['avatar'] = !empty($val['avatar']) ? $val['avatar'] : "../addons/sz_yi/plugin/commission/images/head.jpg";
         	$val['nickname'] = !empty($val['nickname']) ? $val['nickname'] : "未获取";
