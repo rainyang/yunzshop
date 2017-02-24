c832cc38f694d8e74d438b47d5c43d18986d6cd8
yittian <livsyitian@163.com>
2017-02-24 17:52:12 +0800

    完成会员分组增、删、查

--- app/backend/modules/member/models/MemberGroup.php
+++ app/backend/modules/member/models/MemberGroup.php
@@ -11,8 +11,16 @@ namespace app\backend\modules\member\models;
 
 class MemberGroup extends \app\common\models\MemberGroup
 {
+
     /**
-     * 获取会员分组列表
+     *  不可填充字段.
+     *
+     * @var array
+     */
+    protected $guarded = [''];
+
+    /**
+     * 获取当前公众号会员分组列表
      * @Author::yitian 2017-02-23 qq:751818588
      * @access public
      * @param int $uniacid 公众号id
@@ -23,5 +31,43 @@ class MemberGroup extends \app\common\models\MemberGroup
     {
         $group_list = MemberGroup::where('uniacid', $uniacid)->get();
         return $group_list;
+    }
+    /**
+     * 添加会员分组列表
+     * @Author::yitian 2017-02-24 qq:751818588
+     * @access public
+     * @param array $data 会员组信息
+     *
+     * @return int $id
+     **/
+    public static function createMemberGroup($data)
+    {
+        $id = static::insert($data);
+        return $id;
+    }
+    /**
+     * 修改会员分组列表
+     * @Author::yitian 2017-02-24 qq:751818588
+     * @access public
+     * @param int $group_id 会员组ID
+     *
+     * @return
+     **/
+    public function updateMemberGroup($group_id)
+    {
+
+    }
+    /**
+     * 删除会员分组列表
+     * @Author::yitian 2017-02-24 qq:751818588
+     * @access public
+     * @param int $group_id 会员组ID
+     *
+     * @return
+     **/
+    public static function deleteMemberGroup($group_id)
+    {
+        $id = static::where('id', $group_id)->delete();
+        return $id;
     }
 }
