3177eefc9321ea141d36f6a79db8f6b678246ed1
yittian <livsyitian@163.com>
2017-02-24 09:10:56 +0800

    合并分支 提交

---
+++ app/backend/modules/member/models/MemberCart.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: libaojia
+ * Date: 2017/2/22
+ * Time: 下午3:00
+ */
+
+namespace app\backend\modules\member\models;
+
+
+class MemberCart extends \app\common\models\MemberCart
+{
+    public function test()
+    {}
+}
