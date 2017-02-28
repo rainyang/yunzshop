d19ef4fefde04c750a12a564dac7d37ce7f4989c
kaizhao <patiencing@outlook.com>
2017-02-28 14:51:48 +0800

    订单操作之"确认发货"和"取消发货"

---
+++ app/common/models/OrderCancelSend.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\common\models;
+
+use Illuminate\Database\Eloquent\Model;
+use app\common\modules;
+
+/*
+ * 取消发货
+ */
+class OrderCancelSent
+{
+    public $order_model;
+
+    public function __construct(OrderModel $order_model)
+    {
+        $this->order_model = $order_model->getData();
+    }
+
+    public function cancelSend()
+    {
+        Order::where('shop_id', $this->order_model['shop_id'])
+            ->where('id', $this->order_model['id'])
+            ->update(['status' => 1]);
+    }
+
+    public function cancelSendable()
+    {
+        if ($this->order_model['status'] == 2) {
+            return true;
+        }
+    }
+}
---
+++ app/common/models/OrderSend.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\common\models;
+
+use Illuminate\Database\Eloquent\Model;
+use app\common\modules;
+
+/*
+ * 确认发货
+ */
+class OrderSent
+{
+    public $order_model;
+
+    public function __construct(OrderModel $order_model)
+    {
+        $this->order_model = $order_model->getData();
+    }
+
+    public function send()
+    {
+        Order::where('shop_id', $this->order_model['shop_id'])
+            ->where('id', $this->order_model['id'])
+            ->update(['status' => 2]);
+    }
+
+    public function sendable()
+    {
+        if ($this->order_model['status'] == 1) {
+            return true;
+        }
+    }
+}
