5aa9c3be5b8cdd5533aee6b3d650a722f005e85f
沈阳 <564345292@qq.com>
2017-03-02 14:28:15 +0800

    订单model更新

---
+++ app/common/models/OrderGoods.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shenyang
+ * Date: 2017/3/2
+ * Time: 上午11:24
+ */
+
+namespace app\common\models;
+
+
+class OrderGoods
+{
+
+}
---
+++ app/frontend/modules/order/model/factory/WaiPayOrderModelFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shenyang
+ * Date: 2017/3/2
+ * Time: 上午9:44
+ */
+
+namespace app\frontend\modules\order\model\factory;
+
+
+class WaiPayOrderModelFactory
+{
+
+}
