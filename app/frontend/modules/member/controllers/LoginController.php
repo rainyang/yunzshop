e287d0e65525834c3bf30f9ca8e4f8e7356e98c7
dingran80s <dingran@zongji.in>
2017-03-03 09:07:30 +0800

    增加发送短信限制功能

--- app/frontend/modules/member/controllers/LoginController.php
+++ app/frontend/modules/member/controllers/LoginController.php
@@ -13,6 +13,7 @@ use app\common\components\BaseController;
 use app\frontend\modules\member\services\factory\MemberFactory;
 use app\frontend\modules\member\models\MemberModel;
 use Illuminate\Session\Store;
+use League\Flysystem\Exception;
 
 class LoginController extends BaseController
 {
@@ -25,33 +26,46 @@ class LoginController extends BaseController
         // 1-公众号;2-小程序;3-微信app;4-pc扫码;5-手机号/app;6-QQ
         $type = \YunShop::request()->type;
 
-        switch ($type) {
-            case '1':
-                $member = MemberFactory::create('OfficeAccount');
-                break;
-            case '2':
-                $member = MemberFactory::create('MiniApp');
-                break;
-            case '3':
-                $member = MemberFactory::create('AppWechat');
-                break;
-            case '4':
-                $member = MemberFactory::create('Wechat');
-                break;
-            case '5':
-                if ((\YunShop::app()->isajax) && (\YunShop::app()->ispost && $this->_validate())) {
-                    $member = MemberFactory::create('Mc');
-                }
-                if (SZ_YI_DEBUG) {
-                    $member = MemberFactory::create('Mc');
-                }
-                break;
-            case '6':
-                $member = MemberFactory::create('QQ');
-                break;
-        }
+        if (!empty($type)) {
+            switch ($type) {
+                case '1':
+                    $member = MemberFactory::create('OfficeAccount');
+                    break;
+                case '2':
+                    $member = MemberFactory::create('MiniApp');
+                    break;
+                case '3':
+                    $member = MemberFactory::create('AppWechat');
+                    break;
+                case '4':
+                    $member = MemberFactory::create('Wechat');
+                    break;
+                case '5':
+                    if ((\YunShop::app()->isajax) && (\YunShop::app()->ispost && $this->_validate())) {
+                        $member = MemberFactory::create('Mc');
+                    }
+                    if (SZ_YI_DEBUG) {
+                        $member = MemberFactory::create('Mc');
+                    }
+                    break;
+                case '6':
+                    $member = MemberFactory::create('QQ');
+                    break;
+                default:
+                    $member = null;
+            }
 
-        $member->login();
+            try{
+                $member->login();
+            } catch (Exception $e) {
+                if ($e->getHttpStatus() != NULL) {
+                    header('Status: ' . $e->getHttpStatus());
+                    echo $e->getHttpBody();
+                }
+            }
+        } else {
+            return show_json(0, array('msg' => '登录失败'));
+        }
     }
 
     private function _validate()
