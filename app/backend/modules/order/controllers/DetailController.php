fb79f789b33b840e8e124acf22d54848d3c6402b
杨洋 <yang_0458@126.com>
2017-03-04 16:39:04 +0800

    后台订单详情页

--- app/backend/modules/order/controllers/DetailController.php
+++ app/backend/modules/order/controllers/DetailController.php
@@ -17,9 +17,44 @@ class DetailController extends BaseController
     {
         $db_order_models = Order::waitPay()->with('hasManyOrderGoods')->first();
         $order = $db_order_models->toArray();
+        $order['goods'] = [];
         $this->render('detail', [
-            'order' => $order
+            'order' => $order,
+            'lang' => $this->_lang(),
+            'totals'=> $this->_totals(),
+            'dispatch' => ['id' => 1],
+            'member' => [
+                'avatar' => '',
+                'nickname' => '',
+                'id' => '',
+                'mobile' => '',
+                'realname' => '',
+                'weixin' => ''
+            ]
         ]);
-        dd($order);
+    }
+
+    private function _lang()
+    {
+        return array(
+            'goods' => '商品',
+            'good' => '商品',
+            'orderlist' => '订单列表'
+        );
+    }
+
+    private function _totals()
+    {
+        return array(
+            'index' => '30',
+            'waitPay' => '3',
+            'waitSend' => '2',
+            'waitReceive' => '5',
+            'complete' => '6',
+            'close' => '7',
+            'waitRefund' => '2',
+            'refund' => '1',
+            'applyWithdraw' => '4',
+        );
     }
 }
