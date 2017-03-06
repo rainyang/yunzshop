cb6d687297796359ed019b55c29b0d20a726acc3
沈阳 <564345292@qq.com>
2017-03-06 16:07:08 +0800

    订单列表模板

--- app/backend/modules/order/controllers/ListController.php
+++ app/backend/modules/order/controllers/ListController.php
@@ -18,26 +18,31 @@ class ListController extends BaseController
     public function index()
     {
         $pageSize = 2;
-        $list = Order::with('belongsToMember','hasOneOrderDispatch','hasManyOrderGoods.hasOneGoods')->paginate($pageSize)->toArray();
+        $total_price = Order::sum('price');
+        $list = Order::with('belongsToMember', 'hasOneOrderDispatch', 'hasManyOrderGoods.hasOneGoods')->paginate($pageSize)->toArray();
         $pager = PaginationHelper::show($list['total'], $list['current_page'], $list['per_page']);
         //dd($list);
         $data = [
-            'list' => $list['data'],
+            'list' => $list,
+            'total_price' => $total_price,
             'lang' => $this->_lang(),
-            'totals'=> $this->_totals(),
+            'totals' => $this->_totals(),
             'pager' => $pager,
         ];
-        $data+=$this->fakeData();
+        $data += $this->fakeData();
         $this->render('order/list', $data);
 
     }
-    public function test(){
+
+    public function test()
+    {
         $list = Order::with('hasOneOrderDispatch')->first();
 
         dd($list);
 
 
     }
+
     public function waitPay()
     {
         $db_order_models = Order::waitPay()->with('hasManyOrderGoods')->get();
@@ -47,43 +52,45 @@ class ListController extends BaseController
         exit;
     }
 
-    private function fakeData(){
+    private function fakeData()
+    {
         return array(
-            'supplierapply'=>'',
-            'stores'=>'',
-            'list'=>'',
-            'yunbiset'=>'',
-            'card_plugin'=>'',
-            'perm_role'=>'',
-            'sstarttime'=>0,
-            'r_type'=>'',
-            'costmoney'=>'',
-            'card_set'=>'',
-            'liveRooms'=>'',
-            'paytype'=>'',
-            'sendtime'=>0,
-            'cashier_stores'=>'',
-            'supplier'=>'',
-            'type'=>'',
-            'store'=>'',
-            'status'=>'',
-            'pendtime'=>0,
-            'p_cashier'=>'',
-            'liveRoom'=>'',
-            'cashier_store'=>'',
-            'key'=>'',
-            'endtime'=>0,
-            'fendtime'=>0,
-            'shopset'=>'',
-            'pstarttime'=>0,
-            'level'=>'',
-            'suppliers'=>'',
-            'request'=>'',
-            'fstarttime'=>0,
-            'starttime'=>0,
-            'agentid'=>'',
+            'supplierapply' => '',
+            'stores' => '',
+            'list' => '',
+            'yunbiset' => '',
+            'card_plugin' => '',
+            'perm_role' => '',
+            'sstarttime' => 0,
+            'r_type' => '',
+            'costmoney' => '',
+            'card_set' => '',
+            'liveRooms' => '',
+            'paytype' => '',
+            'sendtime' => 0,
+            'cashier_stores' => '',
+            'supplier' => '',
+            'type' => '',
+            'store' => '',
+            'status' => '',
+            'pendtime' => 0,
+            'p_cashier' => '',
+            'liveRoom' => '',
+            'cashier_store' => '',
+            'key' => '',
+            'endtime' => 0,
+            'fendtime' => 0,
+            'shopset' => '',
+            'pstarttime' => 0,
+            'level' => '',
+            'suppliers' => '',
+            'request' => '',
+            'fstarttime' => 0,
+            'starttime' => 0,
+            'agentid' => '',
         );
     }
+
     private function _lang()
     {
         return array(
@@ -92,6 +99,7 @@ class ListController extends BaseController
             'orderlist' => '订单列表'
         );
     }
+
     private function _totals()
     {
         return array(
