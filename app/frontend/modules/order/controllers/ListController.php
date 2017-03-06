d363dff82dfe40254aecfb26050a2542c3512e8c
kaizhao <patiencing@outlook.com>
2017-03-06 12:51:41 +0800

    [修补Bug]报错:title字段不存在(title字段在goods表,不在order表); [增加逻辑]新增"待付款订单列表"

--- app/frontend/modules/order/controllers/ListController.php
+++ app/frontend/modules/order/controllers/ListController.php
@@ -14,24 +14,37 @@ use app\common\models\Order;
 
 class ListController extends BaseController
 {
+    //所有订单的列表
     public function index(){
         $pageSize=5;
         
         $list = Order::with(['hasManyOrderGoods'=>function($query){
             return $query->select(['id','order_id','goods_id','goods_price','total','price'])
                             ->with(['belongsToGood'=>function($query1){
-                                return $query1->select(['id','price']);
+                                return $query1->select(['id','price','title']);
                             }]);
-        }])->get(['id','order_sn','goods_price','price','status'])->toArray();
+        }])->get(['id','order_sn','goods_price','price'])->toArray();
         
         dd($list);
 
     }
+
+    //待付款订单列表
     public function waitPay(){
-        $db_order_models = Order::waitPay()->with('hasManyOrderGoods')->get();
-        //dd($db_order_models);
-        $order_models = $db_order_models;
-        dd($order_models[0]->button_models);
-        exit;
+        // $db_order_models = Order::waitPay()->with('hasManyOrderGoods')->get();
+        // //dd($db_order_models);
+        // $order_models = $db_order_models;
+        // dd($order_models[0]->button_models);
+        // exit;
+        $list = Order::waitPay()->with(['hasManyOrderGoods'=>function($query){
+            return $query->select(['id','order_id','goods_id','goods_price','total','price'])
+                            ->with(['belongsToGood'=>function($query1){
+                                return $query1->select(['id','price','title']);
+                            }]);
+        }])->get(['id','order_sn','goods_price','price'])->toArray();
+        
+        dd($list);
     }
+
+    //
 }
