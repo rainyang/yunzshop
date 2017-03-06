d8d24dbf10eaa67036808ae3b48241fa8c06e585
杨洋 <yang_0458@126.com>
2017-03-06 17:45:01 +0800

    后台订单详情操作

--- app/frontend/modules/order/controllers/OpController.php
+++ app/frontend/modules/order/controllers/OpController.php
@@ -17,50 +17,49 @@ use app\frontend\modules\order\services\behavior\OrderSend;
 
 class OpController
 {
-    public function pay($order_id){
-        dd(2);
-        $order = Order::find($order_id);
+    public function pay(){
+        $order = Order::find(\YunShop::request()->order_id);
         $order_pay = new OrderPay($order);
         if (!$order_pay->payable()) {
             echo '状态不正确';exit;
         }
         $order_pay->pay();
     }
-    public function cancelPay($order_id){
-        $order = Order::find($order_id);
+    public function cancelPay(){
+        $order = Order::find(\YunShop::request()->order_id);
         $cancel_pay = new OrderCancelPay($order);
         if (!$cancel_pay->cancelable()) {
             echo '状态不正确';exit;
         }
         $cancel_pay->cancelPay();
     }
-    public function send($order_id){
-        $order = Order::find($order_id);
+    public function send(){
+        $order = Order::find(\YunShop::request()->order_id);
         $order_send = new OrderSend($order);
         if (!$order_send) {
             echo '状态不正确';exit;
         }
         $order_send->send();
     }
-    public function cancelSend($order_id){
-        $order = Order::find($order_id);
+    public function cancelSend(){
+        $order = Order::find(\YunShop::request()->order_id);
         $cancel_send = new OrderCancelSend($order);
         if (!$cancel_send->sendable()) {
             echo '状态不正确';exit;
         }
         $cancel_send->cancelSend();
     }
-    public function Receive($order_id){
-        $order = Order::find($order_id);
+    public function Receive(){
+        $order = Order::find(\YunShop::request()->order_id);
         $order_receive = new OrderReceive($order);
         if (!$order_receive->receiveable()) {
             echo '状态不正确';exit;
         }
         $order_receive->receive();
     }
-    public function Delete($order_id)
+    public function Delete()
     {
-        $order = Order::find($order_id);
+        $order = Order::find(\YunShop::request()->order_id);
         $order_delete = new OrderDelete($order);
         if (!$order_delete->deleteable()) {
             echo '状态不正确';exit;
