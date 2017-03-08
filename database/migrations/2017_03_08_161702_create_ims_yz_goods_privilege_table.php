6ed05aafd5601bad7216f6c3ec080172985652e8
LuckyStar.D <18646546800@163.com>
2017-03-08 16:19:19 +0800

    更新数据库字段类型

--- database/migrations/2017_03_08_161702_create_ims_yz_goods_privilege_table.php
+++ database/migrations/2017_03_08_161702_create_ims_yz_goods_privilege_table.php
@@ -20,10 +20,10 @@ class CreateImsYzGoodsPrivilegeTable extends Migration {
 			$table->text('show_groups', 65535)->nullable()->comment('会员组浏览权限');
 			$table->text('buy_levels', 65535)->nullable()->comment('会员等级购买权限');
 			$table->text('buy_groups', 65535)->nullable()->comment('会员组购买权限');
-			$table->integer('once_buy_limit')->nullable()->comment('每次限购数量');
-			$table->integer('total_buy_limit')->nullable()->comment('总共限购数量');
-			$table->dateTime('time_begin_limit')->nullable()->comment('限购开始时间');
-			$table->dateTime('time_end_limit')->nullable()->comment('限购结束时间');
+			$table->integer('once_buy_limit')->nullable()->default(0)->comment('每次限购数量');
+			$table->integer('total_buy_limit')->nullable()->default(0)->comment('总共限购数量');
+			$table->integer('time_begin_limit')->nullable()->comment('限购开始时间');
+			$table->integer('time_end_limit')->nullable()->comment('限购结束时间');
 		});
 	}
 
