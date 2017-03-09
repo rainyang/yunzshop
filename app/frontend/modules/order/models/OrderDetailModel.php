c6f8960c420af2037b3bb6fc01bb06670845a89d
kaizhao <patiencing@outlook.com>
2017-03-09 10:00:17 +0800

    修改"订单详情"返回字段

--- app/frontend/modules/order/models/OrderDetailModel.php
+++ app/frontend/modules/order/models/OrderDetailModel.php
@@ -7,14 +7,16 @@ use app\common\models\Order;
 
 class OrderDetailModel extends Order
 {
+//    protected $hidden = ['id','uniacid','status','is_deleted','is_member_deleted','created_at','updated_at','deleted_at'];
+
     /*
      * 根据 Order_id 获取订单详情,包括订单内商品的详情
      */
     public static function getOrderDetail($orderId)
     {
-        $orderModels = Order::with(['hasManyOrderGoods'=>function($query){
-            return $query->select(['id','order_id','goods_id','goods_price','total','price','title','thumb']);
-        }])->get(['id','order_sn'])->find($orderId);
+        $orderModels = self::with(['hasManyOrderGoods'=>function($query){
+            return $query->select(['order_id','goods_id','goods_price','total','price','title','thumb']);
+        }])->get(['id','member_id','order_sn','price','goods_price','create_time','finish_time','pay_time','send_time','cancel_time'])->find($orderId);
         return $orderModels;
     }
 }
