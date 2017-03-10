aa2541f353e6be33d06e386bc0e41669331fd49f
杨洋 <yang_0458@126.com>
2017-03-10 10:50:31 +0800

    no message

--- app/frontend/modules/order/controllers/PayController.php
+++ app/frontend/modules/order/controllers/PayController.php
@@ -18,29 +18,34 @@ class PayController extends BaseController
 {
     public function display()
     {
-        //if (\YunShop::app()->isajax) {
-            $set = Setting::get('pay');
-            $order_id = \YunShop::request()->order_id = 1;
-            VerifyPayService::verifyPay($order_id);
-            //还需要商品规格
-            $db_order_model = Order::with(
-                [
-                    'hasManyOrderGoods.belongsToGood',
-                    'hasOnePay'
-                ]
-            )->find($order_id);
-            //dd($db_order_model->hasOnePay->pay_sn);
-            VerifyPayService::verifyLog($db_order_model);
-            //所有支付方式
-            $all_pays = [];
-            $return_url = urlencode($this->createMobileUrl('order.pay.display', array('order_id' => $order_id)));
+        $set = Setting::get('pay');
+        $order_id = \YunShop::request()->order_id;
+        $msg = VerifyPayService::verifyPay($order_id);
+        if ($msg) {
+            return $this->errorJson($msg, $data = []);
+        }
+        $db_order_model = Order::with(
+            [
+                'hasManyOrderGoods.belongsToGood',
+                'hasOnePay'
+            ]
+        )->find($order_id);
+        $log_msg = VerifyPayService::verifyLog($db_order_model);
+        if ($log_msg) {
+           return $this->errorJson($log_msg, $data = []);
+        }
+        //所有支付方式
+        $all_pays = [];
+        $return_url = urlencode($this->createMobileUrl('order.pay.display', array('order_id' => $order_id)));
 
-            return show_json(1, [
+        return $this->successJson(
+            $msg = 'ok',
+            $data = [
                 'order'     => $db_order_model,
                 'set'       => $set,
                 'all_pays'  => $all_pays,
                 'return_url'    => $return_url,
-            ]);
-        //}
+            ]
+        );
     }
 }
