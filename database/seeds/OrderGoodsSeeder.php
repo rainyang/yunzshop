068c6c8083d6048afbbdca438bdf27bddd1bf5ff
kaizhao <patiencing@outlook.com>
2017-03-13 05:39:20 +0000

    使用假数据,跑通数据迁移脚本(后续需要会员和商品提供新旧ID对应关系的数据)

--- database/seeds/OrderGoodsSeeder.php
+++ database/seeds/OrderGoodsSeeder.php
@@ -8,8 +8,8 @@ class OrderGoodsSeeder extends Seeder
     protected $table = 'yz_order_goods';
     protected $sourceGoodsTable = 'sz_yi_goods';
     protected $orderTable = 'yz_order';
-    protected $mappingOrderTable = 'yz_mapping_orders';
-    protected $mappingGoodTable = 'yz_mapping_goods'; //todo 等待"商品重构组"生成新旧goods_id的对应表
+//    protected $mappingOrderTable = 'yz_mapping_orders';
+//    protected $mappingGoodTable = 'yz_mapping_goods'; //todo 等待"商品重构组"生成新旧goods_id的对应表
 
     /**
      * Run the database seeds.
@@ -19,11 +19,12 @@ class OrderGoodsSeeder extends Seeder
     public function run()
     {
         //检测新的数据表是否有数据
-        $newList = DB::table($this->table)->first();
-        if($newList){
-            echo $this->table."数据表已经有数据, 请检查.\n";
-            return;
-        }
+//        $newList = DB::table($this->table)->first();
+//        if($newList){
+//            echo $this->table."数据表已经有数据, 请检查.\n";
+//            return;
+//        }
+//        print_r($_SERVER);
 
         $sourceList = DB::table($this->sourceTable)->chunk(100, function($records){
             foreach ($records as $record){
@@ -44,10 +45,11 @@ class OrderGoodsSeeder extends Seeder
                     ->where('id','=',$record['goodsid'])->first();
                 if ($goodInfo){
                     $record['title'] = $goodInfo['title'];
-                    if(preg_match('/^http.*/',$record['title'])){
+                    if(preg_match('/^http.*/',$goodInfo['thumb'])){
                         $record['thumb'] = $goodInfo['thumb']; //todo 最后的url取决于迁徙后放在哪个路径
-                    } else if (preg_match('/^images.*/',$record['title'])){
-                        $record['thumb'] = $_SERVER['SERVER_NAME'].'/attachment/'.$goodInfo['thumb']; //todo 最后的url取决于迁徙后放在哪个路径
+                    } else if (preg_match('/^images.*/',$goodInfo['thumb'])){
+                        $record['thumb'] = 'http://demo.yunzshop.com'.'/attachment/'.$goodInfo['thumb']; //todo 最后的url取决于迁徙后放在哪个路径
+                        //无法通过全局变量$_SERVER['SERVER_NAME']获取网站域名
                     }
                 } else {
 //                    echo '在旧的goods表中找不到该商品信息';
@@ -57,22 +59,25 @@ class OrderGoodsSeeder extends Seeder
                 }
 
                 //获取orderid和member_id
-                $orderMapping = DB::table($this->mappingOrderTable)->select('new_order_id','new_member_id')
-                    ->where('old_order_id','=',$record['orderid'])->first();
-                if(!$orderMapping){
-                    echo $this->mappingOrderTable.'表中没有找到新旧orderid的对应关系,旧orderid为'.$record['orderid'];
-                    return;
-                }
-                $uid = $orderMapping['new_member_id'];
-                $orderId = $orderMapping['new_order_id'];
+//                $orderMapping = DB::table($this->mappingOrderTable)->select('new_order_id','new_member_id')
+//                    ->where('old_order_id','=',$record['orderid'])->first();
+//                if(!$orderMapping){
+//                    echo $this->mappingOrderTable.'表中没有找到新旧orderid的对应关系,旧orderid为'.$record['orderid'];
+//                    return;
+//                }
+//                $uid = $orderMapping['new_member_id'];
+//                $orderId = $orderMapping['new_order_id'];
+                $uid = 1; //todo 调试用
+                $orderId = $record['orderid']; //todo 调试用
 
                 //获取goods_id
-                $goodsId = DB::table($this->mappingGoodTable)->select('new_good_id')
-                    ->where('old_good_id','=',$record['goodsid'])->first();
-                if(!goodsId){
-                    echo $this->mappingGoodTable.'表中没有找到新旧goods_id的对应关系,旧goodsid为'.$record['goodsid'];
-                    return;
-                }
+//                $goodsId = DB::table($this->mappingGoodTable)->select('new_good_id')
+//                    ->where('old_good_id','=',$record['goodsid'])->first();
+//                if(!goodsId){
+//                    echo $this->mappingGoodTable.'表中没有找到新旧goods_id的对应关系,旧goodsid为'.$record['goodsid'];
+//                    return;
+//                }
+                $goodsId = $record['goodsid']; //todo 调试用
 
                 DB::table($this->table)->insert(
                     [
