e617bd5990c739520ec4995525878afc39bcc7dc
杨洋 <yang_0458@126.com>
2017-03-14 17:11:04 +0800

    no message

--- app/backend/modules/order/observers/OrderObserver.php
+++ app/backend/modules/order/observers/OrderObserver.php
@@ -9,8 +9,13 @@
 namespace app\backend\modules\order\observers;
 
 use app\common\observers\BaseObserver;
+use Illuminate\Database\Eloquent\Model;
 
 class OrderObserver extends BaseObserver
 {
-
+    public function saved(Model $model)
+    {
+        echo '<pre>';print_r(111);exit;
+        $this->pluginObserver('observer.goods',$model,'saved');
+    }
 }
