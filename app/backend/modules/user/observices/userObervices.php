c94093b05f43ce12ba7daf6e1ffcc54637f4f179
yittian <livsyitian@163.com>
2017-03-16 15:25:40 +0800

    拉取代码

---
+++ app/backend/modules/user/observices/userObervices.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: libaojia
+ * Date: 2017/3/16
+ * Time: 下午1:41
+ */
+class TestServices
+{
+
+}
---
+++ resources/views/user/user/form.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: libaojia
+ * Date: 2017/3/15
+ * Time: 上午9:51
+ */
