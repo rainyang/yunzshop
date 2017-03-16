97e37932d42eb8de94a507d4305aef98a839356e
dingran80s <dingran@zongji.in>
2017-03-16 16:35:47 +0800

    模板增加ICON选项

--- resources/views/menu/form.blade.php
+++ resources/views/menu/form.blade.php
@@ -11,6 +11,7 @@
             {!! BootForm::text('menu[name]','* 菜单名称',$model->name) !!}
             {!! BootForm::text('menu[url]','URL路由或链接地址',$model->url,['help_text'=>'填写路由 menu.add 或 http(s)://xxxx']) !!}
             {!! BootForm::text('menu[url_params]','URL参数',$model->url_params) !!}
+            {!! BootForm::text('menu[icon]','ICON',$model->icon) !!}
             {!! BootForm::text('menu[sort]','排序',$model->sort) !!}
             {!! BootForm::radios('menu[permit]','权限控制',[1=>'是',0=>'否'],(int)$model->permit,true) !!}
             {!! BootForm::radios('menu[menu]','菜单显示',[1=>'是',0=>'否'],(int)$model->menu,true) !!}
