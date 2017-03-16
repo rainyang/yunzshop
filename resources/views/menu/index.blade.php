d0134a5c8f0cdcd840d8f1f837c82840fc761b01
janpan <huajian15@gmail.com>
2017-03-16 17:38:57 +0800

    菜单管理修改

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -12,31 +12,29 @@
             <tr>
                 <th>名称</th>
                 <th>标识</th>
-                <th>菜单</th>
-                <th>权限</th>
-                <th>状态</th>
-                <th>操作</th>
+                <th>菜单
+                 权限
+                 状态</th>
+                <th> <span class="pull-right">操作</span></th>
             </tr>
             </thead>
             <tbody>
            @foreach($menuList as $row)
             <tr>
-                <td>{{$row['spacer']}}{{$row['name']}}</td>
+                <td>{!! $row['spacer'] !!}{{$row['name']}}</td>
                 <td>{{$row['item']}}</td>
                 <td>
                     <span class='label label-{{$row['menu'] ? 'success':'default'}}'>{{$row['menu'] ? '启用':'禁用'}}</span>
-                </td>
-                <td>
+
                     <span class='label label-{{$row['permit'] ? 'success':'default'}}'>{{$row['permit'] ? '启用':'禁用'}}</span>
-                </td>
-                <td>
+
                     <span class='label label-{{$row['status'] ? 'success':'default'}}'>{{$row['status'] ? '启用':'禁用'}}</span>
                 </td>
                 <td>
                    <span class="pull-right">
                    <a class='btn btn-default btn-sm' href="{{yzWebUrl('menu.add', array('parent_id'=>$row['id']))}}" title='添加子分类' ><i class="fa fa-plus"></i></a>
 
-                     <a class='btn btn-default btn-sm' href="{{yzWebUrl('menu.edit', array('id' => $row['id']))}}" title="查看" ><i class="fa fa-edit"></i></a>
+                     <a class='btn btn-default btn-sm' href="{{yzWebUrl('menu.edit', array('id' => $row['id']))}}" title="编辑" ><i class="fa fa-edit"></i></a>
                     <a class='btn btn-default btn-sm' href="{{yzWebUrl('menu.del', array('id' => $row['id']))}}" title='删除' onclick="return confirm('确认删除此菜单吗？');return false;"><i class="fa fa-remove"></i></a>
                 </span>
             </tr>
