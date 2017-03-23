b95df370a2feadc8bc44a83c4b48c4ac5ea46835
沈阳 <564345292@qq.com>
2017-03-23 16:14:51 +0800

    订单模板替换为blade

--- app/backend/modules/order/controllers/ListController.php
+++ app/backend/modules/order/controllers/ListController.php
@@ -18,10 +18,11 @@ class ListController extends BaseController
 {
     private $_order_model;
     public function index(){
+
         $params = \YunShop::request();
         $pageSize = 2;
         $this->_order_model = Order::getAllOrders($params['search'],$pageSize);
-        $this->render('order/list', $this->getData());
+        return view('order.index', $this->getData())->render();
 
     }
     public function waitPay()
@@ -75,6 +76,13 @@ class ListController extends BaseController
                 ]
             ]
         ];*/
+        $requestSearch = \YunShop::request()->get('search');
+
+        if ($requestSearch) {
+            $requestSearch = array_filter($requestSearch, function ($item) {
+                return !empty($item);
+            });
+        }
         $list = $this->_order_model;
         $pager = PaginationHelper::show($list['total'], $list['current_page'], $list['per_page']);
         //dd($list);
@@ -84,6 +92,8 @@ class ListController extends BaseController
             'lang' => $this->_lang(),
             'totals' => $this->_totals(),
             'pager' => $pager,
+            'requestSearch' => $requestSearch,
+            'var' => \YunShop::app()->get(),
         ];
         $data += $this->fakeData();
         return $data;
