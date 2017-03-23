e9a506325269c140b4db5fdae4ec85d348ffe206
yittian <livsyitian@163.com>
2017-03-23 10:44:35 +0800

    增加微擎框架关键字数据表rule Model文件

--- app/common/models/frame/Rule.php
+++ app/common/models/frame/Rule.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: libaojia
  * Date: 2017/3/23
- * Time: 上午10:32
+ * Time: 上午10:41
  */
 
 namespace app\common\frame;
@@ -11,14 +11,13 @@ namespace app\common\frame;
 
 use app\common\models\BaseModel;
 
-class RuleKeyword extends BaseModel
+class Rule extends BaseModel
 {
-    public $table = 'rule_keyword';
+    public $table = 'rule';
 
     public $timestamps = false;
 
     protected $guarded = [''];
-
 
 
 }
