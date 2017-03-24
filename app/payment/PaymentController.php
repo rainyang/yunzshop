c62fe2a0c523d2430687eb371ff02607a75439a6
dingran80s <dingran@zongji.in>
2017-03-24 21:31:53 +0800

    修改支付宝退款功能

--- app/payment/PaymentController.php
+++ app/payment/PaymentController.php
@@ -18,17 +18,41 @@ class PaymentController extends BaseController
 
        // file_put_contents('../../../../addons/sz_yi/data/p2.log', print_r($_SERVER['SCRIPT_NAME'],1));
 
-        $script_name = $_SERVER['SCRIPT_NAME'];
+        $script_info = pathinfo($_SERVER['SCRIPT_NAME']);
 
-echo '<pre>';print_r(pathinfo($_SERVER['SCRIPT_NAME']));exit;
+        if (!empty($script_info)) {
+            switch ($script_info['filename']) {
+                case 'notifyUrl':
+                case 'retrunUrl':
+                    $body = !empty($_REQUEST['body']) ? $_REQUEST['body'] : '';
+                    $splits = explode(':', $body);
+                    break;
+                case 'refundNotifyUrl':
+                    $detail_data = !empty($_REQUEST['detail_data']) ? $_REQUEST['detail_data'] : '';
+                    $data = explode('^', $detail_data);
 
-        $body = !empty($_REQUEST['body']) ? $_REQUEST['body'] : '';
-        $splits = explode(':', $body);
+                    if (!empty($data[2])) {
+                        $splits = explode('-', $data[2]);
+                    }
+                    break;
+                case 'withdrawNotifyUrl':
+                    $detail_data = !empty($_REQUEST['detail_data']) ? $_REQUEST['detail_data'] : '';
+                    $data = explode('^', $detail_data);
 
-        if (!empty($splits[1])) {
-            \YunShop::app()->uniacid = intval($splits[1]);
-        } else {
-            \YunShop::app()->uniacid = 0;
+                    if (!empty($data[4])) {
+                        $splits = explode('-', $data[4]);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            file_put_contents('../../../../addons/sz_yi/data/p2.log', print_r($splits,1));
+            if (!empty($splits[1])) {
+                \YunShop::app()->uniacid = intval($splits[1]);
+            } else {
+                \YunShop::app()->uniacid = 0;
+            }
+
         }
     }
 }
