1b4f99f6b387bccb82bd18322b2bdecb3fc498c9
Rui <steveruiyang@163.com>
2017-03-24 11:46:38 +0800

    添加默认首页接口

--- app/common/models/MemberCoupon.php
+++ app/common/models/MemberCoupon.php
@@ -10,6 +10,13 @@ class MemberCoupon extends BaseModel
 
     public function belongsToCoupon()
     {
-        $this->belongsTo('app\common\models\Coupon');
+        return $this->belongsTo('app\common\models\Coupon', 'coupon_id', 'id');
+    }
+
+    public static function getMemberCoupon($MemberModel)
+    {
+        return static::with(['belongsToCoupon' => function ($query) {
+            return $query->where('status', 0);
+        }])->where('member_id', $MemberModel->uid)->where('used', 0);
     }
 }
