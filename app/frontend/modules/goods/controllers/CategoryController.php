48d13c29dbf86cad63f85f7d88f78c317a6571c1
杨雷 <heiangdao@sina.com>
2017-03-25 10:34:10 +0800

    修改分类接口

--- app/frontend/modules/goods/controllers/CategoryController.php
+++ app/frontend/modules/goods/controllers/CategoryController.php
@@ -25,9 +25,11 @@ class CategoryController extends BaseController
 {
     public function getCategory()
     {
+        $set = Setting::get('shop.category');
         $pageSize = 10;
         $parent_id = \YunShop::request()->parent_id ? \YunShop::request()->parent_id : '0';
         $list = Category::getCategorys($parent_id)->paginate($pageSize)->toArray();
+        $list['set'] = $set;
         if($list['data']){
             return $this->successJson('获取分类数据成功!', $list);
         }
@@ -37,20 +39,22 @@ class CategoryController extends BaseController
     public function getChildrenCategory()
     {
         //$pageSize = 10;
+        $set = Setting::get('shop.category');
         $parent_id = intval(\YunShop::request()->parent_id);
-        $list = Category::getChildrenCategorys($parent_id)->get()->toArray();//->paginate($pageSize)
+        $list = Category::getChildrenCategorys($parent_id,$set)->get()->toArray();//->paginate($pageSize)
+        
         if($list){
             return $this->successJson('获取子分类数据成功!', $list);
         }
         return $this->errorJson('未检测到子分类数据!',$list);
     }
 
-    public function getCategorySetting()
-    {
-        $set = Setting::get('shop.category');
-        if($set){
-            return $this->successJson('获取分类设置数据成功!', $set);
-        }
-        return $this->errorJson('未检测到分类设置数据!',$set);
-    }
+//    public function getCategorySetting()
+//    {
+//        $set = Setting::get('shop.category');
+//        if($set){
+//            return $this->successJson('获取分类设置数据成功!', $set);
+//        }
+//        return $this->errorJson('未检测到分类设置数据!',$set);
+//    }
 }
