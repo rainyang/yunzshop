f4016263dc33329e8df42f309eede2e6efb24165
dingran80s <dingran@zongji.in>
2017-03-26 08:32:05 +0800

    微信登陆调试

--- resources/views/order/pay.blade.php
+++ resources/views/order/pay.blade.php
@@ -7,7 +7,7 @@
 
     <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
     <script type="text/javascript" charset="utf-8">
-        wx.config({{ $js->config(array('chooseWXPay')) }});
+        wx.config({!! $js->config(array('chooseWXPay')) !!});
     </script>
     <script>
         $(function(){
