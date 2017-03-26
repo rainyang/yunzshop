37dffac31ce33b93e68c2e82de415293791948e0
dingran80s <dingran@zongji.in>
2017-03-26 09:06:48 +0800

    微信登陆调试

--- app/common/services/WechatPay.php
+++ app/common/services/WechatPay.php
@@ -47,7 +47,7 @@ class WechatPay extends Pay
         $js = $app->js;
         $js = json_decode($js->config(array('chooseWXPay')),1);
         echo '<pre>';print_r($js );
-//unset($js['beta']);
+unset($js['beta']);
         echo '<pre>';print_r($js );exit;
         return ['config'=>$config, 'js'=>$js];
     }
