b11a7b5fff13ae3da46780ff78e2b4be1f55ed29
dingran80s <dingran@zongji.in>
2017-03-26 12:37:21 +0800

    微信登陆调试

--- app/frontend/modules/member/controllers/TestController.php
+++ app/frontend/modules/member/controllers/TestController.php
@@ -20,9 +20,9 @@ class TestController extends BaseController
    public function index()
    {
        $pay = new WechatPay();
-       $result = $pay->doRefund('1490501934', '4001322001201703264692073547', '0.1');
-       echo '<pre>';print_r($result);exit;
-       $data = $pay->doPay(['order_no'=>time(),'amount'=>0.1, 'subject'=>'微信支付', 'body'=>'测试:2', 'extra'=>'']);
+      // $result = $pay->doRefund('1490501934', '4001322001201703264692073547', '0.1');
+      // echo '<pre>';print_r($result);exit;
+       $data = $pay->doPay(['order_no'=>time(),'amount'=>1, 'subject'=>'微信支付', 'body'=>'测试:2', 'extra'=>'']);
 
        return view('order.pay', [
            'config' => $data['config'],
