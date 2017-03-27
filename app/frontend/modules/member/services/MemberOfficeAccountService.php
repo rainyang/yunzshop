9e1abf63fb067dc0b606cb3bca2d4601b61fb1d9
dingran80s <dingran@zongji.in>
2017-03-27 16:14:08 +0800

    增加默认会员id

--- app/frontend/modules/member/services/MemberOfficeAccountService.php
+++ app/frontend/modules/member/services/MemberOfficeAccountService.php
@@ -162,7 +162,6 @@ class MemberOfficeAccountService extends MemberService
 
                 session(['member_id'=>$member_id]);
                 \Session::save();
-                $this->save(['uid'=>$member_id, 'realname'=>$userinfo['nickname']], $uniacid);
             } else {
                 redirect($authurl)->send();
                 exit;
