0e1da08dc761853df751314e188eab9ef4f84157
沈阳 <564345292@qq.com>
2017-03-27 22:36:17 +0800

    支付测试

--- app/frontend/modules/order/controllers/TestPayController.php
+++ app/frontend/modules/order/controllers/TestPayController.php
@@ -19,21 +19,17 @@ class TestPayController extends BaseController
 
     public function index()
     {
-        $pay = new WechatPay();
-//       $str  = $pay->setUniacidNo(122, 5);
-//       echo $str . '<BR>';
-//       echo substr($str, 17, 5);
-        // $pay->doWithdraw(123, time(), 0.1);
-        //$result = $pay->doRefund('1490503054', '4001322001201703264702511714', 1, 1);
-
-        $data = $pay->doPay(['order_no'=>time(),'amount'=>0.1, 'subject'=>'微信支付', 'body'=>'测试:2', 'extra'=>'']);
-
-        return view('order.pay', [
-            'config' => $data['config'],
-            'js' => $data['js']
-        ])->render();
-
-        exit;
+        //$param =  ['order_no' => $_POST['order_no'], 'amount' => $_POST['amount'], 'subject' => $_POST['subject'], 'body' => $_POST['body'], 'extra' => $_POST['extra']];
+        $param = [
+            'order_no' => time(),
+            'amount' => 0.1,
+            'subject' => '微信支付',
+            'body' => '商品的描述:2',
+            'extra' => ['type'=>1]
+        ];
+        $pay = PayFactory::create(PayFactory::PAY_WEACHAT);
+        $data = $pay->doPay($param);
+        return $this->successJson('成功',$data);
     }
     public function test(){
         define('IS_TEST',true);
