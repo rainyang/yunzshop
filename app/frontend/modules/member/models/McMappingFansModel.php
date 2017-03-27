83148fa7b29e81f0647256027abb986661bf8a97
dingran80s <dingran@zongji.in>
2017-03-27 19:59:45 +0800

    微信登陆调试

--- app/frontend/modules/member/models/McMappingFansModel.php
+++ app/frontend/modules/member/models/McMappingFansModel.php
@@ -22,7 +22,7 @@ class McMappingFansModel extends BackendModel
 
     protected $fillable = ['openid','uid','acid','uniacid', 'salt', 'updatetime', 'nickname', 'follow', 'followtime', 'unfollowtime', 'tag'];
 
-    protected $attributes = ['unionid'=>''];
+    protected $attributes = ['unionid'=>'', 'groupid'=>''];
 
 
     public function getOauthUserInfo()
