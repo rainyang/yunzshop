3a154e3b7213be2c8f2112f72497c69c2e194cdc
LuckyStar.D <18646546800@163.com>
2017-03-28 14:33:00 +0800

    商品bug

--- resources/views/goods/goods.blade.php
+++ resources/views/goods/goods.blade.php
@@ -1,6 +1,5 @@
 ﻿@extends('layouts.base')
 
-@section('js')
     <script type="text/javascript" src="./resource/js/lib/jquery-ui-1.10.3.min.js"></script>
 
     <script type="text/javascript">
@@ -536,7 +535,6 @@
         }
 
     </script>
-@stop
 
 @section('content')
 
@@ -553,53 +551,7 @@
 
     <form action="" method="post" class="form-horizontal form" enctype="multipart/form-data">
         <div class="panel-default panel-center">
-<<<<<<< HEAD
-            <!--             <div class="panel-heading">
-                            {if empty($goods['id'])}添加商品{else}编辑商品{/if}
-                        </div> -->
-                                                                    `
-            <div>
-                <ul class="add-shopnav" id="myTab">
-                    <li class="active"><a href="#tab_basic">基本信息</a></li>
-                    <li><a href="#tab_des">{{$lang['shopdesc']}}</a></li>
-                    <li><a href="#tab_param">属性</a></li>
-                    <li><a href="#tab_option">{{$lang['shopoption']}}</a></li>
-                    @foreach(Config::get('widget.goods') as $key=>$value)
-                        <li><a href="#{{$key}}">{{$value['title']}}</a></li>
-                    @endforeach
 
-                </ul>
-            </div>
-            <div style="padding-top:50px">
-                <div class="panel-body">
-                    <div class="tab-content">
-                        <div class="tab-pane  active" id="tab_basic">@include('goods.basic')</div>
-                        <div class="tab-pane" id="tab_des">@include('goods.des')</div>
-                        <div class="tab-pane" id="tab_param">@include('goods.tpl.param')</div>
-                        <div class="tab-pane" id="tab_option">@include('goods.tpl.option')</div>
-                        @foreach(Config::get('widget.goods') as $key=>$value)
-                            <div class="tab-pane"
-                                 id="{{$key}}">{!! widget($value['class'], ['goods_id'=> $goods->id])!!}</div>
-                        @endforeach
-
-                    </div>
-                    <div class="form-group col-sm-12 mrleft40 border-t" style="text-align: right;">
-                        <input type="submit" name="submit" value="{{$lang['shopsubmit']}}"
-                               class="btn btn-primary col-lg-1" onclick="return formcheck()"
-                               style="float: right;margin-left: 8px;"/>
-                        <input type="hidden" name="token" value="{{$var['token']}}"/>
-                        <input type="button" name="back" style='margin-left:10px;' value="返回列表"
-                               class="btn btn-default"/>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
-    {{--</div>--}}
-
-@endsection('content')
-=======
 <!--             <div class="panel-heading">
                 {if empty($goods['id'])}添加商品{else}编辑商品{/if}
             </div> -->
@@ -644,4 +596,3 @@
 {{--</div>--}}
 
 	@endsection('content')
->>>>>>> b2989fd2c62b73a1138008cf92a98ab28943ef04
