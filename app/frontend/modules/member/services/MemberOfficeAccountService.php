b9250b23a8390449fa076e497a9421879fb31c87
dingran80s <dingran@zongji.in>
2017-03-29 22:19:45 +0800

    登陆调试

--- app/frontend/modules/member/services/MemberOfficeAccountService.php
+++ app/frontend/modules/member/services/MemberOfficeAccountService.php
@@ -25,7 +25,7 @@ class MemberOfficeAccountService extends MemberService
     {}
 
     public function login()
-    {echo 'end';exit;
+    {
         $uniacid      = \YunShop::app()->uniacid;
         $code         = \YunShop::request()->code;
         $mid          = \YunShop::app()->uniacid ? \YunShop::app()->uniacid : 0;
