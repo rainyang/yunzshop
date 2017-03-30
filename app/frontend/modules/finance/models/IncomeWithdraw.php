48107c2ce9b34c7e241c8142ff1471a1d8db10fa
杨雷 <heiangdao@sina.com>
2017-03-30 10:38:15 +0800

    收入提现

---
+++ app/frontend/modules/finance/models/IncomeWithdraw.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\frontend\modules\finance\models;
+/**
+ * Created by PhpStorm.
+ * User: yanglei
+ * Date: 2017/3/30
+ * Time: 上午9:40
+ */
+
+class IncomeWithdraw extends \app\common\models\IncomeWithdraw
+{
+    
+}
