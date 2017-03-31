26fa43f0cb3cbefad534aa66b301ebe0088cd047
yittian <livsyitian@163.com>
2017-03-31 09:36:35 +0800

    拉取代码

--- app/backend/modules/finance/controllers/BalanceController.php
+++ app/backend/modules/finance/controllers/BalanceController.php
@@ -10,27 +10,39 @@ namespace app\backend\modules\finance\controllers;
 
 
 use app\common\components\BaseController;
+use app\common\facades\Setting;
+use app\common\helpers\Url;
 
 class BalanceController extends BaseController
 {
     //余额基础设置页面
     public function index()
     {
-        $balance = array(
-            'transfer' => 0,
-            'recharge' => 0,
-            'withdraw' => array('status' => 0)
-        );
+        $balance = Setting::get('balance.recharge');
+
+        $requestModel = \YunShop::request()->balance;
+        if ($requestModel) {
+            //dd($requestModel);
+            $requestModel[''] = '';
+            if (Setting::set('balance.recharge', $requestModel)) {
+                return $this->message('余额基础设置保存成功', Url::absoluteWeb('finance.balance.index'));
+            } else {
+                $this->error('余额基础设置保存失败！！');
+            }
+        }
+
+
         return view('finance.balance.index', [
             'balance' => $balance,
             'pager' => ''
         ])->render();
     }
 
-    //保存设置
-    public function save()
+    //用户余额管理
+    public function member()
     {
-        echo '<pre>'; print_r(\YunShop::request()->balance); exit;
+        
     }
+
 
 }
