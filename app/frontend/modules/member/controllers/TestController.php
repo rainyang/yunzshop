b5096cc48579ef9c68803aeff5b5c70373c8bf62
dingran80s <dingran@zongji.in>
2017-03-31 20:51:17 +0800

    微信支付

--- app/frontend/modules/member/controllers/TestController.php
+++ app/frontend/modules/member/controllers/TestController.php
@@ -27,7 +27,7 @@ class TestController extends BaseController
 //       $str  = $pay->setUniacidNo(122, 5);
 //       echo $str . '<BR>';
 //       echo substr($str, 17, 5);
-       $pay->doWithdraw(146,  0.1);
+//       $pay->doWithdraw(146,  0.1);
 //       $result = $pay->doRefund('1490503054', '4001322001201703264702511714', 1, 1);
 //
 //       $data = $pay->doPay(['order_no'=>time(),'amount'=>0.1, 'subject'=>'微信支付', 'body'=>'测试:2', 'extra'=>['type'=>1]]);
