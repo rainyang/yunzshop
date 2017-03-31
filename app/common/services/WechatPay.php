c56393bad5c1594630de2be3c474998f9bbbe139
dingran80s <dingran@zongji.in>
2017-03-31 16:10:11 +0800

    修改微信支付

--- app/common/services/WechatPay.php
+++ app/common/services/WechatPay.php
@@ -43,7 +43,7 @@ class WechatPay extends Pay
 
             $this->changeOrderStatus($pay_order_model, Pay::ORDER_STATUS_WAITPAY);
         } else {
-            throw new \AppException('微信预下单失败');
+            throw new AppException('微信预下单失败');
         }
 
         $config = $payment->configForJSSDKPayment($prepayId);
