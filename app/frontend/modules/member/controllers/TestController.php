f8ba97d1b4cd72e0a9ebbe76d7d4de05c32143c7
dingran80s <dingran@zongji.in>
2017-04-02 13:01:07 +0800

    微信提现调试

--- app/frontend/modules/member/controllers/TestController.php
+++ app/frontend/modules/member/controllers/TestController.php
@@ -28,12 +28,12 @@ class TestController extends ApiController
 //       $pay->doWithdraw(146,  0.1);
 //       $result = $pay->doRefund('1490503054', '4001322001201703264702511714', 1, 1);
 //
-//       $data = $pay->doPay(['order_no'=>time(),'amount'=>0.1, 'subject'=>'微信支付', 'body'=>'测试:2', 'extra'=>['type'=>1]]);
+      $data = $pay->doPay(['order_no'=>time(),'amount'=>1, 'subject'=>'微信支付', 'body'=>'测试:2', 'extra'=>['type'=>1]]);
 
-//       return view('order.pay', [
-//           'config' => $data['config'],
-//           'js' => $data['js']
-//       ])->render();
+       return view('order.pay', [
+           'config' => $data['config'],
+           'js' => $data['js']
+       ])->render();
 exit;
        $pay = new AliPay();
 
