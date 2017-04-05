43fefa1ca44c57bc21ad4cf97450f4a3b47c63c2
dingran80s <dingran@zongji.in>
2017-04-05 20:32:07 +0800

    微信登陆调试

--- app/common/components/ApiController.php
+++ app/common/components/ApiController.php
@@ -66,10 +66,11 @@ echo $session_id . '<BR>';
         session_save_path('/tmp');
         file_put_contents(storage_path('logs/ssid.log'), print_r(['ssid'=>$session_id, 'path'=>session_save_path('/tmp')],1), FILE_APPEND);
         session_id($session_id);
-
+file_put_contents(storage_path('logs/ss.log'), print_r($_SESSION, 1));
         session_start();
  echo        session_id();
         file_put_contents(storage_path('logs/ssid2.log'), print_r(['ssid'=>session_id(), 'path'=>session_save_path('/tmp')],1), FILE_APPEND);
+        file_put_contents(storage_path('logs/ss2.log'), print_r($_SESSION, 1));
        // echo '<pre>';print_r($_SESSION);exit;
     }
 
