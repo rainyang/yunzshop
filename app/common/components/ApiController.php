5e5f1604f19757cdd0c67b9c870aab12e6d86036
dingran80s <dingran@zongji.in>
2017-04-07 10:23:36 +0800

    打印路径

--- app/common/components/ApiController.php
+++ app/common/components/ApiController.php
@@ -64,7 +64,7 @@ class ApiController extends BaseController
         session_save_path('/tmp');
        // session_id($session_id);
         session_start();
-        Log::debug('apiController: path : '. $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+        \Log::debug('apiController: path : '. $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
         \Log::debug('apiController: setCookie session_start : '. session_id());
         \Log::debug('apicontroller: printCookie result : ' . print_r($_SESSION, 1));
     }
