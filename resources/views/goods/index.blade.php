eb7f7017255564ffaa9f68bd43befdbe711bcaff
杨雷 <heiangdao@sina.com>
2017-04-11 21:38:05 +0800

    修改商品

--- resources/views/goods/index.blade.php
+++ resources/views/goods/index.blade.php
@@ -1,5 +1,4 @@
 ﻿@extends('layouts.base')
-
 @section('content')
 
     <div class="w1200 ">
@@ -162,7 +161,7 @@
                                             <img src="{{tomedia($item['thumb'])}}"
                                                  style="width:40px;height:40px;padding:1px;border:1px solid #ccc;"/>
                                         </td>
-                                        <td title="{$item['title']}" class='tdedit' width="26%">
+                                        <td title="{{$item['title']}}" class='tdedit' width="26%">
                                             <span class=' fa-edit-item' style='cursor:pointer'><i class='fa fa-pencil'
                                                                                                   style="display:none"></i> <span
                                                         class="title">{{$item['title']}}</span> </span>
