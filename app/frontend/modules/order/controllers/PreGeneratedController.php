bacb8e4e6087730e3c9330344b53f01105ae8af3
沈阳 <564345292@qq.com>
2017-04-11 15:31:31 +0800

    分单bug修改

--- app/frontend/modules/order/controllers/PreGeneratedController.php
+++ app/frontend/modules/order/controllers/PreGeneratedController.php
@@ -10,6 +10,7 @@ namespace app\frontend\modules\order\controllers;
 
 use app\common\components\ApiController;
 use app\common\events\order\ShowPreGenerateOrder;
+use app\frontend\modules\member\services\MemberCartService;
 use app\frontend\modules\order\services\OrderService;
 
 abstract class PreGeneratedController extends ApiController
@@ -24,8 +25,7 @@ abstract class PreGeneratedController extends ApiController
         $total_dispatch_price = $order_data->sum('order.dispatch_price');
 
         $data['dispatch'] = $order_data[0]['dispatch'];
-        $order_data->map(function ($order_data)
-        {
+        $order_data->map(function ($order_data) {
             return $order_data->forget('dispatch');
         });
 
@@ -33,19 +33,54 @@ abstract class PreGeneratedController extends ApiController
         return $this->successJson('成功', $data);
 
     }
-    protected function getOrderData(){
+
+    /**
+     * 获取订单数据组
+     * @return \Illuminate\Support\Collection|static
+     */
+    protected function getOrderData()
+    {
         $order_data = collect();
-        $order_data->push(OrderService::getOrderData($this->getShopOrder()));
+        $shop_order = $this->getShopOrder();
+
+        if(!empty($shop_order)){
+            $order_data->push(OrderService::getOrderData($shop_order));
+        }
 
         $order_data = $order_data->merge($this->getPluginOrderData()[0]);
+
         return $order_data;
     }
+
+    /**
+     * 获取全部购物车记录
+     * @return mixed
+     */
     abstract protected function getMemberCarts();
+
+    /**
+     * 获取商城的订单
+     * @return \app\frontend\modules\order\services\models\PreGeneratedOrderModel
+     */
     private function getShopOrder()
     {
-        return OrderService::createOrderByMemberCarts($this->getMemberCarts());
+
+        return OrderService::createOrderByMemberCarts($this->getShopMemberCarts());
     }
 
+    /**
+     * 获取商城的购物车组
+     * @return static
+     */
+    protected function getShopMemberCarts()
+    {
+        return MemberCartService::filterShopMemberCart($this->getMemberCarts());
+    }
+
+    /**
+     * 获取插件的订单数据
+     * @return array
+     */
     private function getPluginOrderData()
     {
         $event = new ShowPreGenerateOrder($this->getMemberCarts());
