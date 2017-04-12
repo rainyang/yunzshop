b1934371a2559a9d39eb23590bdda7779a351f04
janpan <huajian15@gmail.com>
2017-04-12 15:33:42 +0800

    修改后台菜单默认icon

--- resources/views/layouts/mainSidebar.blade.php
+++ resources/views/layouts/mainSidebar.blade.php
@@ -15,7 +15,7 @@
                 @if(isset($value['menu']) && $value['menu'] == 1 && can($key))
                     @if(isset($value['child']) && array_child_kv_exists($value['child'],'menu',1))
                         <li class="treeview {{in_array($key,Yunshop::$currentItems) ? 'active' : ''}}">
-                            <a href="javascript:void(0);" ><i class="fa {{$value['icon'] or 'fa-circle-o'}}"></i> <span>{{$value['name'] or ''}}</span>
+                            <a href="javascript:void(0);" ><i class="fa {{array_get($value,'icon','fa-circle-o') ?: 'fa-circle-o'}}"></i> <span>{{$value['name'] or ''}}</span>
                             <span class="pull-right-container"><i
                                         class="fa fa-angle-left pull-right"></i>
                             </span>
@@ -24,7 +24,7 @@
                         </li>
                     @else
                         <li class="{{in_array($key,Yunshop::$currentItems) ? 'active' : ''}}"><a href="{{isset($value['url']) ? yzWebFullUrl($value['url']):''}}">
-                                <i class="fa {{$value['icon'] or 'fa-circle-o'}}"></i> {{$value['name'] or ''}}
+                                <i class="fa {{array_get($value,'icon','fa-circle-o') ?: 'fa-circle-o'}}"></i> {{$value['name'] or ''}}
                             </a>
                         </li>
                     @endif
