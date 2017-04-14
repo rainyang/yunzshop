59bd9453ab402fba5ffc213ae50ab25b739249a3
yittian <livsyitian@163.com>
2017-04-14 21:04:41 +0800

    完成余额提现功能

--- app/backend/modules/finance/controllers/BalanceWithdrawController.php
+++ app/backend/modules/finance/controllers/BalanceWithdrawController.php
@@ -10,8 +10,10 @@ namespace app\backend\modules\finance\controllers;
 
 
 use app\backend\modules\finance\models\Withdraw;
+use app\backend\modules\finance\services\WithdrawService;
 use app\common\components\BaseController;
 use app\common\facades\Setting;
+use Illuminate\Support\Facades\Log;
 
 class BalanceWithdrawController extends BaseController
 {
@@ -24,14 +26,10 @@ class BalanceWithdrawController extends BaseController
 
     public function detail()
     {
-        //$this->withdrawMath();
-
-
-
-
         $this->attachedMode();
-
-        echo '<pre>'; print_r($this->getExamine()); exit;
+        if (!$this->withdrawModel) {
+            return $this->message('数据错误，请刷新重试！');
+        }
 
         return view('finance.balance.withdraw', [
             'item' => $this->withdrawModel->toArray(),
@@ -39,9 +37,80 @@ class BalanceWithdrawController extends BaseController
         ])->render();
     }
 
-    public function updateStatus()
+    public function examine()
     {
+        $this->attachedMode();
+        if (!$this->withdrawModel) {
+            return $this->message('数据错误，请刷新重试！');
+        }
+        $requestData = \YunShop::request();
+        if (isset($requestData['submit_check']) || isset($requestData['submit_cancel'])) {
+            //审核--重新审核
+            $result = $this->submitCheck();
+            if ($result === true) {
+                return $this->message('提交审核成功', yzWebUrl("finance.balance-withdraw.detail", ['id' => $requestData['id']]));
+            }
+            return $this->message($result, yzWebUrl("finance.balance-withdraw.detail", ['id' => $requestData['id']]), 'error');
+        } elseif (isset($requestData['submit_pay'])) {
+            //打款
+            if ($this->withdrawModel->status !== '1') {
+                return ['msg' => '打款失败,数据不存在或不符合打款规则!'];
+            }
+            $result = $this->submitPay();
+            if ($result === true) {
+               return $this->message('提交审核成功', yzWebUrl("finance.balance-withdraw.detail", ['id' => $requestData['id']]));
+            }
+            return $this->message($result, yzWebUrl("finance.balance-withdraw.detail", ['id' => $requestData['id']]), 'error');
+        }
+        return $this->message('提交数据有误，请刷新重试', yzWebUrl("finance.balance-withdraw.detail", ['id' => $requestData['id']]));
+    }
 
+    //提交审核
+    private function submitCheck()
+    {
+        $this->attachedExamineData();
+        $this->withdrawModel->status = \YunShop::request()->status;
+        return $this->withdrawUpdate();
+    }
+
+    //打款
+    private function submitPay()
+    {
+        $resultPay = '';
+        $remark = '提现打款-' . $this->withdrawModel->type_name . '-金额:' . $this->withdrawModel->actual_amounts . '元,' .
+            '手续费:' . $this->withdrawModel->actual_poundage;
+        if ($this->withdrawModel->pay_way == '2') {
+            //支付宝打款
+            $resultPay = WithdrawService::alipayWithdrawPay($this->withdrawModel, $remark);
+            Log::info('MemberId:' . $this->withdrawModel->member_id . ', ' . $remark . "支付宝打款中!");
+        } elseif ($this->withdrawModel->pay_way == '1') {
+            //微信打款
+            $resultPay = WithdrawService::wechtWithdrawPay($this->withdrawModel, $remark);
+            Log::info('MemberId:' . $this->withdrawModel->member_id . ', ' . $remark . "微信打款中!");
+        }
+        return $resultPay ? $this->updatePayTime(): "打款失败";
+    }
+
+    //保存数据
+    private function withdrawUpdate()
+    {
+        return $this->withdrawModel->save() ?: '数据修改失败，请刷新重试';
+    }
+
+    //修改打款时间
+    private function updatePayTime()
+    {
+        $this->withdrawModel->pay_at = time();
+        return $this->withdrawUpdate();
+    }
+
+    //附值审核数据
+    private function attachedExamineData()
+    {
+        $examine = $this->getExamine();
+        $this->withdrawModel->actual_poundage = $examine['poundage'];
+        $this->withdrawModel->actual_amounts  = $examine['result_money'];
+        $this->withdrawModel->audit_at        = time();
     }
 
     //获取去提现手续费设置
@@ -58,29 +127,21 @@ class BalanceWithdrawController extends BaseController
         return round(floatval($this->withdrawModel->amounts * $this->withdrawPoundage), 2);
     }
 
+    //审核金额运算数据、结果
     private function getExamine()
     {
         return array(
             'examine_money' => $this->withdrawModel->amounts,
             'poundage'      => $this->withdrawPoundageMath(),
             'result_money'  => $this->withdrawModel->amounts - $this->withdrawPoundageMath()
-
         );
     }
 
-    private function withdrawShackles()
-    {
-
-    }
-
-
+    //附值 model
     private function attachedMode()
     {
-        $this->withdrawModel = Withdraw::getBalanceWithdrawById(\YunShop::request()->id) ?: new Withdraw();
+        $this->withdrawModel = Withdraw::getBalanceWithdrawById(\YunShop::request()->id);
     }
 
-    private function getData()
-    {
 
-    }
 }
