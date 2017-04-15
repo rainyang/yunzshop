c232371e1fb53bc9c6ca8af3e9c823f34c3ec6bf
yittian <livsyitian@163.com>
2017-04-15 17:17:45 +0800

    test

--- app/frontend/modules/member/controllers/MemberAddressController.php
+++ app/frontend/modules/member/controllers/MemberAddressController.php
@@ -51,7 +51,7 @@ class MemberAddressController extends ApiController
      * 修改默认收货地址
      *
      * */
-    public function test()
+    public function setDefault()
     {
         echo '<pre>'; print_r(123); exit;
         $memberId = \YunShop::app()->getMemberId();
