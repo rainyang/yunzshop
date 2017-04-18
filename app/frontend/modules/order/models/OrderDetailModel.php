a779b8b433491f396fdb116e8e6fc14f654c6f0b
沈阳 <564345292@qq.com>
2017-04-18 15:14:53 +0800

    订单详情运费,抵扣 金额显示

--- app/frontend/modules/order/models/OrderDetailModel.php
+++ app/frontend/modules/order/models/OrderDetailModel.php
@@ -16,7 +16,7 @@ class OrderDetailModel extends Order
     {
         $orderModels = self::with(['hasManyOrderGoods'=>function($query){
             return $query->select(['order_id','goods_option_title','goods_id','goods_price','total','price','title','thumb','comment_status']);
-        }])->select(['id','uid','order_sn','price','goods_price','create_time','finish_time','pay_time','send_time','cancel_time','dispatch_type_id','pay_type_id','status','refund_id'])->find($orderId);
+        }])->select(['id','uid','order_sn','price','goods_price','create_time','finish_time','pay_time','send_time','cancel_time','dispatch_type_id','pay_type_id','status','refund_id','dispatch_price','deduction_price'])->find($orderId);
         return $orderModels;
     }
 }
