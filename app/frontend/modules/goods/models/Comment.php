354eba7afdd78b97710afae729f6202112894769
杨雷 <heiangdao@sina.com>
2017-04-18 20:14:50 +0800

    no message

--- app/frontend/modules/goods/models/Comment.php
+++ app/frontend/modules/goods/models/Comment.php
@@ -21,7 +21,7 @@ class Comment extends \app\common\models\Comment
                 return $query->select(['id','comment_id', 'order_id', 'goods_id', 'uid',
                     'nick_name', 'head_img_url', 'content', 'level',
                     'images', 'created_at', 'reply_id', 'reply_name'])
-                    ->orderBy('created_at', 'desc');
+                    ->orderBy('created_at', 'asc');
             }])
             ->where('goods_id', $goods_id)
             ->where('comment_id', 0)
