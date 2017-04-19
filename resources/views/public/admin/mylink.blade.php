e97b686addb39b15abec656cbb5c72dd55bab631
yittian <livsyitian@163.com>
2017-04-19 15:09:01 +0800

    完善会员等级验证

--- resources/views/public/admin/mylink.blade.php
+++ resources/views/public/admin/mylink.blade.php
@@ -27,13 +27,13 @@
                         </div>
                         <div id="fe-tab-link-li-21" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 21)" data-href="{{ yzAppUrl('member') }}">会员中心</div>
                         <div id="fe-tab-link-li-22" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 22)" data-href="{{ yzAppUrl('member.orderList') }}">我的订单</div>
-                        <div id="fe-tab-link-li-23" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 23)" data-href="{{ yzAppUrl('cart') }}">我的购物车</div>
-                        <div id="fe-tab-link-li-24" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 24)" data-href="{{ yzAppUrl('member.collection') }}">我的收藏</div>
-                        <div id="fe-tab-link-li-25" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 25)" data-href="{{ yzAppUrl('member.footprint') }}">我的足迹</div>
-                        <div id="fe-tab-link-li-26" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 26)" data-href="{{ yzAppUrl('member.balance_recharge') }}">会员充值</div>
-                        <div id="fe-tab-link-li-27" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 27)" data-href="{{ yzAppUrl('member.details') }}">余额明细</div>
-                        <div id="fe-tab-link-li-28" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 28)" data-href="{{ yzAppUrl('member.balance_withdrawal') }}">余额提现</div>
-                        <div id="fe-tab-link-li-29" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 29)" data-href="{{ yzAppUrl('member.address') }}">我的收货地址</div>
+                        <div id="fe-tab-link-li-23" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 23)" data-href="{{ yzApiUrl('cart') }}">我的购物车</div>
+                        <div id="fe-tab-link-li-24" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 24)" data-href="{{ yzApiUrl('member.collection') }}">我的收藏</div>
+                        <div id="fe-tab-link-li-25" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 25)" data-href="{{ yzApiUrl('member.footprint') }}">我的足迹</div>
+                        <div id="fe-tab-link-li-26" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 26)" data-href="{{ yzApiUrl('member.balance_recharge') }}">会员充值</div>
+                        <div id="fe-tab-link-li-27" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 27)" data-href="{{ yzApiUrl('member.details') }}">余额明细</div>
+                        <div id="fe-tab-link-li-28" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 28)" data-href="{{ yzApiUrl('member.balance_withdrawal') }}">余额提现</div>
+                        <div id="fe-tab-link-li-29" class="btn btn-default mylink-nav" ng-click="chooseLink(1, 29)" data-href="{{ yzApiUrl('member.address') }}">我的收货地址</div>
 
 
 
