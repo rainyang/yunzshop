d7a49116c3b4aa8d63c416828ec33bd7d748a4bd
yittian <livsyitian@163.com>
2017-04-21 14:55:17 +0800

    测试会员详情

--- resources/views/member/detail.blade.php
+++ resources/views/member/detail.blade.php
@@ -45,9 +45,9 @@
                             <label class="col-xs-12 col-sm-3 col-md-2 control-label">会员分组</label>
                             <div class="col-sm-9 col-xs-12">
                                 <select name='data[group_id]' class='form-control'>
-                                    <option value=>无1分组</option>
+                                    <option value='0' selected>无1分组</option>
                                     @foreach($groups as $group)
-                                    <option value='{{$group['id']}}' selected>{{ $group['group_name'] }}1</option>
+                                    <option value='{{$group['id']}}' @if($member['yz_member']['group_id'] == $group['id']) selected @endif >{{ $group['group_name'] }}1</option>
                                     @endforeach
                                 </select>
                             </div>
