4f0d5d892aae107b0e6ba2f68b069f043e6db7c0
沈阳 <564345292@qq.com>
2017-04-21 17:38:48 +0800

    订单退款

---
+++ app/backend/modules/refund/models/type/RefundType.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shenyang
+ * Date: 2017/4/21
+ * Time: 下午4:54
+ */
+
+namespace app\backend\modules\refund\models;
+
+
+abstract class RefundType
+{
+    /**
+     * @var $refundApply RefundApply
+     */
+    protected $refundApply;
+    public function __construct($refundApply)
+    {
+        $this->refundApply = $refundApply;
+    }
+    public function reject($data)
+    {
+        $this->refundApply->status = RefundApply::REJECT;
+        $this->refundApply->reject_reason = $data['reject_reason'];
+        return $this->refundApply->save();
+    }
+    public function consensus($data)
+    {
+        $this->refundApply->status = RefundApply::CONSENSUS;
+        return $this->refundApply->save();
+    }
+}
