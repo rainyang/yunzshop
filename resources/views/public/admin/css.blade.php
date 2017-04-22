888a568ba32be43919e961f9c631f4be2d33fffa
janpan <huajian15@gmail.com>
2017-04-22 15:07:56 +0800

    删除无效文件与目录

--- resources/views/public/admin/css.blade.php
+++ resources/views/public/admin/css.blade.php
@@ -2,8 +2,7 @@
 <link href="{{static_url('resource/css/bootstrap-theme.min.css')}}" rel="stylesheet">
 <link href="{{static_url('resource/css/font-awesome.min.css')}}" rel="stylesheet">
 <link href="{{static_url('resource/css/style.min.css')}}" rel="stylesheet">
-<link href="{{static_url('font/iconfont.css')}}" rel="stylesheet">
+<link href="{{static_url('fonts/iconfont.css')}}" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="{{static_url('css/webstyle.css')}}">
-<link rel="stylesheet" type="text/css" href="{{static_url('font/iconfont.css')}}">
 <link rel="stylesheet" type="text/css" href="{{static_url('css/common.css')}}">
 
