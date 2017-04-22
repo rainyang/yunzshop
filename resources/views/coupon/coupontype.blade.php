753432a3da8bbf9a8a9a416db05d425545c7c069
kaizhao <patiencing@outlook.com>
2017-04-22 14:59:47 +0800

    [@优惠券#修补]编辑优惠券时没有正确选中"适用范围"的类型

--- resources/views/coupon/coupontype.blade.php
+++ resources/views/coupon/coupontype.blade.php
@@ -37,7 +37,7 @@
 <div class="form-group">
     <label class="col-xs-12 col-sm-3 col-md-2 control-label">适用范围</label>
     <div class="col-sm-9 col-xs-12">
-        <label class="radio-inline"><input type="radio" name="usetype" onclick='showusetype(0)' value="0">全类适用</label>
+        <label class="radio-inline"><input type="radio" name="usetype" onclick='showusetype(0)' value="0" checked>全类适用</label>
         <label class="radio-inline"><input type="radio" name="usetype" onclick='showusetype(1)' value="1" @if($coupon['use_type']==1)checked @endif>指定商品分类</label>
         <label class="radio-inline"><input type="radio" name="usetype" onclick='showusetype(2)' value="2" @if($coupon['use_type']==2)checked @endif>指定商品</label>
     </div>
