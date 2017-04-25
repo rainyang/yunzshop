eb1bba4b07ea14434010ddda4234c1af8ac6a90e
沈阳 <564345292@qq.com>
2017-04-25 15:00:12 +0800

    修改优惠券数据结构

--- app/frontend/modules/order/controllers/PreGeneratedController.php
+++ app/frontend/modules/order/controllers/PreGeneratedController.php
@@ -24,12 +24,16 @@ abstract class PreGeneratedController extends ApiController
         $total_goods_price = $order_data->sum('order.goods_price');
         $total_dispatch_price = $order_data->sum('order.dispatch_price');
 
-        //todo 临时处理,需要改写
+        $data['discount']['coupon'] = $order_data->map(function ($order_data) {
+            return $order_data['discount']['coupon'];
+
+        })->flatten();
+
         $data['dispatch'] = $order_data[0]['dispatch'];
         $order_data->map(function ($order_data) {
+            $order_data['discount']->forget('coupon');
             return $order_data->forget('dispatch');
         });
-
         $data += compact('total_price', 'total_dispatch_price', 'order_data', 'total_goods_price');
         return $this->successJson('成功', $data);
 
