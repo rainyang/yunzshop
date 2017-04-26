40b3245bc2f4617e71ea70ddeff52d3b518770e0
dingran80s <dingran@yunzshop.com>
2017-04-26 19:57:04 +0800

    支付调试

--- app/payment/PaymentController.php
+++ app/payment/PaymentController.php
@@ -99,7 +99,7 @@ class PaymentController extends BaseController
                 if ($data['unit'] == 'fen') {
                     $orderPay->amount = $orderPay->amount * 100;
                 }
-                \Log::debug('操作的订单', $orderPay->toArray());
+                \Log::debug('操作的订单', $orderPay->amount . '/' . $data['total_fee']);
                 if (bccomp($orderPay->amount, $data['total_fee'], 2) == 0) {
                     MemberRelation::checkOrderPay();
 
