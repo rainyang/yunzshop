07c05bd978c41e9f5c65a11a45ebfd74516731a5
dingran80s <dingran@yunzshop.com>
2017-04-26 10:02:30 +0800

    提现修改

--- app/backend/controllers/TestController.php
+++ app/backend/controllers/TestController.php
@@ -20,4 +20,11 @@ class TestController extends BaseController
         $result = (new JsonRpc())->client('plus',['user'=>'1','pass'=>2]);
         dd($result);
     }
+
+    public function test()
+    {
+        $url = 'aHR0cDovL3Rlc3QueXVuenNob3AuY29tL2FkZG9ucy95dW5fc2hvcC8jL2dvb2RzLzE1Mj9pPTImdHlwZT0xJm1pZD0yMDQmc2hhcmVfdGFnPTI';
+
+        echo base64_decode($url);
+    }
 }
