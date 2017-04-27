2a6decdcc624729a75081e9d56ce18c96e9bef2c
dingran80s <dingran@yunzshop.com>
2017-04-27 10:04:54 +0800

    充值调试

--- resources/views/order/pay.blade.php
+++ resources/views/order/pay.blade.php
@@ -10,7 +10,7 @@
 </script>
     <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
     <script type="text/javascript" charset="utf-8">
-        wx.config({"debug":false,"beta":false,"appId":"wx6be17f352e859277","nonceStr":"Tsr30K54SB","timestamp":"1493212870","url":"http:\/\/test.yunzshop.com\/addons\/yun_shop\/api.php?i=2&mid=null&type=1&route=finance.balance.recharge&i=2&type=1&pay_type=1&recharge_money=50","signature":"0671870e9046ae3896741d57417e4cc2a1c932aa","jsApiList":["chooseWXPay"]});
+        wx.config({"debug":false,"beta":false,"appId":"wx6be17f352e859277","nonceStr":"f9BUapEznF","timestamp":"1493258184","url":"http://test.yunzshop.com/addons/yun_shop/api.php?i=2&mid=null&type=1&route=finance.balance.recharge&i=2&type=1&pay_type=1&recharge_money=50","signature":"cb54d953ee4fefb411497e78db1118c20ea99537","jsApiList":["chooseWXPay"]});
     </script>
     <script>
         $(function(){
@@ -18,11 +18,11 @@
             $(".btn-do-it").click(function(){
                 wx.chooseWXPay({
                     appId: "wx6be17f352e859277",
-                    timestamp: "1493212870", // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                    nonceStr: '59009ec6692a2', // 支付签名随机串，不长于 32 位
-                    package: 'prepay_id=wx20170426212110ffdc7de2800844657131', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                    timestamp: "59014fc851234", // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                    nonceStr: '1493258184', // 支付签名随机串，不长于 32 位
+                    package: 'prepay_id=wx201704270956245959a0f7d20600308143', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
                     signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                    paySign: '2A63E04691B7E4DA76CAB27976905076', // 支付签名
+                    paySign: 'A31FAAFC7B875D09C31F7520A12D4A1F', // 支付签名
                     success: function (res) {
                         // 支付成功后的回调函数
                         if(res.errMsg == "chooseWXPay:ok" ) {
