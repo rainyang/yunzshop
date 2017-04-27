6dc4e9455080aaa2d9cc252ddef3c3e5aff14d71
沈阳 <564345292@qq.com>
2017-04-27 10:16:21 +0800

    优惠券返回数据格式修改

--- app/frontend/modules/coupon/services/TestService.php
+++ app/frontend/modules/coupon/services/TestService.php
@@ -48,11 +48,12 @@ class TestService
             return new Coupon($memberCoupon, $this->order);
         });
         $result = $coupons->filter(function($coupon){
-            $result = $coupon->isOptional();
             //exit;
             /**
              * @var $coupon Coupon
              */
+            $result = $coupon->isOptional();
+
             $coupon->getMemberCoupon()->valid = $coupon->valid();
 //            if($result){
 //                dd($coupon->getMemberCoupon()->id);
