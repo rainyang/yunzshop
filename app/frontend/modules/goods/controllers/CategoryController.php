ae4785ec48f495fc899a9888ce1cde02930f83ee
杨雷 <heiangdao@sina.com>
2017-05-03 13:48:39 +0800

    调试

--- app/frontend/modules/goods/controllers/CategoryController.php
+++ app/frontend/modules/goods/controllers/CategoryController.php
@@ -47,7 +47,6 @@ class CategoryController extends BaseController
         $set = Setting::get('shop.category');
         $parent_id = intval(\YunShop::request()->parent_id);
         $list = Category::getChildrenCategorys($parent_id,$set)->paginate($pageSize)->toArray();
-        echo "<pre>"; print_r($list);exit;
         foreach ($list['data'] as &$item) {
             $item['thumb'] = tomedia($item['thumb']);
             $item['adv_img'] = tomedia($item['adv_img']);
