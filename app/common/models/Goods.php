b97b01b4e2a925df61104140b1d020182fa5500d
YiTian <livsyitian@163.com>
2017-05-16 17:57:27 +0800

    test

--- app/common/models/Goods.php
+++ app/common/models/Goods.php
@@ -262,6 +262,7 @@ class Goods extends BaseModel
      */
     public static function getGoodsByName($keyword)
     {
+        
         return static::uniacid()->select('id', 'title', 'thumb','market_price','price','real_sales','sku')
             ->where('title', 'like', '%' . $keyword . '%')
             ->get();
