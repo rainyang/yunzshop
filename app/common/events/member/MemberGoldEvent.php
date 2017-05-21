5f477ae1e9a7f7f5ff3aaa85136505c2f46b6d87
杨洋 <yangyang@yunzshop.com>
2017-05-21 15:28:46 +0800

    金币事件

---
+++ app/common/events/member/MemberGoldEvent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangyang
+ * Date: 2017/5/21
+ * Time: 下午3:26
+ */
+
+namespace app\common\events\member;
+
+
+class MemberGoldEvent extends GetMemberGoldEvent
+{
+
+}
