354866516926427579ae522440374668550947f3
杨雷 <heiangdao@sina.com>
2017-05-27 15:16:07 +0800

    no message

--- app/frontend/modules/shop/controllers/IndexController.php
+++ app/frontend/modules/shop/controllers/IndexController.php
@@ -22,7 +22,7 @@ class IndexController extends ApiController
     public function getDefaultIndex()
     {
         $set = Setting::get('shop.category');
-        $set['cat_adv_img'] = tomedia($set['cat_adv_img']);
+        $set['cat_adv_img'] = replace_yunshop(tomedia($set['cat_adv_img']));
         $category = $this->getRecommentCategoryList();
         foreach ($category  as &$item){
             $item['thumb'] = replace_yunshop(tomedia($item['thumb']));
