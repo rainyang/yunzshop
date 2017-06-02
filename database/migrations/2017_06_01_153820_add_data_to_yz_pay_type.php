4a821be5a6bd38ea53961824a7e1e43399c460b8
沈阳 <564345292@qq.com>
2017-06-02 09:58:05 +0800

    修改migrations

--- database/migrations/2017_06_01_153820_add_data_to_yz_pay_type.php
+++ database/migrations/2017_06_01_153820_add_data_to_yz_pay_type.php
@@ -18,6 +18,9 @@ class AddDataToYzPayType extends Migration
                 if (!\Schema::hasColumn('yz_pay_type', 'code')) {
                     $table->string('code', 50);
                 }
+                if (!\Schema::hasColumn('yz_pay_type', 'type')) {
+                    $table->tinyInteger('type');
+                }
                 if (!\Schema::hasColumn('yz_pay_type', 'plugin_id')) {
                     $table->tinyInteger('plugin_id');
                 }
@@ -27,20 +30,20 @@ class AddDataToYzPayType extends Migration
 
                 if (!\Schema::hasColumn('yz_pay_type', 'updated_at')) {
 
-                    $table->integer('updated_at')->default(0);
+                    $table->integer('updated_at')->nullable();
                 }
                 if (!\Schema::hasColumn('yz_pay_type', 'created_at')) {
 
-                    $table->integer('created_at')->default(0);
+                    $table->integer('created_at')->nullable();
                 }
                 if (!\Schema::hasColumn('yz_pay_type', 'deleted_at')) {
 
-                    $table->integer('deleted_at')->default(0);
+                    $table->integer('deleted_at')->nullable();
                 }
-                \Illuminate\Support\Facades\DB::transaction(function () {
-                    $this->syncData();
-                });
 
+            });
+            \Illuminate\Support\Facades\DB::transaction(function () {
+                $this->syncData();
             });
         }
     }
@@ -48,15 +51,18 @@ class AddDataToYzPayType extends Migration
     private function syncData()
     {
         \app\common\models\PayType::where('id','>','-1')->delete();
-
         \app\common\models\PayType::insert([
-                ['id'=>0, 'name'=>'未支付', 'code'=>'unPay', 'type'=>0, 'plugin_id'=>0, 'unit'=>''],
-                ['id'=>1, 'name'=>'微信支付', 'code'=>'wechatPay', 'type'=>2, 'plugin_id'=>0, 'unit'=>'元'],
-                ['id'=>2, 'name'=>'支付宝支付', 'code'=>'alipay', 'type'=>2, 'plugin_id'=>0, 'unit'=>'元'],
-                ['id'=>3, 'name'=>'余额支付', 'code'=>'balance', 'type'=>1, 'plugin_id'=>0, 'unit'=>'元'],
-                ['id'=>4, 'name'=>'金币支付', 'code'=>'gold', 'type'=>1, 'plugin_id'=>0, 'unit'=>'金币']]
+                ['name'=>'未支付', 'code'=>'unPay', 'type'=>0, 'plugin_id'=>0, 'unit'=>''],
+                ['name'=>'微信支付', 'code'=>'wechatPay', 'type'=>2, 'plugin_id'=>0, 'unit'=>'元'],
+                ['name'=>'支付宝支付', 'code'=>'alipay', 'type'=>2, 'plugin_id'=>0, 'unit'=>'元'],
+                ['name'=>'余额支付', 'code'=>'balance', 'type'=>1, 'plugin_id'=>0, 'unit'=>'元'],
+                ['name'=>'金币支付', 'code'=>'gold', 'type'=>1, 'plugin_id'=>0, 'unit'=>'金币']]
         );
         \app\common\models\PayType::where('code','unPay')->update(['id'=>0]);
+        \app\common\models\PayType::where('code','wechatPay')->update(['id'=>1]);
+        \app\common\models\PayType::where('code','alipay')->update(['id'=>2]);
+        \app\common\models\PayType::where('code','balance')->update(['id'=>3]);
+        \app\common\models\PayType::where('code','gold')->update(['id'=>4]);
 
     }
 
