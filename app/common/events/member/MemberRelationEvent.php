13058a1434e015d7340647fbf2d8cf17b3c0f0dd
dingran80s <dingran@yunzshop.com>
2017-06-06 16:01:00 +0800

    增加获得推广权限事件

---
+++ app/common/events/member/MemberRelationEvent.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Author: 芸众商城 www.yunzshop.com
+ * Date: 2017/6/6
+ * Time: 下午3:48
+ */
+
+namespace app\common\events\member;
+
+
+use app\backend\modules\member\models\Member;
+
+class MemberRelationEvent extends Event
+{
+    protected $mid;
+
+    protected $user;
+
+    public function __construct(Member $model)
+    {
+        $this->user = $model;
+    }
+
+    public function getMemberModel()
+    {
+        return $this->user;
+    }
+}
