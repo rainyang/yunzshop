7619b7bb07ed4d7a885a57665fbba4421e27dbd5
杨洋 <yangyang@yunzshop.com>
2017-06-06 17:23:15 +0800

    积分通知

--- app/common/services/finance/PointService.php
+++ app/common/services/finance/PointService.php
@@ -98,10 +98,13 @@ class PointService
         if (!$noticeMember->hasOneFans->openid) {
             return;
         }
+        if ($noticeMember->hasOneFans->follow != 1) {
+            return;
+        }
         $msg = [
             "first" => '您好',
             "keyword1" => '积分变动通知',
-            "keyword2" => '尊敬的[' . $this->member['nickname'] . ']，您与[' . date('Y-m-d H:i', time()) . ']发生积分变动，变动数值为[' . $this->point_data['point'] . ']，类型[' . $this->point_data['point_mode'] . ']，您目前积分余值为[' . $this->point_data['after_point'] . ']',
+            "keyword2" => '尊敬的[' . $this->member['nickname'] . ']，您于[' . date('Y-m-d H:i', time()) . ']发生积分变动，变动数值为[' . $this->point_data['point'] . ']，类型[' . $this->point_data['point_mode'] . ']，您目前积分余值为[' . $this->point_data['after_point'] . ']',
             "remark" => "",
         ];
         if (!\Setting::get('shop.notice')['task']) {
