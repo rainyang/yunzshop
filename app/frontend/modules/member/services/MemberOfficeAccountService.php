3312086098b82b52db3eda81d9be5ac5cf41ec8c
dingran80s <dingran@yunzshop.com>
2017-06-09 10:52:06 +0800

    登陆测试

--- app/frontend/modules/member/services/MemberOfficeAccountService.php
+++ app/frontend/modules/member/services/MemberOfficeAccountService.php
@@ -128,6 +128,11 @@ class MemberOfficeAccountService extends MemberService
             $member_id = $mc_mapping_fans_model->uid;
         }
 
+        \Log::debug('UnionidInfo', $UnionidInfo);
+        \Log::debug('member_model', $member_model);
+        \Log::debug('mc_mapping_fans_model', $mc_mapping_fans_model);
+        \Log::debug('member_shop_info_model', $member_shop_info_model);
+
         if (!empty($UnionidInfo['unionid']) && !empty($member_model) && !empty($mc_mapping_fans_model) && !empty($member_shop_info_model)) {
             $types = explode('|', $UnionidInfo['type']);
             $member_id = $UnionidInfo['member_id'];
