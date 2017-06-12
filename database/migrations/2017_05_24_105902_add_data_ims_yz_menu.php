3b8f61de649e63af0abe5bce5235e1588aa11dbb
YiTian <libaojia@yunzshop.com>
2017-06-12 20:17:54 +0800

    修改升级报错

--- database/migrations/2017_05_24_105902_add_data_ims_yz_menu.php
+++ database/migrations/2017_05_24_105902_add_data_ims_yz_menu.php
@@ -100,6 +100,221 @@ class AddDataImsYzMenu extends Migration
                     ]);
                 }
             }
+
+            $_menu = Menu::select('id')->where('url', 'order.list')->first();
+            if ($_menu->id) {
+                $modelOne = Menu::select('id')->where('url', 'refund.list.refund')->first();
+                if ($modelOne) {
+                    Menu::where('id', $modelOne->id)->update([
+                        'name'              => '退换货订单',
+                        'item'              => 'refund_list_refund',
+                        'url'               => 'refund.list.refund',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                } else {
+                    Menu::insert([
+                        'name'              => '退换货订单',
+                        'item'              => 'refund_list_refund',
+                        'url'               => 'refund.list.refund',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                }
+                $modelTwo = Menu::select('id')->where('url', 'refund.list.refunded')->first();
+                if ($modelTwo) {
+                    Menu::where('id', $modelTwo->id)->update([
+                        'name'              => '已退款',
+                        'item'              => 'refund_list_refunded',
+                        'url'               => 'refund.list.refunded',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                } else {
+                    Menu::insert([
+                        'name'              => '已退款',
+                        'item'              => 'refund_list_refunded',
+                        'url'               => 'refund.list.refunded',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                }
+            }
+
+            $_menu = Menu::select('id')->where('url', 'refund.list.refund')->first();
+            if ($_menu->id) {
+                $modelOne = Menu::select('id')->where('url', 'refund.list.refundMoney')->first();
+                if ($modelOne) {
+                    Menu::where('id', $modelOne->id)->update([
+                        'name'              => '仅退款',
+                        'item'              => 'refund_list_refundMoney',
+                        'url'               => 'refund.list.refundMoney',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                } else {
+                    Menu::insert([
+                        'name'              => '仅退款',
+                        'item'              => 'refund_list_refundMoney',
+                        'url'               => 'refund.list.refundMoney',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                }
+                $modelTwo = Menu::select('id')->where('url', 'refund.list.returnGoods')->first();
+                if ($modelTwo) {
+                    Menu::where('id', $modelTwo->id)->update([
+                        'name'              => '退货退款',
+                        'item'              => 'refund_list_returnGoods',
+                        'url'               => 'refund.list.returnGoods',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                } else {
+                    Menu::insert([
+                        'name'              => '退货退款',
+                        'item'              => 'refund_list_returnGoods',
+                        'url'               => 'refund.list.returnGoods',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                }
+                $modelThree = Menu::select('id')->where('url', 'refund.list.exchangeGoods')->first();
+                if ($modelThree) {
+                    Menu::where('id', $modelThree->id)->update([
+                        'name'              => '换货',
+                        'item'              => 'refund_list_exchangeGoods',
+                        'url'               => 'refund.list.exchangeGoods',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                } else {
+                    Menu::insert([
+                        'name'              => '换货',
+                        'item'              => 'refund_list_exchangeGoods',
+                        'url'               => 'refund.list.exchangeGoods',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                }
+                $modelFour = Menu::select('id')->where('url', 'refund.list.refund')->first();
+                if ($modelFour) {
+                    Menu::where('id', $modelFour->id)->update([
+                        'name'              => '全部',
+                        'item'              => 'refund_list_refund_all',
+                        'url'               => 'refund.list.refund',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                } else {
+                    Menu::insert([
+                        'name'              => '全部',
+                        'item'              => 'refund_list_refund_all',
+                        'url'               => 'refund.list.refund',
+                        'url_params'        => '',
+                        'permit'            => 1,
+                        'menu'              => 1,
+                        'icon'              => 'fa-circle-o',
+                        'parent_id'         => $_menu->id,
+                        'sort'              => 1,
+                        'status'            => 1,
+                        'created_at'        => time(),
+                        'updated_at'        => time(),
+                        'deleted_at'        => NULL
+                    ]);
+                }
+            }
+
+
+
         }
     }
 
