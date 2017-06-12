0ee6ec3aac020e73b7eacc01a32049f01ae9b98e
dingran80s <dingran@yunzshop.com>
2017-06-12 22:10:18 +0800

    会员查询修改

--- app/common/components/ApiController.php
+++ app/common/components/ApiController.php
@@ -16,6 +16,7 @@ use app\common\helpers\Url;
 use app\common\models\Member;
 use app\common\models\MemberShopInfo;
 use app\common\models\UniAccount;
+use app\common\services\Session;
 use app\frontend\modules\member\services\MemberService;
 
 class ApiController extends BaseController
@@ -61,6 +62,11 @@ class ApiController extends BaseController
 
             return $this->errorJson('',['login_status'=> 0,'login_url'=>Url::absoluteApi('member.login.index', $queryString)]);
         } else {
+
+            if (!MemerShopInfo::getMemberShopInfo(\YunShop::app()->getMemberId())) {
+                Session::clear('member_id');
+            }
+
             if (MemberShopInfo::isBlack(\YunShop::app()->getMemberId())) {
                 return $this->errorJson('黑名单用户，请联系管理员', ['login_status' => -1]);
             }
@@ -80,6 +86,15 @@ class ApiController extends BaseController
         if ($validator->fails()) {
             throw new AppException(current($this->formatValidationErrors($validator)));
         }
+    }
+
+    private function jumpUrl($type)
+    {
+        if (5 == $type) {
+            return $this->errorJson('',['login_status'=> 1,'login_url'=>'', 'type'=>$type,'session_id'=>session_id(), 'i'=>\YunShop::app()->uniacid, 'mid'=>$mid]);
+        }
+
+        return $this->errorJson('',['login_status'=> 0,'login_url'=>Url::absoluteApi('member.login.index', $queryString)]);
     }
 
 }
