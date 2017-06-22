55daeb01744616c859fb70813912c183c9ef52c9
YiTian <libaojia@yunzshop.com>
2017-06-22 16:23:50 +0800

    修改income config 配置

--- config/income.php
+++ config/income.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'balance' => [
-        'title' => '余额',
+        'title' => '余额提现',
         'type' => 'balance',
-        'type_name' => '余额提现',
         'class' => 'balance',
+        'url' => 'finance.balance-withdraw.detail'
     ]
 
 ];
