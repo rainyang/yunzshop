eff8e82ac36c21a2533ce834562464b024e1aed3
杨雷 <heiangdao@sina.com>
2017-07-12 10:43:39 +0800

    单品满立减 后台设置

--- app/common/models/Sale.php
+++ app/common/models/Sale.php
@@ -17,6 +17,8 @@ class Sale extends BaseModel
         'max_balance_deduct' => 0,
         'is_sendfree' => 0,
         'ed_num' => 0,
+        'ed_full' => 0,
+        'ed_reduction' => 0,
         'ed_money' => 0,
         'point' => '',
         'bonus' => 0
