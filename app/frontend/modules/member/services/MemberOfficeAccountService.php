222d3f3a0fe4ac3f00cc9e0240ba6ed5514b8490
dingran80s <dingran@yunzshop.com>
2017-07-12 11:31:56 +0800

    会员Nickname编码

--- app/frontend/modules/member/services/MemberOfficeAccountService.php
+++ app/frontend/modules/member/services/MemberOfficeAccountService.php
@@ -377,7 +377,7 @@ class MemberOfficeAccountService extends MemberService
         \Log::debug('nickname', [$nickname]);
         $nickname = preg_replace($patten, "", $nickname);
         \Log::debug('pre', [$nickname]);
-        $nickname = preg_replace("#\\\u([0-9a-f]+)#ie","iconv('UCS-2','UTF-8', pack('H4', '\\1'))",$nickname);
+        $nickname = preg_replace("#\\\u([0-9a-f]+)#ie","iconv('UCS-2BE','UTF-8', pack('H4', '\\1'))",$nickname);
         \Log::debug('post', [$nickname]);
         \Log::debug('json', [json_decode($this->cutNickname($nickname))]);
         return json_decode($this->cutNickname($nickname));
