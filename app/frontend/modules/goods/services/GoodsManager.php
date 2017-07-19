e7704e22ada3e4203fd3974bffdf5c5491a445fb
沈阳 <564345292@qq.com>
2017-07-19 14:07:15 +0800

    添加管理者类

---
+++ app/frontend/modules/goods/services/GoodsManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shenyang
+ * Date: 2017/7/19
+ * Time: 下午2:06
+ */
+
+namespace app\frontend\modules\goods\services;
+
+
+class GoodsManager
+{
+
+}
---
+++ app/frontend/modules/order/services/OrderManager.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shenyang
+ * Date: 2017/5/24
+ * Time: 下午3:11
+ */
+namespace app\frontend\modules\order\services;
+
+use app\backend\modules\order\models\Order;
+use app\frontend\modules\goods\services\models\PreGeneratedOrderGoodsModel;
+use Illuminate\Container\Container;
+
+class OrderManager extends Container
+{
+    public function __construct()
+    {
+        //
+        $this->bind('PreGeneratedOrderGoodsModel', function ($orderManager, $attributes) {
+            return new PreGeneratedOrderGoodsModel($attributes);
+        });
+        // 订单model
+        $this->bind('Order', function ($orderManager) {
+            if(\YunShop::isApi()){
+                return new \app\frontend\models\Order();
+
+            }else{
+                return new Order();
+            }
+        });
+        // 订单service
+        $this->singleton('OrderService', function ($orderManager) {
+            return new OrderService();
+        });
+    }
+}
