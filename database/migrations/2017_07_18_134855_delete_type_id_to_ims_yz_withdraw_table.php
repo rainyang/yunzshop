9a3254f48f8e50a331819ea328aac283c6677243
沈阳 <564345292@qq.com>
2017-07-19 10:21:51 +0800

    修复migrations文件

--- database/migrations/2017_07_18_134855_delete_type_id_to_ims_yz_withdraw_table.php
+++ database/migrations/2017_07_18_134855_delete_type_id_to_ims_yz_withdraw_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DelteTypeIdToImsYzWithdrawTable extends Migration
+class DeleteTypeIdToImsYzWithdrawTable extends Migration
 {
     /**
      * Run the migrations.
