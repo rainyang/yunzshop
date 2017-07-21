65b321e9a2cc30d73168a44c3476b854c15e8493
杨雷 <heiangdao@sina.com>
2017-07-21 14:40:32 +0800

    添加 menu

--- database/seeds/YzMenuUpgradeSeeder.php
+++ database/seeds/YzMenuUpgradeSeeder.php
@@ -7,5 +7,29 @@
  */
 class YzMenuUpgradeSeeder
 {
+    protected $table = 'yz_menu';
+
+    public function run()
+    {
+        $item = DB::table($this->table)->where('item', 'system_update')->first();
+        if ($item->isNotEmpty()) {
+            echo "system_update 已经有数据了跳过\n";
+            return;
+        }
+        $data = [
+            'name' => '系统升级',
+            'item' => 'system_update',
+            'url' => 'update.index',
+            'url_params' => '',
+            'permit' => 1,
+            'menu' => 1,
+            'icon' => 'fa-arrow-circle-up',
+            'parent_id' => 1,
+            'sort' => 0,
+            'status' => 1,
+            'created_at' => time(),
+        ];
+        DB::table($this->table)->insert($data);
+    }
 
 }
