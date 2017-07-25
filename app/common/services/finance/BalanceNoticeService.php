9940a8796e0025c2252e407569ad71fad2fe4e88
YiTian <libaojia@yunzshop.com>
2017-07-26 00:00:27 +0800

    修改审核时间错误

--- app/common/services/finance/BalanceNoticeService.php
+++ app/common/services/finance/BalanceNoticeService.php
@@ -56,7 +56,7 @@ class BalanceNoticeService
             ),
             'timet' => array(
                 'title' => '提现时间',
-                'value' => $withdrawModel->audit_at->toDateTimeString(),
+                'value' => date('Y-m-d H:i:s', $withdrawModel->pay_at),
                 "color" => "#4a5077"
             ),
             'remark' => array(
@@ -82,7 +82,7 @@ class BalanceNoticeService
             ),
             'timet' => array(
                 'title' => '提现时间',
-                'value' => $withdrawModel->pay_at->toDateTimeString(),
+                'value' => date('Y-m-d H:i:s', $withdrawModel->audit_at),
                 "color" => "#4a5077"
             ),
             'remark' => array(
