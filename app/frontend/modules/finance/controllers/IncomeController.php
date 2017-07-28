cdbd98d944a78c3ffe6fe37ab7dd044debc0e98d
YiTian <libaojia@yunzshop.com>
2017-07-28 17:50:08 +0800

    test

--- app/frontend/modules/finance/controllers/IncomeController.php
+++ app/frontend/modules/finance/controllers/IncomeController.php
@@ -223,7 +223,7 @@ class IncomeController extends ApiController
     {
         $config = \Config::get('income');
         $withdrawData = \YunShop::request()->data;
-        dd($withdrawData);
+        echo '<pre>'; print_r($withdrawData); exit;
         if (!$withdrawData) {
             return $this->errorJson('未检测到数据!');
         }
