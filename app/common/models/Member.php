e6c2d54166549bf785dd44de1b8023156d0abbc6
YiTian <libaojia@yunzshop.com>
2017-08-01 21:13:20 +0800

    Test

--- app/common/models/Member.php
+++ app/common/models/Member.php
@@ -346,9 +346,9 @@ class Member extends BackendModel
             ];
         }
 
-        // = \Config::get('member_center.plugins');
+        $plugins = \Config::get('member_center.plugins');
 
-        //$data = array_merge($data,$plugins);
+        $data = array_merge($data,$plugins);
 
         return $data;
     }
