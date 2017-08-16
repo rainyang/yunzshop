efab1d68805dfe09a7cec23b073b1510d26addd8
沈阳 <564345292@qq.com>
2017-08-16 14:49:51 +0800

    前端接口隐藏订单商品中的商品模型

--- app/frontend/modules/orderGoods/models/PreGeneratedOrderGoods.php
+++ app/frontend/modules/orderGoods/models/PreGeneratedOrderGoods.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Collection;
 
 class PreGeneratedOrderGoods extends OrderGoods
 {
-    protected $hidden = ['goods', 'sale'];
+    protected $hidden = ['goods', 'sale','belongsToGood'];
     /**
      * @var PreGeneratedOrder
      */
