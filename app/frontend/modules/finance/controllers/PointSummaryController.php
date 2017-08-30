ba3a8a949878c77ae88b1be25bca5a81f3385d52
杨洋 <yangyang@yunzshop.com>
2017-08-30 20:19:18 +0800

    no message

--- app/frontend/modules/finance/controllers/PointSummaryController.php
+++ app/frontend/modules/finance/controllers/PointSummaryController.php
@@ -20,41 +20,12 @@ class PointSummaryController extends ApiController
         $member_id = \YunShop::app()->getMemberId();
         $point_total = Member::getMemberById($member_id)['credit1'];
 
-        $list = PointLog::getPointLogList($member_id)->limit(5)->get();
-        $this->attachedServiceType($list);
+        $list = PointLog::getPointLogList($member_id)->limit(10)->get();
         return $this->successJson('成功',
             [
                 'point_total'       => $point_total,
                 'list'              => $list,
             ]
         );
-    }
-
-    private function attachedServiceType($list)
-    {
-        if ($list) {
-            foreach ($list as $key => $log) {
-                switch ($log->point_mode) {
-                    case PointService::POINT_MODE_GOODS:
-                        $log->point_mode = PointService::POINT_MODE_GOODS_ATTACHED;
-                        break;
-                    case PointService::POINT_MODE_ORDER:
-                        $log->point_mode = PointService::POINT_MODE_ORDER_ATTACHED;
-                        break;
-                    case PointService::POINT_MODE_POSTER:
-                        $log->point_mode = PointService::POINT_MODE_POSTER_ATTACHED;
-                        break;
-                    case PointService::POINT_MODE_ARTICLE:
-                        $log->point_mode = PointService::POINT_MODE_ARTICLE_ATTACHED;
-                        break;
-                    case PointService::POINT_MODE_ADMIN:
-                        $log->point_mode = PointService::POINT_MODE_ADMIN_ATTACHED;
-                        break;
-                    case PointService::POINT_MODE_BY:
-                        $log->point_mode = PointService::POINT_MODE_BY_ATTACHED;
-                        break;
-                }
-            }
-        }
     }
 }
