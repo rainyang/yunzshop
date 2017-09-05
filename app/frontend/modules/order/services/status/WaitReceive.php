406a707493afc1e088eb17a37801f936db1b7db7
沈阳 <564345292@qq.com>
2017-09-05 17:01:58 +0800

    自提订单

--- app/frontend/modules/order/services/status/WaitReceive.php
+++ app/frontend/modules/order/services/status/WaitReceive.php
@@ -9,28 +9,41 @@
 namespace app\frontend\modules\order\services\status;
 
 
+use app\common\models\DispatchType;
 use app\common\models\Order;
 
 class WaitReceive extends Status
 {
+    protected $name = '收货';
     private $order;
+
     public function __construct(Order $order)
     {
         $this->order = $order;
+        if ($this->order->dispatch_type == DispatchType::SELF_DELIVERY) {
+            // 自提
+            $this->name = '使用';
+        }
+        if ($this->order->dispatch_type == DispatchType::STORE_DELIVERY) {
+            // 商家配送
+            $this->name = '核销';
+        }
     }
 
     public function getStatusName()
     {
-        return '待收货';
+        return "待{$this->name}";
     }
 
     public function getButtonModels()
     {
         $result[] = [
-            'name' => '确认收货',
+            'name' => "确认{$this->name}",
             'api' => 'order.operation.receive',
             'value' => static::COMPLETE //todo
         ];
+        // 确认核销
+        // 确认
         if (!$this->order->isVirtual()) {
             $result[] = [
                 'name' => '查看物流', //todo 原来商城的逻辑是, 当有物流单号时, 才显示"查看物流"按钮
