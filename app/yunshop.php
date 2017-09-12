73a5482fc794a4bde27f62e0f995acbf231f6d59
dingran80s <dingran@yunzshop.com>
2017-09-12 14:53:00 +0800

    测试

--- app/yunshop.php
+++ app/yunshop.php
@@ -30,9 +30,7 @@ class YunShop
     }
 
     public static function run($namespace, $modules, $controllerName, $action, $currentRoutes)
-    {  Log::debug('------boot run-------');
-        include_once __DIR__ . '/../../../framework/bootstrap.inc.php';
-
+    {
         //检测命名空间
         if (!class_exists($namespace)) {
             throw new NotFoundException(" 不存在类: " . $namespace);
