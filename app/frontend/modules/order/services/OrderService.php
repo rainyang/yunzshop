0c041cfcee5a6d82cf28059ce60a40b7502fbe9a
沈阳 <564345292@qq.com>
2017-09-13 11:16:47 +0800

    调试

--- app/frontend/modules/order/services/OrderService.php
+++ app/frontend/modules/order/services/OrderService.php
@@ -283,8 +283,6 @@ class OrderService
         if (isset($param['pay_type_id'])) {
             $orderOperation->pay_type_id = $param['pay_type_id'];
         }
-        dd($param);
-        exit;
 
         if (isset($param['order_pay_id'])) {
             $orderOperation->order_pay_id = $param['order_pay_id'];
