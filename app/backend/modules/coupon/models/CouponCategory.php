0ce3f6f4ab6de4aec145bd278f2c684914e51a89
沈阳 <564345292@qq.com>
2017-09-18 16:24:57 +0800

    添加migrations

--- app/backend/modules/coupon/models/CouponCategory.php
+++ app/backend/modules/coupon/models/CouponCategory.php
@@ -2,8 +2,6 @@
 
 namespace app\backend\modules\coupon\models;
 
-use Illuminate\Database\Eloquent\Model;
-
 class CouponCategory extends \app\common\models\CouponCategory
 {
     static protected $needLog = true;
