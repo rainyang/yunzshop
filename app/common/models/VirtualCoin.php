715d9450112812cebed518a8923704634249979f
沈阳 <564345292@qq.com>
2017-09-25 15:50:35 +0800

    虚拟币model

---
+++ app/common/models/VirtualCoin.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shenyang
+ * Date: 2017/9/10
+ * Time: 上午11:30
+ */
+
+namespace app\common\models;
+
+
+class VirtualCoin extends BaseModel
+{
+    protected $table = 'yz_virtual_coin';
+    protected $amountOfCoin;
+    protected $amountOfMoney;
+    protected $exchange_rate;
+    public function setAmountOfCoin($amount){
+        $this->amountOfCoin = $amount;
+    }
+    public function setMountOfMoney($amount){
+        $this->amountOfMoney = $amount;
+    }
+    public function getAmountOfCoin()
+    {
+        if(isset($this->amountOfCoin)){
+            return $this->amountOfCoin;
+        }
+
+        return $this->amountOfCoin = $this->amountOfMoney / $this->exchange_rate;
+    }
+
+    public function getAmountOfMoney()
+    {
+        if(isset($this->amountOfMoney)){
+            return $this->amountOfMoney;
+        }
+
+        return $this->amountOfMoney = $this->amountOfCoin * $this->exchange_rate;
+    }
+}
