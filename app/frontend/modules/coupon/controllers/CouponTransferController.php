93816f1eaebd8d61541effd4551855b2e5acfe12
YiTian <libaojia@yunzshop.com>
2017-09-26 13:59:55 +0800

    会员中心优惠劵列表增加 V2 接口

---
+++ app/frontend/modules/coupon/controllers/CouponTransferController.php
@@ -0,0 +1,16 @@
+<?php
+/****************************************************************
+ * Author:  libaojia
+ * Date:    2017/9/26 下午1:44
+ * Email:   livsyitian@163.com
+ * QQ:      995265288
+ * User:    芸众商城 www.yunzshop.com
+ ****************************************************************/
+
+namespace app\frontend\modules\coupon\controllers;
+
+
+class CouponTransferController
+{
+
+}
