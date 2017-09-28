ce952d5d0be2469def111cb3b7caf10e3955ffa2
dingran80s <dingran@yunzshop.com>
2017-09-28 14:23:10 +0800

    后台check用于footer检查更新check2新更新机制

--- app/backend/controllers/UpdateController.php
+++ app/backend/controllers/UpdateController.php
@@ -47,12 +47,48 @@ class UpdateController extends BaseController
         return view('update.upgrad')->render();
     }
 
+    /**
+     * footer检测更新
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function check()
+    {
+        $result = ['msg' => '', 'last_version' => '', 'updated' => 0];
+        $key = Setting::get('shop.key')['key'];
+        $secret = Setting::get('shop.key')['secret'];
+        if(!$key || !$secret) {
+            return;
+        }
+
+        $update = new AutoUpdate(null, null, 300);
+        $update->setUpdateFile('check_app.json');
+        $update->setCurrentVersion(config('version'));
+        $update->setUpdateUrl(config('auto-update.checkUrl')); //Replace with your server update directory
+        $update->setBasicAuth($key, $secret);
+        //$update->setBasicAuth();
+
+        //Check for a new update
+        if ($update->checkUpdate() === false) {
+            $result['msg'] = 'Could not check for updates! See log file for details.';
+            response()->json($result)->send();
+            return;
+        }
+
+        if ($update->newVersionAvailable()) {
+            $result['last_version'] = $update->getLatestVersion()->getVersion();
+            $result['updated'] = 1;
+            $result['current_version'] = config('version');
+        }
+        response()->json($result)->send();
+        return;
+    }
+
 
     /**
      * 检测更新
      * @return \Illuminate\Http\JsonResponse
      */
-    public function check()
+    public function check2()
     {
         $filesystem = new Filesystem();
 
