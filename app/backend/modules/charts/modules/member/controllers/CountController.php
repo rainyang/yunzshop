303caf5b12e7641768f95429b59c4b3d84bf9c7a
YiTian <libaojia@yunzshop.com>
2017-10-10 10:39:49 +0800

    暂存

---
+++ app/backend/modules/charts/modules/member/controllers/CountController.php
@@ -0,0 +1,23 @@
+<?php
+/****************************************************************
+ * Author:  libaojia
+ * Date:    2017/10/10 上午10:36
+ * Email:   livsyitian@163.com
+ * QQ:      995265288
+ * User:    芸众商城 www.yunzshop.com
+ ****************************************************************/
+
+namespace app\backend\modules\charts\modules\member\controllers;
+
+
+use app\common\components\BaseController;
+
+class CountController extends BaseController
+{
+
+    public function index()
+    {
+        return view()->render();
+    }
+
+}
