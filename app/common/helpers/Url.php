e670f7bc541b1c4c0f03219acdd1a1eceb9dc111
dingran80s <dingran@yunzshop.com>
2017-10-11 17:23:38 +0800

    修复微信支付

--- app/common/helpers/Url.php
+++ app/common/helpers/Url.php
@@ -19,6 +19,16 @@ class Url
         return '/addons/' . $module . (strpos($uri,'/') === 0 ? '':'/') . $uri;
     }
 
+    public static function shopSchemeUrl($uri)
+    {
+        if(empty($uri) || self::isHttp($uri)){
+            return $uri;
+        }
+        $domain = request()->getSchemeAndHttpHost();
+        $module = request()->get('m','yun_shop');
+        return $domain . '/addons/' . $module . (strpos($uri,'/') === 0 ? '':'/') . $uri;
+    }
+
     /**
      * 生成后台相对Url
      *      路由   api.v1.test.index  为  app/backend/moduels/api/modules/v1/TestController   index
