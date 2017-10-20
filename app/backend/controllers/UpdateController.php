9c5be06d82b9c4a9ea8abc34ea7355d8d65b985e
dingran80s <dingran@yunzshop.com>
2017-10-20 16:42:14 +0800

    修复删除更新文件

--- app/backend/controllers/UpdateController.php
+++ app/backend/controllers/UpdateController.php
@@ -300,10 +300,12 @@ class UpdateController extends BaseController
 
                 if (!empty($content)) {
                     file_put_contents(base_path($path), $content);
+
+                    @unlink(storage_path('app/auto-update/shop') . '/' . $path);
                 }
             }
 
-            $filesystem->deleteDirectory(storage_path('app/auto-update/shop'));
+            //$filesystem->deleteDirectory(storage_path('app/auto-update/shop'));
 
             //更新完执行数据表
             \Log::debug('----CLI----');
