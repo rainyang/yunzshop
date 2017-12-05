38e5d0b4433dd5391f8397207e013a27d5cbc2d6
杨洋 <yangyang@yunzshop.com>
2017-12-05 16:55:05 +0800

    no message

--- app/common/services/finance/PointService.php
+++ app/common/services/finance/PointService.php
@@ -142,7 +142,9 @@ class PointService
 
     public function messageNotice()
     {
+        \Log::info('code', $this->point_data['point_mode']);
         $this->point_data['point_mode'] = $this->getModeAttribute($this->point_data['point_mode']);
+        \Log::info('code_name', $this->point_data['point_mode']);
         $noticeMember = Member::getMemberByUid($this->member->uid)->with('hasOneFans')->first();
         if (!$noticeMember->hasOneFans->openid) {
             return;
@@ -160,7 +162,9 @@ class PointService
             ['name' => '积分变动类型', 'value' => $this->point_data['point_mode']],
             ['name' => '变动后余额数值', 'value' => $this->point_data['after_point']]
         ];
+        \Log::info('params', $params);
         $msg = MessageTemp::getSendMsg($temp_id, $params);
+        \Log::info('msg', $msg);
         if (!$msg) {
             return;
         }
