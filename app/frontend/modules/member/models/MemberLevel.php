0762c8afb4bbd9fb8979e882f7b9c5df655c26de
aaa <123@qq.com>
2017-12-11 09:52:39 +0800

    2017/12/11 显示等级升级信息

---
+++ app/frontend/modules/member/models/MemberLevel.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Author: 芸众商城 www.yunzshop.com
+ * Date: 2017/12/8
+ * Time: 上午11:54
+ */
+
+
+namespace app\frontend\modules\member\models;
+
+use app\backend\modules\member\models\MemberLevel as backendMemberLevel;
+
+class MemberLevel extends backendMemberLevel
+{
+
+    /**
+     * 获取会员等级信息
+     * @param  string $type 会员等级依据
+     * @return array $data 各等级升级的信息
+     * 
+     */
+    public function getLevelData($type)
+    {
+
+        $data['level_type'] = $type;
+
+        if ($type == 2) {
+
+            $data['data'] = self::select('level_name','goods_id')->uniacid()
+            ->with(['goods' => function($query) {
+                return $query->select('id','title','thumb','price');
+            }])->orderBy('level')->get()->toArray();
+            return $data;
+        }
+
+        $content = $type == 1 ? 'order_count' : 'order_money';
+
+        $data['data'] = self::select('level_name', $content)
+            ->uniacid()
+            ->orderBy('level')
+            ->get()->toArray();
+
+        return $data;
+
+    }
+}
