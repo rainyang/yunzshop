99ae78b0421995ccf411d1c0a68a4e8eca2e2249
沈阳 <564345292@qq.com>
2017-12-27 15:45:36 +0800

    修复migrations

--- database/migrations/2017_12_15_145350_create_ims_yz_member_address_table.php
+++ database/migrations/2017_12_15_145350_create_ims_yz_member_address_table.php
@@ -12,21 +12,23 @@ class CreateImsYzMemberAddressTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('yz_member_address', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('uniacid')->unsigned()->index('idx_uinacid');
-			$table->integer('uid')->unsigned()->index('idx_uid');
-			$table->string('username', 20);
-			$table->string('mobile', 11);
-			$table->string('zipcode', 6);
-			$table->string('province', 32);
-			$table->string('city', 32);
-			$table->string('district', 32);
-			$table->string('street', 32);
-			$table->string('address', 512);
-			$table->boolean('isdefault');
-		});
+        if (!Schema::hasTable('yz_member_address')) {
+
+            Schema::create('yz_member_address', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('uniacid')->unsigned()->index('idx_uinacid');
+                $table->integer('uid')->unsigned()->index('idx_uid');
+                $table->string('username', 20);
+                $table->string('mobile', 11);
+                $table->string('zipcode', 6);
+                $table->string('province', 32);
+                $table->string('city', 32);
+                $table->string('district', 32);
+                $table->string('street', 32);
+                $table->string('address', 512);
+                $table->boolean('isdefault');
+            });
+        }
 	}
 
 
@@ -37,7 +39,10 @@ class CreateImsYzMemberAddressTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('ims_yz_member_address');
+        if (Schema::hasTable('ims_yz_member_address')) {
+
+            Schema::drop('ims_yz_member_address');
+        }
 	}
 
 }
--- database/migrations/2017_12_15_154004_add_street_to_ims_yz_order_address.php
+++ database/migrations/2017_12_15_154004_add_street_to_ims_yz_order_address.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStreetToImsYzOrderAddressTable extends Migration
+class AddStreetToImsYzOrderAddress extends Migration
 {
     /**
      * Run the migrations.
