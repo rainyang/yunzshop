9ab67a62e9e49059fb3c12c2708ed32f749c72a4
杨洋 <yangyang@yunzshop.com>
2017-12-29 11:00:43 +0800

    no message

--- app/frontend/modules/dispatch/listeners/prices/TemplateOrderDispatchPrice.php
+++ app/frontend/modules/dispatch/listeners/prices/TemplateOrderDispatchPrice.php
@@ -91,11 +91,11 @@ class TemplateOrderDispatchPrice
      */
     private function calculationByPiece($orderGoods)
     {
-        // 订单商品总重
+        // 件数
         $goods_total = $orderGoods->total;
 
         $piece_data = unserialize($this->dispatch->piece_data);
-        // 存在重量数据
+        // 存在
         if ($piece_data) {
             $dispatch = '';
 
@@ -116,10 +116,12 @@ class TemplateOrderDispatchPrice
             if ($dispatch) {
                 // 找到匹配的数量数据
                 if ($goods_total > $dispatch['first_piece']) {
-
-                    return $dispatch['first_piece_price'] + ($goods_total - $dispatch['another_piece']) * $dispatch['another_piece_price'];
-
-                    /*return $dispatch['first_piece_price'] + ceil(($goods_total - $dispatch['first_piece_price']) / $dispatch['first_piece_price']) * $dispatch['first_piece_price'];*/
+                    $diff = $goods_total - $dispatch['another_piece'];
+                    $another_piece = $dispatch['another_piece_price'];
+                    if ($diff > 0) {
+                        $another_piece = ceil($diff / $dispatch['another_piece']) * $dispatch['another_piece_price'];
+                    }
+                    return $dispatch['first_piece_price'] + $another_piece;
                 } else {
                     return $dispatch['first_piece_price'];
                 }
