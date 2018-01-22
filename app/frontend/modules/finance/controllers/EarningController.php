075cbf8bbceae57542deaf27565f488d6677da9a
杨洋 <yangyang@yunzshop.com>
2018-01-22 13:13:25 +0800

    no message

--- app/frontend/modules/finance/controllers/EarningController.php
+++ app/frontend/modules/finance/controllers/EarningController.php
@@ -74,7 +74,7 @@ class EarningController extends ApiController
         foreach ($config as $key => $item) {
 
             if ($item['class'] == 'Yunshop\Commission\models\CommissionOrder') {
-                dd($this->incomeModel->get());
+                dd($this->incomeModel->where('incometable_type', $item['class']));
                 dd(Income::uniacid()->where('member_id',$this->getMemberId())->whereStatus(0)->whereIncometableType($item['class'])->get());
             }
             $typeModel = $this->incomeModel->where('incometable_type', $item['class']);
