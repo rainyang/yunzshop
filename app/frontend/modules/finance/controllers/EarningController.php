79620a199fadbae3932cdf7d9141e4c3ec774818
杨洋 <yangyang@yunzshop.com>
2018-01-22 13:23:56 +0800

    no message

--- app/frontend/modules/finance/controllers/EarningController.php
+++ app/frontend/modules/finance/controllers/EarningController.php
@@ -73,7 +73,7 @@ class EarningController extends ApiController
         $array = [];
         foreach ($config as $key => $item) {
 
-            $typeModel = Income::uniacid()->where('member_id',$this->getMemberId())->whereStatus(0)->where('incometable_type', $item['class']);
+            $typeModel = $this->incomeModel->where('incometable_type', $item['class']);
             $array[] = [
                 'title' => $item['title'],
                 'ico'   => $item['ico'],
