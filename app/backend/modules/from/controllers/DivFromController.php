90144f8e46e4b9361f1d424fa00aab4b1d517e48
李宝佳 <livsyitian@163.com>
2018-01-23 11:30:01 +0800

    增加商品表单设置后台controller

---
+++ app/backend/modules/from/controllers/DivFromController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: king
+ * Date: 2018/1/23
+ * Time: 上午11:29
+ */
+
+namespace app\backend\modules\from\controllers;
+
+
+class DivFromController
+{
+
+}
