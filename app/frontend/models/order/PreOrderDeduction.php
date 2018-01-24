bd8f4eaad0379b5822b60603d4b0e2f3d98078d4
dingran80s <dingran@yunzshop.com>
2018-01-24 10:18:25 +0800

    修改积分地方去除运费

--- app/frontend/models/order/PreOrderDeduction.php
+++ app/frontend/models/order/PreOrderDeduction.php
@@ -157,7 +157,8 @@ class PreOrderDeduction extends OrderDeduction
         $orderGoodsVirtualCoin = $this->getOrderGoodsDeductionCollection()->getUsablePoint();
 
         // 商品可抵扣虚拟币+运费可抵扣虚拟币
-        $orderVirtualCoin = $orderGoodsVirtualCoin->plus($this->getDispatchPriceDeductionPoint());
+        //$orderVirtualCoin = $orderGoodsVirtualCoin->plus($this->getDispatchPriceDeductionPoint());
+        $orderVirtualCoin = $orderGoodsVirtualCoin;
 
         // 不能超过订单使用其他抵扣金额后的价格
         //$afterOtherDeductionAmount = min($this->order->price - $this->getOtherDeducitonAmount(),$orderVirtualCoin->getMoney());
