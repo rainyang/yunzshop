73948de0a0c6a3f83d6c9441af5fa5ff9c902dea
aaa <123@qq.com>
2018-02-06 10:47:45 +0800

    会员中心二维码地址修改，商品推广最多显示4个

--- app/common/services/goods/SaleGoods.php
+++ app/common/services/goods/SaleGoods.php
@@ -31,8 +31,31 @@ class SaleGoods extends Sale
                $value['is_course'] = $video_demand->isCourse($value['id']);
             }
         } else {
-            $push_goods = array();
+            return array();
         }
-        return $push_goods;
+
+        if (count($push_goods) > 4) {
+            $pushGoods = array_slice($this->shuffle_assoc($push_goods), 0, 4);
+        }
+
+        return $pushGoods;
     }
+
+     /**
+     * 打乱二维数组
+     * @param  [type] $list [description]
+     * @return [type]       [description]
+     */
+    public function shuffle_assoc($list) { 
+        if (!is_array($list)) return $list; 
+        $keys = array_keys($list); 
+        shuffle($keys); 
+
+        $random = array(); 
+
+        foreach ($keys as $key) {
+            $random[$key] = $list[$key];
+        } 
+        return $random; 
+    } 
 }
