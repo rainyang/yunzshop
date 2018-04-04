4db9beb032421bcb8d7019dfce83c6a4d81d210a
dingran <dingran80s@gmail.com>
2018-04-04 13:36:48 +0800

    修复购物车更新商品数量

--- app/frontend/modules/member/controllers/MemberCartController.php
+++ app/frontend/modules/member/controllers/MemberCartController.php
@@ -105,6 +105,11 @@ class MemberCartController extends ApiController
     {
         $cartId = request()->input('id');
         $num = request()->input('num');
+
+        if (is_null($cartId)) {
+            $cartId = $this->getMemberCarId();
+        }
+
         if ($cartId && $num) {
             $cartModel = app('OrderManager')->make('MemberCart')->find($cartId);
             if ($cartModel) {
@@ -126,6 +131,10 @@ class MemberCartController extends ApiController
     {
         $ids = explode(',', request()->input('ids'));
 
+        if (is_null($ids)) {
+            $ids = $this->getMemberCarId();
+        }
+
         $result = MemberCartService::clearCartByIds($ids);
         if ($result) {
             return $this->successJson('移除购物车成功。');
@@ -133,4 +142,26 @@ class MemberCartController extends ApiController
         throw new AppException('写入出错，移除购物车失败！');
     }
 
+    private function getMemberCarId()
+    {
+        $cartId = null;
+        $memberId = \YunShop::app()->getMemberId();
+        $goods_id = request()->input('goods_id');
+
+        if (!is_null($memberId) && !is_null($goods_id)) {
+            $cartList = app('OrderManager')->make('MemberCart')->carts()->where('member_id', $memberId)
+                ->orderBy('created_at', 'desc')
+                ->get();
+
+            if (!is_null($cartList)) {
+                $cartId = collect($cartList)->map(function ($item, $key) use ($goods_id) {
+                    if ($item->goods_id == $goods_id) {
+                        return $item->id;
+                    }
+                });
+            }
+
+            return $cartId;
+        }
+    }
 }
