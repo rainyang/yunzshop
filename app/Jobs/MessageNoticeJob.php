f8d50f28ea1746e7df8b9c31e75c396c3854979e
李宝佳 <livsyitian@163.com>
2018-04-23 10:56:38 +0800

    优化积分消息通知

--- app/Jobs/MessageNoticeJob.php
+++ app/Jobs/MessageNoticeJob.php
@@ -7,7 +7,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class MessageNoticeJob implements ShouldQueue
+class MessageNoticeJob implements  ShouldQueue
 {
 
     use InteractsWithQueue, Queueable, SerializesModels;
@@ -49,10 +49,15 @@ class MessageNoticeJob implements ShouldQueue
     /**
      * Execute the job.
      *
-     * @return void
+     * @return bool
      */
     public function handle()
     {
+        if ($this->attempts() > 2) {
+            \Log::info('消息通知测试，执行大于两次终止');
+            return true;
+        }
         $this->noticeModel->uses($this->templateId)->andData($this->noticeData)->andReceiver($this->openId)->andUrl($this->url)->send();
+        return true;
     }
 }
