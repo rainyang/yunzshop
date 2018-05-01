33f362555efefa05684c751b942bce8ae867d373
weifeng <weifeng@example.com>
2018-05-01 19:27:55 +0800

    修改

--- app/common/models/Member.php
+++ app/common/models/Member.php
@@ -497,6 +497,23 @@ class Member extends BackendModel
             ];
         }
 
+        //快递单插件开启
+        if ($plugin_class->isEnabled('courier')) {
+            //dd(123);
+            $status = \Setting::get('courier.courier.status');
+
+            $data['courier'] = [
+                'button_name' => '快递',
+                'status'         => $status ? true : false
+            ];
+        } else {
+            $data['courier'] = [
+                'button_name' => '快递',
+                'status' => false
+            ];
+        }
+
+
 
         return $data;
     }
@@ -654,5 +671,124 @@ class Member extends BackendModel
         $pid = \YunShop::request()->pid;
 
         return ($pid && ($pid != 'null' || $pid != 'undefined')) ? (int)$pid : 0;
+    }
+
+    public function getEnablePlugins()
+    {
+        $filter = [
+            'conference', 'store-cashier', 'recharge-code'
+        ];
+        $data = [];
+
+        collect(app('plugins')->getPlugins())->filter(function ($item) use ($filter) {
+            if (1 == $item->getEnabled()) {
+                $info = $item->toArray();
+
+                if (in_array($info['name'], $filter)) {
+                    return $item;
+                }
+            }
+        })->each(function ($item) use (&$data) {
+            $info = $item->toArray();
+
+            $name = $info['name'];
+            if ($info['name'] == "store-cashier") {
+
+                $name = 'store_cashier';
+            } elseif ($info['name'] == 'recharge-code') {
+                $name = 'recharge_code';
+            }
+
+
+            $data[] = [
+                'name'  => $name,
+                'title' => $info['title']
+            ];
+        });
+
+        if (app('plugins')->isEnabled('credit')) {
+            $credit_setting = Setting::get('plugin.credit');
+
+            if ($credit_setting && 1 == $credit_setting['is_credit']) {
+                $data[] = [
+                    'name' => 'credit',
+                    'title' => '信用值'
+                ];
+            }
+        }
+
+        if (app('plugins')->isEnabled('ranking')) {
+            $ranking_setting = Setting::get('plugin.ranking');
+
+            if ($ranking_setting && 1 == $ranking_setting['is_ranking']) {
+                $data[] = [
+                    'name' => 'ranking',
+                    'title' => '排行榜'
+                ];
+            }
+        }
+
+        if (app('plugins')->isEnabled('article')) {
+            $article_setting = Setting::get('plugin.article');
+
+            if ($article_setting) {
+                $data[] = [
+                    'name' => 'article',
+                    'title' => $article_setting['center'] ? $article_setting['center'] : '文章中心'
+                ];
+            }
+        }
+
+        if (app('plugins')->isEnabled('clock-in')) {
+            $clockInService = new \Yunshop\ClockIn\services\ClockInService();
+            $pluginName = $clockInService->get('plugin_name');
+
+            $clock_in_setting = Setting::get('plugin.clock_in');
+
+            if ($clock_in_setting && 1 == $clock_in_setting['is_clock_in']) {
+                $data[] = [
+                    'name' => 'clock_in',
+                    'title' => $pluginName
+                ];
+            }
+        }
+
+        if (app('plugins')->isEnabled('video_demand')) {
+
+            $video_demand_setting = Setting::get('plugin.video_demand');
+
+            if ($video_demand_setting && $video_demand_setting['is_video_demand']) {
+                $data[] = [
+                    'name' => 'video_demand',
+                    'title' => '视频点播'
+                ];
+            }
+        }
+
+        if (app('plugins')->isEnabled('help-center')) {
+
+            $help_center_setting = Setting::get('plugin.help_center');
+
+            if ($help_center_setting && 1 == $help_center_setting['status']) {
+                $data[] = [
+                    'name' => 'help_center',
+                    'title' => '帮助中心'
+                ];
+            }
+        }
+
+        if (app('plugins')->isEnabled('courier')) {
+
+            $courier_setting = Setting::get('courier.courier');
+
+            if ($courier_setting && 1 == $courier_setting['status']) {
+                $data[] = [
+                    'name' => 'courier',
+                    'title' => '快递'
+                ];
+            }
+        }
+
+        return $this->successJson('ok', $data);
     }
 }
