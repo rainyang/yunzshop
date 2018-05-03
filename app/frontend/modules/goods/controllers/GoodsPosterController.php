3f6dfed2e93c15537f7fb03459f1817f7660bc1c
wenzhiwei <wenzhiwei@yunzshop.com>
2018-05-03 15:06:08 +0800

    upgrade version is 2.0.887

--- app/frontend/modules/goods/controllers/GoodsPosterController.php
+++ app/frontend/modules/goods/controllers/GoodsPosterController.php
@@ -29,14 +29,14 @@ class GoodsPosterController extends ApiController
     // ];
 
     private $shopText = [
-        'left' => 140,
-        'top'  => 45,
+        'left' => 260,
+        'top'  => 65,
         'type' => 0,
     ];
 
     private $goodsText = [
-        'left' => 10,
-        'top' => 400,
+        'left' => 30,
+        'top' => 800,
         'type' => 1,
     ];
 
@@ -63,6 +63,52 @@ class GoodsPosterController extends ApiController
 
     }
 
+
+    /**
+    * 圆角图片
+    * @param $radius 角度
+    * @return img
+    */
+    public function get_lt_rounder_corner($radius) {  
+        $img     = imagecreatetruecolor($radius, $radius);  // 创建一个正方形的图像  
+        $bgcolor    = imagecolorallocate($img, 1, 1, 1);   // 图像的背景  
+        $fgcolor    = imagecolorallocate($img, 0, 0, 0);  
+        imagefill($img, 0, 0, $bgcolor);  
+        // $radius,$radius：以图像的右下角开始画弧  
+        // $radius*2, $radius*2：已宽度、高度画弧  
+        // 180, 270：指定了角度的起始和结束点  
+        // fgcolor：指定颜色  
+        imagefilledarc($img, $radius, $radius, $radius*2, $radius*2, 180, 270, $fgcolor, IMG_ARC_PIE);  
+        // 将弧角图片的颜色设置为透明  
+        imagecolortransparent($img, $fgcolor);  
+        return $img;  
+    }
+
+    public function roundRadius($resource, $image_width, $image_height, $radius = 8)
+    {
+        // lt(左上角)  
+        $lt_corner  = $this->get_lt_rounder_corner($radius);  
+
+        // header('Content-Type: image/png');  
+        // imagepng($lt_corner);  
+        // exit;  
+        imagecopymerge($resource, $lt_corner, 0, 0, 0, 0, $radius, $radius, 100);  
+        // lb(左下角)  
+        $lb_corner  = imagerotate($lt_corner, 90, 0);  
+        imagecopymerge($resource, $lb_corner, 0, $image_height - $radius, 0, 0, $radius, $radius, 100);  
+        // rb(右上角)  
+        $rb_corner  = imagerotate($lt_corner, 180, 0);  
+        imagecopymerge($resource, $rb_corner, $image_width - $radius, $image_height - $radius, 0, 0, $radius, $radius, 100);  
+        // rt(右下角)  
+        $rt_corner  = imagerotate($lt_corner, 270, 0);  
+        imagecopymerge($resource, $rt_corner, $image_width - $radius, 0, 0, 0, $radius, $radius, 100);  
+      
+        // header('Content-Type: image/png');  
+        // imagepng($resource);  
+        // exit;  
+        return $resource;
+    }
+
     /**
      * 生成商品海报
      * @return string 商品海报绝对路径
@@ -70,16 +116,22 @@ class GoodsPosterController extends ApiController
     public function get_lt()
     {   
 
+
         set_time_limit(0);
         @ini_set('memory_limit', '256M');
 
-        $target = imagecreatetruecolor(335, 485);
+        $image_width = 600; //335
+        $image_height = 1000; //485
+
+        $target = imagecreatetruecolor($image_width, $image_height);
         $white  = imagecolorallocate($target, 255, 255, 255);
         $color  = imagecolorallocate($target, 226, 226, 226);
         //设置白色背景色
         imagefill($target,0,0,$white);
         //设置线条
-        imageline( $target, 0, 60, 485, 60, $color);
+        imageline( $target, 0, 100, $image_width, 100, $color);
+
+        $target = $this->roundRadius($target, $image_width, $image_height);
 
         $shopLogo = imagecreatefromstring(\Curl::to(yz_tomedia($this->shopSet['logo']))->get());
         if ($this->goodsModel->hasOneShare->share_thumb) {
@@ -109,14 +161,12 @@ class GoodsPosterController extends ApiController
         $target = $this->mergeText($target, $this->shopText, $this->shopSet['name']);
 
         $target = $this->mergePriceText($target);
-
-
+       
         $target = $this->mergeQrImage($target, $goodsQr);
 
         // header ( "Content-type: image/png" );
         // imagePng ( $target );
         // exit();
-
 
         imagepng($target, $this->getGoodsPosterPath());
         imagedestroy($target);
@@ -147,8 +197,9 @@ class GoodsPosterController extends ApiController
         $width  = imagesx($img);
         $height = imagesy($img);
 
-        imagecopyresized($target, $img, 31.5, 80, 0, 0, 272, 272, $width, $height);
+        imagecopyresized($target, $img, 30, 180, 0, 0, 540, 540, $width, $height);
         imagedestroy($img);
+
 
         return $target;
     }
@@ -162,7 +213,7 @@ class GoodsPosterController extends ApiController
     {
         $width  = imagesx($img);
         $height = imagesy($img);
-        imagecopyresized($target, $img, 102, 20, 0, 0, 31, 31, $width, $height);
+        imagecopyresized($target, $img, 200, 25, 0, 0, 50, 50, $width, $height);
         imagedestroy($img);
 
         return $target;
@@ -177,7 +228,7 @@ class GoodsPosterController extends ApiController
     {
         $width  = imagesx($img);
         $height = imagesy($img);
-        imagecopy($target, $img, 210, 360, 0, 0, $width, $height);
+        imagecopy($target, $img, 400, 750, 0, 0, $width, $height);
         imagedestroy($img);
 
         return $target;
@@ -198,12 +249,11 @@ class GoodsPosterController extends ApiController
         // $font="c:/windows/fonts/simhei.ttf";
 
         if ($params['type']) {
-            $text = $this->autowrap(14, 0, $font, $text, 187);
+            $text = $this->autowrap(30, 0, $font, $text, 320);
         }
 
         $black = imagecolorallocate($target,  51, 51, 51);//文字颜色
-        imagettftext($target, 14, 0, $params['left'], $params['top'], $black, $font, $text);
-
+        imagettftext($target, 30, 0, $params['left'], $params['top'], $black, $font, $text);
 
         return $target;
     }
@@ -223,19 +273,19 @@ class GoodsPosterController extends ApiController
 
         // $font="c:/windows/fonts/simhei.ttf";
             
-        $price = '￥'.$this->goodsModel->price;
-        $market_price = '￥'.$this->goodsModel->market_price;
+        $price = '现价:￥'.$this->goodsModel->price;
+        $market_price = '原价:￥'.$this->goodsModel->market_price;
         $black = imagecolorallocate($target, 241,83,83);//当前价格颜色
 
         $price_box = imagettfbbox(18, 0, $font, $price);
-        $market_price_box = imagettfbbox(12, 0, $font, $market_price);
+        $market_price_box = imagettfbbox(24, 0, $font, $market_price);
         $gray = imagecolorallocate($target, 107,107,107);//原价颜色
 
         //设置删除线条
-        imageline($target, $price_box[2] + 12, 458, $price_box[2]+$market_price_box[2] + 14, 460, $color);
+        // imageline($target, $price_box[2] + 12, 900, $price_box[2]+$market_price_box[2] + 14, 900, $color);
 
-        imagettftext($target, 18, 0, 5, 465, $black, $font, $price);
-        imagettftext($target, 12, 0, $price_box[2]+10, 465, $gray, $font, $market_price);
+        imagettftext($target, 24, 0, 30, 910, $black, $font, $price);
+        imagettftext($target, 16, 0, 30, 940, $gray, $font, $market_price);
 
         return $target;
         
@@ -258,7 +308,7 @@ class GoodsPosterController extends ApiController
 
         if (!is_file($path.'/'.$file)) {
 
-            \QrCode::format('png')->size(120)->generate($url, $path.'/'.$file);
+            \QrCode::format('png')->size(200)->generate($url, $path.'/'.$file);
         }
         $img = imagecreatefromstring(file_get_contents($path.'/'.$file));
         // unlink($path.'/'.$file);
