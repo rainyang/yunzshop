81e74aaff87e7e12bb0e642d86ed4b28913d53ee
fbc <773928614@qq.com>
2018-06-08 11:20:45 +0800

    提示开启失败信息

--- app/backend/modules/setting/controllers/DefaultNoticeController.php
+++ app/backend/modules/setting/controllers/DefaultNoticeController.php
@@ -44,6 +44,12 @@ class DefaultNoticeController extends BaseController
             $template_data = $this->TemplateModel->getData($template_id_short);
             if (!$template_data) {
                 $template_id = $this->WechatApiModel->getTemplateIdByTemplateIdShort($template_id_short);
+                if (empty($template_id)) {
+                    echo json_encode([
+                        'result' => '0',
+                        'msg' => '获取微信模版失败',
+                    ]);
+                }
                 $this->TemplateModel->template_id_short = $template_id_short;
                 $this->TemplateModel->template_id = $template_id;
                 $this->TemplateModel->uniacid = \YunShop::app()->uniacid;
--- resources/views/setting/shop/notice.blade.php
+++ resources/views/setting/shop/notice.blade.php
@@ -747,9 +747,11 @@
                             if ($(id).is(':checked')) {
                                 //开
                                 $.post(url_open,postdata,function(data){
-                                    if (data) {
+                                    if (data.result == 1) {
                                         $(select_name).find("option:selected").val(data.id)
                                         showPopover($(id),"开启成功")
+                                    } else {
+                                        showPopover($(id),"开启失败，请检查微信模版")
                                     }
                                 }, "json");
                             } else {
