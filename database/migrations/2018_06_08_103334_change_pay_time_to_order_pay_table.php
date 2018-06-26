2c0eb0ed7c26cdd50f85f17ddf065991c0e7624b
沈阳 <564345292@qq.com>
2018-06-26 13:41:35 +0800

    调试migration

--- database/migrations/2018_06_08_103334_change_pay_time_to_order_pay_table.php
+++ database/migrations/2018_06_08_103334_change_pay_time_to_order_pay_table.php
@@ -18,8 +18,8 @@ class ChangePayTimeToOrderPayTable extends Migration
                 if (Schema::hasColumn('yz_order_pay', 'pay_time')) {
                     $table->integer('pay_time')->nullable()->change();
                     $table->integer('refund_time')->nullable()->change();
-                    DB::select('update '.app('db')->getTablePrefix().'yz_order_pay set refund_time = null where refund_time = 0');
-                    DB::select('update '.app('db')->getTablePrefix().'yz_order_pay set pay_time = null where pay_time = 0');
+                    //DB::select('update '.app('db')->getTablePrefix().'yz_order_pay set refund_time = null where refund_time = 0');
+                    //DB::select('update '.app('db')->getTablePrefix().'yz_order_pay set pay_time = null where pay_time = 0');
                 }
             });
         }
