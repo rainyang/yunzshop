0c80bf794c34af6f3b312dae8cb37b98da4fcfef
blank <baidu.com>
2018-06-26 17:24:38 +0800

    no messge

--- app/frontend/modules/goods/controllers/GoodsController.php
+++ app/frontend/modules/goods/controllers/GoodsController.php
@@ -74,7 +74,7 @@ class GoodsController extends ApiController
             $goodsModel->hasOneBrand->logo = yz_tomedia($goodsModel->hasOneBrand->logo);
         }
         //商品规格图片处理
-        if ($goodsModel->hasManyOptions->toArray()) {
+        if ($goodsModel->hasManyOptions && $goodsModel->hasManyOptions->toArray()) {
             foreach ($goodsModel->hasManyOptions as &$item) {
                 $item->thumb = replace_yunshop(yz_tomedia($item->thumb));
             }
