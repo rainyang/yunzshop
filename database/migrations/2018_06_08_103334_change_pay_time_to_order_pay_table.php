ca3a43cbcd0332070a7355de0541df1580d48c4c
沈阳 <564345292@qq.com>
2018-06-26 14:36:18 +0800

    添加支付方式的migrations

--- database/migrations/2018_06_08_103334_change_pay_time_to_order_pay_table.php
+++ database/migrations/2018_06_08_103334_change_pay_time_to_order_pay_table.php
@@ -13,6 +13,7 @@ class ChangePayTimeToOrderPayTable extends Migration
      */
     public function up()
     {
+        // todo 在dev3上调好
 //        if (Schema::hasTable('yz_order_pay')) {
 //            Schema::table('yz_order_pay', function (Blueprint $table) {
 //                if (Schema::hasColumn('yz_order_pay', 'pay_time')) {
