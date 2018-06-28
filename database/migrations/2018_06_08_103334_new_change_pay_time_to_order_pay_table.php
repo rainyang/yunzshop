6e91544d2c88c51b4242bd4c38b947a4de020123
沈阳 <564345292@qq.com>
2018-06-28 17:54:45 +0800

    修复订单支付表时间

--- database/migrations/2018_06_08_103334_new_change_pay_time_to_order_pay_table.php
+++ database/migrations/2018_06_08_103334_new_change_pay_time_to_order_pay_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangePayTimeToOrderPayTable extends Migration
+class NewChangePayTimeToOrderPayTable extends Migration
 {
     /**
      * Run the migrations.
