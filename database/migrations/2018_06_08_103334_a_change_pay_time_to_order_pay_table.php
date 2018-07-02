1ddb6fda76debaf9553cd36b644145b2da27cc0e
沈阳 <564345292@qq.com>
2018-07-02 18:11:45 +0800

    修复订单支付表时间字段

--- database/migrations/2018_06_08_103334_a_change_pay_time_to_order_pay_table.php
+++ database/migrations/2018_06_08_103334_a_change_pay_time_to_order_pay_table.php
@@ -15,9 +15,11 @@ class AChangePayTimeToOrderPayTable extends Migration
     {
         if (Schema::hasTable('yz_order_pay')) {
             Schema::table('yz_order_pay', function (Blueprint $table) {
-                $table->integer('pay_time')->nullable()->change();
-                $table->integer('refund_time')->nullable()->change();
+                DB::select("ALTER TABLE `".app('db')->getTablePrefix().$table->getTable()."` CHANGE `refund_time` `refund_time` INT(11)  NULL  DEFAULT NULL");
+                DB::select("ALTER TABLE `".app('db')->getTablePrefix().$table->getTable()."` CHANGE `pay_time` `pay_time` INT(11)  NULL  DEFAULT NULL");
+
             });
+
             DB::select('update ' . app('db')->getTablePrefix() . 'yz_order_pay set refund_time = null where refund_time = 0');
             DB::select('update ' . app('db')->getTablePrefix() . 'yz_order_pay set pay_time = null where pay_time = 0');
         }
