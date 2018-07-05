2ec06bf6de87cc4708ae05a771eec14fcfdee2e6
李宝佳 <livsyitian@163.com>
2018-07-05 09:07:03 +0800

    提现列表增加 环迅、EUP提现方式
    提现详情增加 环迅、EUP打款

--- resources/views/finance/withdraw/withdraw-info.blade.php
+++ resources/views/finance/withdraw/withdraw-info.blade.php
@@ -241,6 +241,16 @@
                     <input type="submit" name="submit_pay" value="打款到支付宝"
                            class="btn btn-primary " style='margin-left:10px;'
                            onclick='return '/>
+                @elseif($item->pay_way == 'hunxun')
+                    <input type="hidden" name="pay_way" value="2">
+                    <input type="submit" name="submit_pay" value="打款到银行卡"
+                           class="btn btn-primary " style='margin-left:10px;'
+                           onclick='return '/>
+                @elseif($item->pay_way == 'eup_pay')
+                    <input type="hidden" name="pay_way" value="2">
+                    <input type="submit" name="submit_pay" value="EUP打款"
+                           class="btn btn-primary " style='margin-left:10px;'
+                           onclick='return '/>
                 @elseif($item->pay_way == 'manual')
                     <input type="hidden" name="pay_way" value="4">
                     <input type="submit" name="submit_pay" value="手动打款"
