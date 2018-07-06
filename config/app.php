82673c1cfabef31e63ab84d0f147e9d31ffd5797
沈阳 <564345292@qq.com>
2018-07-06 13:38:42 +0800

    添加laravel debug插件

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -256,6 +256,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
         'Setting' => app\common\facades\Setting::class,//设置
         'Option'    => app\common\facades\Option::class,
