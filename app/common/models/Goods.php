5c767e966b81c44b2df1a7671bc60dbeae199606
fbc <773928614@qq.com>
2018-07-23 15:47:03 +0800

    优化代码

--- app/common/models/Goods.php
+++ app/common/models/Goods.php
@@ -309,7 +309,9 @@ class Goods extends BaseModel
             //->where('is_plugin', 0)
             ->whereNotIn('plugin_id', [20,31,60])//屏蔽门店、码上点餐、第三方插件接口的虚拟商品
             ->get();
-    }/**
+    }
+
+    /**
      * @param $keyword
      * @return mixed
      */
