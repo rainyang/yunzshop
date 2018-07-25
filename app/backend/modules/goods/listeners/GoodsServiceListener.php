6f29b8b31929ada18300b6eb62fad6997cef4e17
blank <baidu.com>
2018-07-25 17:40:32 +0800

    商品上下架定时任务

--- app/backend/modules/goods/listeners/GoodsServiceListener.php
+++ app/backend/modules/goods/listeners/GoodsServiceListener.php
@@ -6,7 +6,6 @@ use app\common\models\Goods;
 use app\common\models\UniAccount;
 use Illuminate\Contracts\Events\Dispatcher;
 use app\common\models\goods\GoodsService;
-use app\common\models\Goods;
 
 /**
 * 
@@ -17,53 +16,49 @@ class GoodsServiceListener
 	public function subscribe(Dispatcher $events)
 	{
 		$events->listen('cron.collectJobs', function () {
-			$UniAccount = UniAccount::get();
-
-			foreach ($UniAccount as $u) {
-				\Yunshop::app()->uniacid = $u->uniacid;
-				\Setting::$uniqueAccountId = $uniacid = $u->uniacid;
-
-                \Log::info("--商品自动上下架--");
-
-                \Cron::add("upperLowerShelves{$u->uniacid}", '*/10 * * * * *', function() use ($uniacid) {
-                	$this->handle($uniacid);
-            	});
-
-			}
-		})
+		    \Cron::add('upperLowerShelves', '*/10 * * * * *', function() {
+		        $this->handle();
+		    });
+		});
 	}
 
-	public function handle($uniacid)
+	public function handle()
 	{
-		\YunShop::app()->uniacid = $uniacid;
-        \Setting::$uniqueAccountId = $uniacid;
 
-        $goods = Goods::select(['id', 'status'])->whereHas('hasOneGoodsService', function ($query) {
-        	return $query->where('is_automatic', 1);
-        })->with(['hasOneGoodsService' => function ($query2) {
-            return $query2->select(['goods_id', 'on_shelf_time', 'lower_shelf_time']);
-        }])->get();
+        $uniAccount = UniAccount::get() ?: [];
 
+        foreach ($uniAccount as $u) {
+            \YunShop::app()->uniacid = $u->uniacid;
+            \Setting::$uniqueAccountId = $u->uniacid;
 
-        if ($goods) {
-        	$current_time = time();
-        	foreach ($goods as $key => $item) {
-        		//上架
-        		if ($item->hasOneGoodsService->on_shelf_time < $current_time) {
-        			if ($item->status == 0) {
-        				$item->status = 1;
-        				$item->save();
-        			}
-        		}
+            \Log::debug('-----------商品定时上下架-----------------uniacid:'.\YunShop::app()->uniacid);
 
-        		//下架
-        		if ($item->hasOneGoodsService->lower_shelf_time < $current_time) {
-        			if ($item->status == 1) {
-        				$item->status = 0;
-        				$item->save();
-        			}
-        		}
-        	}
+            $goods = Goods::select(['id', 'status'])->whereHas('hasOneGoodsService', function ($query) {
+                return $query->where('is_automatic', 1);
+            })->with(['hasOneGoodsService' => function ($query2) {
+                return $query2->select(['goods_id', 'on_shelf_time', 'lower_shelf_time']);
+            }])->get();
+
+            if ($goods) {
+                $current_time = time();
+                foreach ($goods as $key => $item) {
+                    //上架
+                    if ($item->hasOneGoodsService->on_shelf_time < $current_time) {
+                        if ($item->status == 0) {
+                            $item->status = 1;
+                            $item->save();
+                        }
+                    }
+
+                    //下架
+                    if ($item->hasOneGoodsService->lower_shelf_time < $current_time) {
+                        if ($item->status == 1) {
+                            $item->status = 0;
+                            $item->save();
+                        }
+                    }
+                }
+            }
         }
 
 	}
