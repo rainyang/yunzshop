ad4d99a3bda46d38a4b88a1b911f05320ba84698
沈阳 <564345292@qq.com>
2018-08-09 15:13:09 +0800

    地址区域列表

--- app/backend/modules/area/controllers/ListController.php
+++ app/backend/modules/area/controllers/ListController.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace app\backend\modules\area\controllers;
 
 use app\backend\modules\area\models\Area;
 use app\common\components\BaseController;
+use app\common\models\Street;
+use Illuminate\Database\Eloquent\Collection;
 
 /**
  * Created by PhpStorm.
@@ -15,8 +18,37 @@ class ListController extends BaseController
 
     public function index()
     {
-        $cities = Area::getAreasByCity(\YunShop::request()->parent_id);
-        return $this->successJson('成功',$cities);
-    }
+        if($this->isStreet(request('parent_id'))){
+            $areas = Street::where('parentid',request('parent_id'))->get();
+        }else{
+            $areas = Area::where('parentid',request('parent_id'))->get();
+        }
 
+        return $this->successJson('成功', $this->formatAreas($areas));
+    }
+    private function isStreet($areaId){
+        if($areaId == 0){
+            return false;
+        }
+        $area = Area::find(request('parent_id'));
+
+        if(!$area){
+            return false;
+        }
+        return $area->level == 3;
+    }
+    private function formatAreas(Collection $areas)
+    {
+        if($areas->isEmpty()){
+            return $areas;
+        }
+        // 不是子节点
+        if (!$areas->first()->isLeaf()) {
+            $areas->each(function (Area $area) {
+                $area['children'] = [];
+            });
+        }
+        return $areas;
+
+    }
 }
