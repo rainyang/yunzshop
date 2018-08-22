53d15df14b4c67e6982a2e64182f2876969e34e3
沈阳 <564345292@qq.com>
2018-08-22 16:00:56 +0800

    no message

--- app/frontend/modules/order/services/behavior/OrderSend.php
+++ app/frontend/modules/order/services/behavior/OrderSend.php
@@ -42,9 +42,10 @@ class OrderSend extends ChangeStatusOperation
             $db_express_model->express_code = request()->input('express_code','');
 
             $db_express_model->express_company_name = request()->input('express_company_name', function (){
-                return array_get((new ExpressCompany())->where('code',request()->input('express_code'))->first(),'express_company_name','');
+                return array_get((new ExpressCompany())->where('code',request()->input('express_code',''))->first(),'express_company_name','');
             });
-            $db_express_model->express_sn = request()->input('express_sn');
+            $db_express_model->express_sn = request()->input('express_sn','');
+
             $db_express_model->save();
         }
         parent::updateTable();
