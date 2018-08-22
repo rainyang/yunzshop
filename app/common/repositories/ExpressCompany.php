953f16bc4b8c63b3e95f81bbf0087b4ae11c867d
沈阳 <564345292@qq.com>
2018-08-22 11:50:53 +0800

    快递公司列表

--- app/common/repositories/ExpressCompany.php
+++ app/common/repositories/ExpressCompany.php
@@ -14,68 +14,10 @@ class ExpressCompany extends Collection
 {
     public function __construct($items = [])
     {
-        $items = array (
-            0 =>
-                array (
-                    'name' => '申通',
-                    'code' => 'shentong',
-                ),
-            1 =>
-                array (
-                    'name' => '圆通',
-                    'code' => 'yuantong',
-                ),
-            2 =>
-                array (
-                    'name' => '中通',
-                    'code' => 'zhongtong',
-                ),
-            3 =>
-                array (
-                    'name' => '汇通',
-                    'code' => 'huitongkuaidi',
-                ),
-            4 =>
-                array (
-                    'name' => '韵达',
-                    'code' => 'yunda',
-                ),
-            5 =>
-                array (
-                    'name' => '顺丰',
-                    'code' => 'shunfeng',
-                ),
-            6 =>
-                array (
-                    'name' => 'ems',
-                    'code' => 'ems',
-                ),
-            7 =>
-                array (
-                    'name' => '天天',
-                    'code' => 'tiantian',
-                ),
-            8 =>
-                array (
-                    'name' => '宅急送',
-                    'code' => 'zhaijisong',
-                ),
-            9 =>
-                array (
-                    'name' => '邮政',
-                    'code' => 'youzhengguonei',
-                ),
-            10 =>
-                array (
-                    'name' => '德邦',
-                    'code' => 'debangwuliu',
-                ),
-            11 =>
-                array (
-                    'name' => '全峰',
-                    'code' => 'quanfengkuaidi',
-                ),
-        );
+        $file = implode([app()->path(),'..','static','source','expresscom.json'],DIRECTORY_SEPARATOR);
+        $json = file_get_contents($file);
+
+        $items = json_decode($json,true);
         parent::__construct($items);
     }
 
