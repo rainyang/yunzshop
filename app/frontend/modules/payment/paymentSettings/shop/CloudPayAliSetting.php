08319f3a547944a231fa0919b1a3b75f2261d1bf
dingran <dingran80s@gmail.com>
2018-08-27 16:38:38 +0800

    支付显示类型

--- app/frontend/modules/payment/paymentSettings/shop/CloudPayAliSetting.php
+++ app/frontend/modules/payment/paymentSettings/shop/CloudPayAliSetting.php
@@ -14,7 +14,7 @@ class CloudPayAliSetting extends BaseSetting
     {
         $set = \Setting::get('plugin.cloud_pay_set');
 
-        return \YunShop::request()->type != 7 && !is_null($set) && 1 == $set['switch'];
+        return \YunShop::request()->type != 7 && !is_null($set) && 1 == $set['switch_alipay'];
     }
     public function exist()
     {
