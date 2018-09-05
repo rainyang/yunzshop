6d36ecfb9ab33dfc4473c52716b455848fb6cae8
weifeng <519570979@qq.com>
2018-09-05 15:57:20 +0800

    添加监听统计定时任务

---
+++ app/backend/modules/charts/listeners/Statistics.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/5
+ * Time: 15:47
+ */
+
+namespace app\backend\modules\charts\listeners;
+
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+
+class Statistics
+{
+    use DispatchesJobs;
+
+    public function subscribe()
+    {
+        \Event::listen('cron.collectJobs', function () {
+            \Cron::add('Statistics', '*/10 * * * * *', function () {
+                (new \app\common\services\statistics\TimedTaskService())->handle();
+                return;
+            });
+        });
+    }
+}
