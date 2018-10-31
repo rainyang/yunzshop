b719b62b11f684a6b0a2675a0108112bc4b71197
fbc <773928614@qq.com>
2018-10-31 11:11:41 +0800

    no message

--- app/backend/modules/charts/modules/income/controllers/ShopIncomeStatisticsController.php
+++ app/backend/modules/charts/modules/income/controllers/ShopIncomeStatisticsController.php
@@ -10,8 +10,13 @@ namespace app\backend\modules\charts\modules\income\controllers;
 
 use app\common\components\BaseController;
 use app\common\helpers\PaginationHelper;
+use app\backend\modules\charts\models\Order;
 use app\common\models\order\OrderPluginBonus;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use Yunshop\StoreCashier\common\models\CashierOrder;
+use Yunshop\StoreCashier\common\models\StoreOrder;
+use Yunshop\Supplier\common\models\SupplierOrder;
 
 class ShopIncomeStatisticsController extends BaseController
 {
@@ -24,7 +29,7 @@ class ShopIncomeStatisticsController extends BaseController
 
         $pageSize = 10;
         $search = \YunShop::request()->search;
-        $list = OrderPluginBonus::search($search)
+        $list = Order::search($search)
             ->selectRaw('sum(undividend) as undividend, FROM_UNIXTIME(created_at,"%Y-%m-%d")as date, sum(price) as price')
             ->groupBy(DB::raw("FROM_UNIXTIME(UNIX_TIMESTAMP(created_at),'%Y-%m-%d')"))
             ->with([
@@ -48,5 +53,54 @@ class ShopIncomeStatisticsController extends BaseController
             'pager' => $pager,
             'search' => $search,
         ])->render();
+    }
+
+    public function statistics()
+    {
+        $pageSize = 10;
+        $search = \YunShop::request()->search;
+        $list = Order::uniacid()
+            ->where('status', 3)
+            ->selectRaw('FROM_UNIXTIME(created_at,"%Y-%m-%d")as date, sum(price) as price')
+            ->groupBy(DB::raw("FROM_UNIXTIME(UNIX_TIMESTAMP(created_at),'%Y-%m-%d')"))
+            ->with([
+                'hasOneOrderGoods' => function($q) {
+                    $q->selectRaw('sum(goods_cost_price) as cost_price, order_id')->groupBy('order_id');
+                },
+                'hasManyStoreOrder',
+                'hasManySupplierOrder',
+                'hasManyCashierOrder',
+                'hasManyOrderPluginBonus'
+            ])
+//            ->orderBy('order_id', 'desc')
+            ->get()->toArray();
+//            ->paginate($pageSize);
+        dd($list);
+
+
+
+        $pager = PaginationHelper::show($list->total(), $list->currentPage(), $list->perPage());
+        return view('charts.income.shop_income_statistics',[
+            'list' => $list,
+            'pager' => $pager,
+            'search' => $search,
+        ])->render();
+    }
+
+    /**
+     *
+     */
+    public function count()
+    {
+        $date_start = Carbon::now()->yesterday()->startOfDay()->getTimestamp();
+
+        $date_end = Carbon::now()->yesterday()->endOfDay()->getTimestamp();
+
+        $shopOrder = Order::uniacid()->whereIn('finish_at', [$date_start, $date_end])->where('status',3)->sum('price');
+        $storeOrder = storeOrder::uniacid()->whereIn('created_at', [$date_start, $date_end])->where('has_settlement', 1)->sum('amount');
+        $supplierOrder = cashierOrder::uniacid()->whereIn('created_at', [$date_start, $date_end])->where('has_settlement', 1)->sum('amount');
+        $supplierOrder = supplierOrder::uniacid()->whereIn('created_at', [$date_start, $date_end])->sum('supplier_profit');
+        $undividend = OrderPluginBonus::uniacid()->whereIn('created_at', [$date_start, $date_end])->where('status', 1)->sum('undividend');
+        dd($date_end);
     }
 }
