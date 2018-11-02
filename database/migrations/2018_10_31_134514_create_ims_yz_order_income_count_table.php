bdd12523600eb767ac1b3dfa54978aecaa85fbed
fbc <773928614@qq.com>
2018-11-02 11:42:02 +0800

    增加索引

--- database/migrations/2018_10_31_134514_create_ims_yz_order_income_count_table.php
+++ database/migrations/2018_10_31_134514_create_ims_yz_order_income_count_table.php
@@ -47,7 +47,7 @@ class CreateImsYzOrderIncomeCountTable extends Migration {
                 $table->integer('created_at')->nullable();
                 $table->integer('updated_at')->nullable();
                 $table->integer('deleted_at')->nullable();
-                $table->index(['uniacid', 'uid', 'order_id', 'order_sn', 'status', 'day_time', 'parent_id']);
+                $table->index(['uniacid', 'uid', 'order_id', 'order_sn', 'status', 'day_time', 'parent_id'], 'idx_order');
             });
         }
 	}
