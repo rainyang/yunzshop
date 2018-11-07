f3486f53e6d7a8893e31446519d271de02de1269
weifeng <519570979@qq.com>
2018-11-07 18:37:01 +0800

    优化

--- app/frontend/modules/member/controllers/RegisterController.php
+++ app/frontend/modules/member/controllers/RegisterController.php
@@ -367,6 +367,14 @@ class RegisterController extends ApiController
     {
         $sms = \Setting::get('shop.sms');
 
+        //增加验证码验证
+        $captcha_status = Setting::get('shop.sms.status');
+        if ($captcha_status == 1) {
+            if ( app('captcha')->check(Input::get('captcha')) == false) {
+                return $this->errorJson('验证码错误');
+            }
+        }
+        
         //互亿无线
         if ($sms['type'] == 1) {
             if ($state != '86') {
