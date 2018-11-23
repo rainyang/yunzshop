b2fc0e7bbbe99e6a908f6866263acc2fcdee2523
weifeng <519570979@qq.com>
2018-11-23 14:12:44 +0800

    判断公众号

--- app/backend/modules/charts/modules/order/services/OrderStatisticsService.php
+++ app/backend/modules/charts/modules/order/services/OrderStatisticsService.php
@@ -23,14 +23,14 @@ class OrderStatisticsService
             \Setting::$uniqueAccountId = $u->uniacid;
 
             //全部
-            $order_all_count = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('count(1) as total_quantity'))->groupBy('uid')->get()->toArray());
-            $order_all_money = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('sum(price) as total_amount'))->groupBy('uid')->get()->toArray());
+            $order_all_count = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('count(1) as total_quantity'))->groupBy('uid')->where('uniacid', \YunShop::app()->uniacid)->get()->toArray());
+            $order_all_money = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('sum(price) as total_amount'))->groupBy('uid')->where('uniacid', \YunShop::app()->uniacid)->get()->toArray());
             //已支付
-            $order_pay_count = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('count(1) as total_pay_quantity'))->whereIn('status', [1,2,3])->groupBy('uid')->get()->toArray());
-            $order_pay_money = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('sum(price) as total_pay_amount'))->whereIn('status', [1,2,3])->groupBy('uid')->get()->toArray());
+            $order_pay_count = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('count(1) as total_pay_quantity'))->whereIn('status', [1,2,3])->groupBy('uid')->where('uniacid', \YunShop::app()->uniacid)->get()->toArray());
+            $order_pay_money = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('sum(price) as total_pay_amount'))->whereIn('status', [1,2,3])->groupBy('uid')->where('uniacid', \YunShop::app()->uniacid)->get()->toArray());
             //已完成
-            $order_complete_count = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('count(1) as total_complete_quantity'))->where('status', 3)->groupBy('uid')->get()->toArray());
-            $order_complete_money = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('sum(price) as total_complete_amount'))->where('status', 3)->groupBy('uid')->get()->toArray());
+            $order_complete_count = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('count(1) as total_complete_quantity'))->where('status', 3)->groupBy('uid')->where('uniacid', \YunShop::app()->uniacid)->get()->toArray());
+            $order_complete_money = collect(DB::table('yz_order')->select('uid','uniacid', DB::raw('sum(price) as total_complete_amount'))->where('status', 3)->groupBy('uid')->where('uniacid', \YunShop::app()->uniacid)->get()->toArray());
             $result = $order_all_count->map(function(Collection $order) use($order_all_money,$order_pay_count,$order_pay_money,$order_complete_count,$order_complete_money){
                 $order =  $this->mergeOrderData($order,$order_all_money);
                 $order =  $this->mergeOrderData($order,$order_pay_count);
