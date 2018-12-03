ecb7e23d766a8bddededaad88879e85b54d2fd9f
blank <baidu.com>
2018-12-03 14:45:18 +0800

    no message

--- app/backend/modules/member/controllers/PopularizePageShowController.php
+++ app/backend/modules/member/controllers/PopularizePageShowController.php
@@ -177,13 +177,27 @@ class PopularizePageShowController extends BaseController
     protected function toJson()
     {
         $all_set =  Setting::getByGroup("popularize");
-
         $data = [
-            'wechat' => !empty($all_set['wechat']['vue_route'])?$all_set['wechat']['vue_route']:[],
-            'mini' => !empty($all_set['mini']['vue_route'])?$all_set['mini']['vue_route']:[],
-            'wap' => !empty($all_set['wap']['vue_route'])?$all_set['wap']['vue_route']:[],
-            'app' => !empty($all_set['app']['vue_route'])?$all_set['app']['vue_route']:[],
-            'alipay' => !empty($all_set['alipay']['vue_route'])?$all_set['alipay']['vue_route']:[],
+            'wechat' => [
+                'vue_route' => !empty($all_set['wechat']['vue_route'])?$all_set['wechat']['vue_route']:[],
+                'url' => !empty($all_set['wechat']['callback_url'])?$all_set['wechat']['callback_url']:'',
+            ],
+            'mini' => [
+                'vue_route' => !empty($all_set['mini']['vue_route'])?$all_set['mini']['vue_route']:[],
+                'url' => !empty($all_set['mini']['callback_url'])?$all_set['mini']['callback_url']:'',
+            ],
+            'wap' => [
+                'vue_route' => !empty($all_set['wap']['vue_route'])?$all_set['wap']['vue_route']:[],
+                'url' => !empty($all_set['wap']['callback_url'])?$all_set['wap']['callback_url']:'',
+            ],
+            'app' => [
+                'vue_route' => !empty($all_set['app']['vue_route'])?$all_set['app']['vue_route']:[],
+                'url' => !empty($all_set['app']['callback_url'])?$all_set['app']['callback_url']:'',
+            ],
+            'alipay' => [
+                'vue_route' => !empty($all_set['alipay']['vue_route'])?$all_set['alipay']['vue_route']:[],
+                'url' => !empty($all_set['alipay']['callback_url'])?$all_set['alipay']['callback_url']:'',
+            ],
         ];
         $string =  json_encode($data);
         $sj = date('Y-m-d H:i:s', time());
--- static/yunshop/js/popularize/popularize.js
+++ static/yunshop/js/popularize/popularize.js
@@ -1,5 +1,5 @@
-//update 2018-12-03 11:47:41
-let popularize = {"wechat":[],"mini":[],"wap":[],"app":[],"alipay":[]};
+//update 2018-12-03 14:44:29
+let popularize = {"wechat":{"vue_route":[],"url":""},"mini":{"vue_route":[],"url":""},"wap":{"vue_route":[],"url":""},"app":{"vue_route":[],"url":""},"alipay":{"vue_route":[],"url":""}};
 if (typeof define === "function") {
     define(popularize)
 } else {
