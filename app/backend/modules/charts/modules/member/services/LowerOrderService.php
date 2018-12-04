2a0c1bd07cd883839faa546ecf293493dfffa72f
weifeng <519570979@qq.com>
2018-12-04 10:54:20 +0800

    优化查询

--- app/backend/modules/charts/modules/member/services/LowerOrderService.php
+++ app/backend/modules/charts/modules/member/services/LowerOrderService.php
@@ -22,14 +22,13 @@ class LowerOrderService
             \YunShop::app()->uniacid = $u->uniacid;
             \Setting::$uniqueAccountId = $u->uniacid;
 
-            $uniacid = \YunShop::app()->uniacid;
-            $member_1 = DB::table('yz_member_children')->select('member_id', DB::raw('group_concat(child_id) as child'))->where('level', 1)->groupBy('member_id')->get()->toArray();
-
-            foreach ($member_1 as $k => $item) {
+            $order = DB::table('yz_order')->select('uid','price')->where('uniacid', \YunShop::app()->uniacid)->get();
+            $member_1 = DB::select('select member_id, group_concat(child_id) as child,level from ims_yz_member_children where level =1' . ' and uniacid =' . \YunShop::app()->uniacid . ' group by member_id');
+            foreach ($member_1 as $item) {
                 $result[$item['member_id']]['uid'] = $item['member_id'];
-                $result[$item['member_id']]['uniacid'] = $uniacid;
-                $result[$item['member_id']]['first_order_quantity'] = DB::table('yz_order')->select(DB::raw('count(id) as total'))->where('uid', $item['child'])->get()->toArray()[0]['total'] ?: 0;
-                $result[$item['member_id']]['first_order_amount'] = intval(DB::table('yz_order')->select(DB::raw('sum(price) as money'))->where('uid', $item['child'])->get()->toArray()[0]['money']) ?: 0;
+                $result[$item['member_id']]['uniacid'] = \YunShop::app()->uniacid;
+                $result[$item['member_id']]['first_order_quantity'] = $order->whereIn('uid', explode(',',$item['child']))->count();
+                $result[$item['member_id']]['first_order_amount'] = $order->whereIn('uid', explode(',',$item['child']))->sum('price');
                 $result[$item['member_id']]['second_order_quantity'] = 0;
                 $result[$item['member_id']]['second_order_amount'] = 0;
                 $result[$item['member_id']]['third_order_quantity'] = 0;
@@ -37,26 +36,29 @@ class LowerOrderService
                 $result[$item['member_id']]['team_order_quantity'] = $result[$item['member_id']]['first_order_quantity'];
                 $result[$item['member_id']]['team_order_amount'] = $result[$item['member_id']]['first_order_amount'];
             }
+//        dd($result);
 
-            $member_2 = DB::table('yz_member_children')->select('member_id', DB::raw('group_concat(child_id) as child'))->where('level', 2)->groupBy('member_id')->get()->toArray();
-            foreach ($member_2 as $k => $item) {
+            $member_2 = DB::select('select member_id, group_concat(child_id) as child,level from ims_yz_member_children where level =2' . ' and uniacid =' . \YunShop::app()->uniacid . ' group by member_id');
+            foreach ($member_2 as $item) {
                 $result[$item['member_id']]['uid'] = $item['member_id'];
-                $result[$item['member_id']]['uniacid'] = $uniacid;
-                $result[$item['member_id']]['second_order_quantity'] = DB::table('yz_order')->select(DB::raw('count(id) as total'))->where('uid', $item['child'])->get()->toArray()[0]['total'] ?: 0;
-                $result[$item['member_id']]['second_order_amount'] = intval(DB::table('yz_order')->select(DB::raw('sum(price) as money'))->where('uid', $item['child'])->get()->toArray()[0]['money']) ?: 0;
+                $result[$item['member_id']]['uniacid'] = \YunShop::app()->uniacid;
+                $result[$item['member_id']]['second_order_quantity'] = $order->whereIn('uid', explode(',',$item['child']))->count();
+                $result[$item['member_id']]['second_order_amount'] = $order->whereIn('uid', explode(',',$item['child']))->sum('price');
                 $result[$item['member_id']]['team_order_quantity'] = $result[$item['member_id']]['first_order_quantity']+$result[$item['member_id']]['second_order_quantity'];
                 $result[$item['member_id']]['team_order_amount'] = $result[$item['member_id']]['first_order_amount']+$result[$item['member_id']]['second_order_amount'];
             }
+//        dd($result);
 
-            $member_3 = DB::table('yz_member_children')->select('member_id', DB::raw('group_concat(child_id) as child'))->where('level', 3)->groupBy('member_id')->get()->toArray();
-            foreach ($member_3 as $k => $item) {
+            $member_3 = DB::select('select member_id, group_concat(child_id) as child,level from ims_yz_member_children where level =3' . ' and uniacid =' . \YunShop::app()->uniacid . ' group by member_id');
+            foreach ($member_3 as $item) {
                 $result[$item['member_id']]['uid'] = $item['member_id'];
-                $result[$item['member_id']]['uniacid'] = $uniacid;
-                $result[$item['member_id']]['third_order_quantity'] = DB::table('yz_order')->select(DB::raw('count(id) as total'))->where('uid', $item['child'])->get()->toArray()[0]['total'] ?: 0;
-                $result[$item['member_id']]['third_order_amount'] = intval(DB::table('yz_order')->select(DB::raw('sum(price) as money'))->where('uid', $item['child'])->get()->toArray()[0]['money']) ?: 0;
+                $result[$item['member_id']]['uniacid'] = \YunShop::app()->uniacid;
+                $result[$item['member_id']]['third_order_quantity'] = $order->whereIn('uid', explode(',',$item['child']))->count();
+                $result[$item['member_id']]['third_order_amount'] = $order->whereIn('uid', explode(',',$item['child']))->sum('price');
                 $result[$item['member_id']]['team_order_quantity'] = $result[$item['member_id']]['first_order_quantity']+$result[$item['member_id']]['second_order_quantity']+$result[$item['member_id']]['third_order_quantity'];
                 $result[$item['member_id']]['team_order_amount'] = $result[$item['member_id']]['first_order_amount']+$result[$item['member_id']]['second_order_amount']+$result[$item['member_id']]['third_order_amount'];
             }
+//        dd($result);
 
             $memberModel = new MemberLowerOrder();
             foreach ($result as $item) {
