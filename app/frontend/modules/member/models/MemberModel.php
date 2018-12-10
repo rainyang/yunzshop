4c98a60458cdb545f09441e899c3cb5c7f74d1b3
weifeng <519570979@qq.com>
2018-12-10 11:12:16 +0800

    优化

--- app/frontend/modules/member/models/MemberModel.php
+++ app/frontend/modules/member/models/MemberModel.php
@@ -477,9 +477,9 @@ class MemberModel extends Member
         }
         $data['wechat'] = $member_set['relation_level']['wechat']?:0;
         $data['phone'] = $member_set['relation_level']['phone']?:0;
-        $data['name1'] = $member_set['relation_level']['name1'];
-        $data['name2'] = $member_set['relation_level']['name2'];
-        $data['name3'] = $member_set['relation_level']['name3'];
+        $data['name1'] = $member_set['relation_level']['name1']?:'';
+        $data['name2'] = $member_set['relation_level']['name2']?:'';
+        $data['name3'] = $member_set['relation_level']['name3']?:'';
 
         return $data;
     }
