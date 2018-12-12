68c0414af7d0af2a3d835d9f564283c4da36477d
weijinsheng <weijinsheng>
2018-12-12 15:25:00 +0800

    分页

--- app/frontend/controllers/HomePageController.php
+++ app/frontend/controllers/HomePageController.php
@@ -316,7 +316,7 @@ class HomePageController extends ApiController
             foreach ($datas['data'] as $key => $itme) {
                 $datas['data'][$key] = unserialize($itme['goods']);//反序列化
             }
-            $this->successJson('ok', $datas);
+            return $this->successJson('ok', $datas);
         }
     }
 
