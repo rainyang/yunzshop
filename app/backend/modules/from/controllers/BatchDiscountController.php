0def52b060436f9c8b54aab9f9c18ca42371214f
weifeng <519570979@qq.com>
2018-12-14 15:32:12 +0800

    暂存

---
+++ app/backend/modules/from/controllers/BatchDiscountController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/14
+ * Time: 15:28
+ */
+
+namespace app\backend\modules\from\controllers;
+
+
+use app\common\components\BaseController;
+
+class BatchDiscountController extends BaseController
+{
+    public function index()
+    {
+
+    }
+}
