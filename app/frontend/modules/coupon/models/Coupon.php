623ca254ad7d7bf7ed111d4dfdb7e0d0495761ba
沈阳 <564345292@qq.com>
2018-12-27 11:55:27 +0800

    优惠券模型查询条件封装

--- app/frontend/modules/coupon/models/Coupon.php
+++ app/frontend/modules/coupon/models/Coupon.php
@@ -31,38 +31,28 @@ class Coupon extends \app\common\models\Coupon
     //获取该用户可领取的优惠券的状态
     public static function getCouponsForMember($memberId, $memberLevel, $couponId = null, $time = null)
     {
+
         $res = static::uniacid()
-                        ->select(['id', 'name', 'coupon_method', 'deduct', 'discount', 'enough', 'use_type', 'category_ids',
-                                'categorynames', 'goods_ids', 'goods_names', 'time_limit', 'time_days', 'time_start', 'time_end', 'get_max', 'total',
-                                'money', 'credit'])
-                        ->where('get_type','=',1)
-                        ->where('status', '=', 1)
-                        ->where('get_max', '!=', 0)
-                        ->where(function($query) use ($memberLevel){
-                            $query->where('level_limit', '<=', $memberLevel)
-                                ->orWhere(function($query){
-                                    $query->whereNull('level_limit');
-                                });
-                        })
-                        ->withCount(['hasManyMemberCoupon'])
-                        ->withCount(['hasManyMemberCoupon as member_got' => function($query) use($memberId){
-                            return $query->where('uid', '=', $memberId);
-                        }]);
+            ->select(['id', 'name', 'coupon_method', 'deduct', 'discount', 'enough', 'use_type', 'category_ids',
+                    'categorynames', 'goods_ids', 'goods_names', 'time_limit', 'time_days', 'time_start', 'time_end', 'get_max', 'total',
+                    'money', 'credit'])
+            ->where('get_type','=',1)
+            ->where('status', '=', 1)
+            ->where('get_max', '!=', 0)
+            ->memberLevel($memberLevel);
 
         if(!is_null($couponId)){
             $res = $res->where('id', '=', $couponId);
         }
 
         if(!is_null($time)){
-            $res = $res->where(function($query) use ($time){
-                        $query->where('time_limit', '=', 1)->where('time_end', '>', $time)
-                            ->orWhere(function($query){
-                                $query->where('time_limit', '=', 0)->where('time_days', '>=', 0);
-                            });
-                    });
+            $res = $res->unexpired($time);
         }
 
-        return $res;
+        return $res->withCount(['hasManyMemberCoupon'])
+            ->withCount(['hasManyMemberCoupon as member_got' => function($query) use($memberId){
+                return $query->where('uid', '=', $memberId);
+            }]);
     }
 
     //指定ID的, 在优惠券中心可领取的, 优惠券
