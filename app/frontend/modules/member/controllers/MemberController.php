27b8633c9692938ccb65159283f02b65aab09e30
weijinsheng <weijinsheng>
2018-12-28 17:23:23 +0800

    注释

--- app/frontend/modules/member/controllers/MemberController.php
+++ app/frontend/modules/member/controllers/MemberController.php
@@ -753,9 +753,9 @@ class MemberController extends ApiController
         $shop = \Setting::get('shop');
 //        dd($shop);
         $shop['icon'] = replace_yunshop(yz_tomedia($shop['logo']));
-        if ($shop){
-            $shop['name'] = $shop['shop']['name'];
-        }
+//        if ($shop){
+//            $shop['name'] = $shop['shop']['name'];
+//        }
         if (!is_null(\Config('customer_service'))) {
             $class = array_get(\Config('customer_service'), 'class');
             $function = array_get(\Config('customer_service'), 'function');
@@ -771,9 +771,9 @@ class MemberController extends ApiController
                 'desc'  => '商家分享'
             ];
         }
-        if(is_null($share['desc'])){
-            $share['desc'] = "";
-        }
+//        if(is_null($share['desc'])){
+//            $share['desc'] = "";
+//        }
 
         $data = [
             'config' => $config,
