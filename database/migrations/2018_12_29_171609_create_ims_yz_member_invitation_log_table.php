4c1399a612fc94bd68f154d103924515c44c1d25
fbc <773928614@qq.com>
2018-12-29 14:34:17 +0800

    修复bug

--- database/migrations/2018_12_29_171609_create_ims_yz_member_invitation_log_table.php
+++ database/migrations/2018_12_29_171609_create_ims_yz_member_invitation_log_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImsYzCategoryDiscount extends Migration
+class CreateImsYzMemberInvitationLogTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,6 @@ class CreateImsYzCategoryDiscount extends Migration
      */
     public function up()
     {
-        //
         if (!Schema::hasTable('yz_member_invitation_log')) {
             Schema::create('yz_member_invitation_log', function (Blueprint $table) {
                 $table->increments('id');
