26bd377aaabd2be4417a1becfb476b0a9a7364ab
崔松岩 <178125247@qq.com>
2019-01-14 16:59:33 +0800

    商城业绩显示

--- app/frontend/modules/order/controllers/OrderAllController.php
+++ app/frontend/modules/order/controllers/OrderAllController.php
@@ -9,7 +9,73 @@
 namespace app\frontend\modules\order\controllers;
 
 
-class OrderAllController
-{
+use app\common\components\ApiController;
+use app\common\facades\Setting;
+use app\common\models\MemberShopInfo;
+use app\common\models\Order;
 
+
+class OrderAllController extends ApiController
+{
+   public function index(){
+       $set = Setting::get('shop.shop');
+       $member_id = \YunShop::app()->getMemberId();
+       $data=['mark'=>0];
+       if($set['achievement'] != 1){
+           return $this->successJson('商城业绩不显示',$data);
+       }
+       if(!in_array(-1,$set['member_level'])){
+           $member_level = MemberShopInfo::where('member_id',$member_id)->first();
+            if(!in_array($member_level['level_id'],$set['member_level'])){
+                return $this->successJson('成员没查看权限',$data);
+            }
+       }
+       $data['mark'] = 1;
+       $data['today'] = $this->getMoney('today');
+       $data['month'] = $this->getMoney('month');
+       $data['all'] = $this->getMoney('all');
+       $data['recent']['One'] = $this->getRecent(1);
+       $data['recent']['Two'] = $this->getRecent(2);
+       $data['recent']['Thr'] = $this->getRecent(3);
+       $data['recent']['Fou'] = $this->getRecent(4);
+       $data['recent']['Fiv'] = $this->getRecent(5);
+       $data['recent']['Six'] = $this->getRecent(6);
+       $data['recent']['Sev'] = $this->getRecent(7);
+       return $this->successJson('ok',$data);
+   }
+
+   private function getMoney($mark){
+       $range = [];
+       $time=time();
+       switch ($mark) {
+           case 'today':
+               $str=date("Y-m-d",time())." 0:0:0";
+               $range[]=strtotime($str);
+               $str=date("Y-m-d",time())." 23:59:59";
+               $range[]=strtotime($str);
+               break;
+           case 'month':
+               $range[]=mktime(0,0,0,date('m'),1,date('Y'));
+               $range[]=mktime(23,59,59,date('m'),date('t'),date('Y'));
+               break;
+       }
+       if($mark == "all"){
+           $price=Order::where('status',Order::COMPLETE)->sum('price');
+       }else {
+           $price = Order::where('status', Order::COMPLETE)->whereBetween('finish_time',$range)->sum('price');
+       }
+   return $price;
+   }
+
+   private function getRecent($mark){
+       $range = [];
+       $str=date("Y-m-d",strtotime("-".$mark." day"))." 0:0:0";
+       $range[]=strtotime($str);
+       $str=date("Y-m-d",strtotime("-".$mark." day"))." 23:59:59";
+       $range[]=strtotime($str);
+       $price = Order::where('status', Order::COMPLETE)->whereBetween('finish_time',$range)->sum('price');
+       $date=date("Y-m-d",strtotime("-".$mark." day"));
+       $data=['price'=>$price , 'date'=>$date];
+       return $data;
+   }
 }
