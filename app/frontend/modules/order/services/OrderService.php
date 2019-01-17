bfd8df870e804097dbca7a701b53a97c45a20b93
zengxiangping <zengxiangping@yunzhong.com>
2019-01-17 17:47:47 +0800

    te

--- app/frontend/modules/order/services/OrderService.php
+++ app/frontend/modules/order/services/OrderService.php
@@ -116,7 +116,7 @@ class OrderService
         DB::transaction(function() use($orderOperation) {
             $orderOperation->handle();
         });
-        \Log::info('-----orderOperation-----', $orderOperation->handle());
+        // \Log::info('-----orderOperation-----', $orderOperation->handle());
     }
 
     /**
@@ -267,7 +267,7 @@ class OrderService
     public static function orderSend($param)
     {
         $orderOperation = OrderSend::find($param['order_id']);
-        \Log::info('----3orderOperation--', $orderOperation);
+        // \Log::info('----3orderOperation--', $orderOperation);
         return self::OrderOperate($orderOperation);
     }
 
