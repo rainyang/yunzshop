d7fbd0ac64418c9c4f5690d1ab07b6db4077fda5
blank <baidu.com>
2019-01-18 16:33:30 +0800

    no message

--- static/js/app/config.js
+++ static/js/app/config.js
@@ -23,9 +23,9 @@ require.config({
         'kindeditor.main':'../../components/kindeditor/kindeditor-min',
         'css': '../lib/css.min',
         'webuploader' : '../../components/webuploader/webuploader.min',
-        //'fileUploader' : window.sysinfo['static_url'] + (window.sysinfo['is_new'] !='1' ? 'resource/js/app/fileUploader': 'resource/js/app/fileUploader.min'),
+        'fileUploader' : window.sysinfo['static_url'] + (window.sysinfo['is_new'] !='1' ? 'resource/js/app/fileUploader': 'resource/js/app/fileUploader.min'),
         //视频上传需要
-        'fileUploader' : window.sysinfo['static_url'] + (window.sysinfo['is_new'] !='1' ? 'resource/js/app/fileUploader': 'js/file.min'),
+        //'fileUploader' : window.sysinfo['static_url'] + (window.sysinfo['is_new'] !='1' ? 'resource/js/app/fileUploader': 'js/file.min'),
         'json2' : '../lib/json2',
         'wapeditor' : './wapeditor',
         'jquery.wookmark': '../lib/jquery.wookmark.min',
