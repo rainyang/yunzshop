12192016d182dbb11482225e05b7b8b56d144378
fbc <773928614@qq.com>
2019-01-29 14:20:22 +0800

    修复

--- app/frontend/modules/order/controllers/ListController.php
+++ app/frontend/modules/order/controllers/ListController.php
@@ -35,7 +35,7 @@ class ListController extends ApiController
      */
     protected function _getOrder()
     {
-        return $this->order = app('OrderManager')->make('Order')->orders()->where(app('OrderManager')->make('Order')->getTable().'.status', '<>', '-1')->where('plugin_id','<','900')->with('hasOnePayType');
+        return $this->order = app('OrderManager')->make('Order')->orders()->where(app('OrderManager')->make('Order')->getTable().'.status', '<>', '-1')->where('plugin_id', '<>')->where('plugin_id','<','900')->with('hasOnePayType');
     }
 
     protected function getData()
