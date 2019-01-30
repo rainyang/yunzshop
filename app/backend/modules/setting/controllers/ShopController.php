fd7b778851d7af84b861d4ac93acc69b312c89f5
linqinghui <linqinghui@yunzshop.com>
2019-01-30 11:01:18 +0800

    会员前端会员中心--余额提现到支付宝，提示状态不能为空

--- app/backend/modules/setting/controllers/ShopController.php
+++ app/backend/modules/setting/controllers/ShopController.php
@@ -39,11 +39,9 @@ class ShopController extends BaseController
             if(Cache::has('shop_setting')){
                 Cache::forget('shop_setting');
             }
-
+            $requestModel['credit']=empty($requestModel['credit'])?"余额":$requestModel['credit'];
             if (Setting::set('shop.shop', $requestModel)) {
-                $dalance=Setting::get('shop.shop');
-                $credit=$dalance['credit']?:"余额";
-                PayType::updateDalance(3,$credit)?:$this->error("余额字样保存失败");
+                PayType::updateDalance(3,$requestModel['credit'])?:$this->error("余额字样保存失败");
                 return $this->message('商城设置成功', Url::absoluteWeb('setting.shop.index'));
             } else {
                 $this->error('商城设置失败');
