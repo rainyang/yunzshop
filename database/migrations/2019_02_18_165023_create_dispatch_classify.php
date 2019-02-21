b20e527aa3af26e835b760d59ab4574e1f4d37c1
linqinghui <linqinghui@yunzshop.com>
2019-02-21 13:44:47 +0800

    批量设置运费

--- database/migrations/2019_02_18_165023_create_dispatch_classify.php
+++ database/migrations/2019_02_18_165023_create_dispatch_classify.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDispatchCalssify extends Migration
+class CreateDispatchClassify extends Migration
 {
     /**
      * Run the migrations.
