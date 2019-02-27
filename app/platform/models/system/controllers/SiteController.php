bd97778957723129b71e15f0195016b35c6cf832
liuyifan <liuyifan@yunzshop.com>
2019-02-27 11:00:07 +0800

    站点设置路由

---
+++ app/platform/models/system/controllers/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuyifan
+ * Date: 2019/2/27
+ * Time: 10:53
+ */
+namespace app\platform\models\system\controllers;
+
+use app\platform\controllers\BaseController;
+
+class SiteController extends BaseController
+{
+    public function index()
+    {
+        echo 2;
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -14,7 +14,13 @@ Route::group(['namespace' => 'platform\controllers'], function () {
 Route::group(['middleware' => ['auth:admin', 'authAdmin']], function () {
 
     Route::get('index', ['as' => 'admin.index', 'uses' => '\app\platform\controllers\IndexController@index']);
-    
+
+    // 站点管理
+    Route::group(['prefix' => 'system'], function (){
+        // 站点设置
+        Route::get('site', ['as' => 'admin.system.site', 'uses' => '\app\platform\models\system\controllers\SiteController@index']);
+    });
+
     //权限管理路由
     Route::get('permission/{parentId}/create', ['as' => 'admin.permission.create', 'uses' => 'PermissionController@create']);
     Route::get('permission/manage', ['as' => 'admin.permission.manage', 'uses' => 'PermissionController@index']);
