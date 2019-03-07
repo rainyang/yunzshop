3997b6e5d734f3c36a53ae23cbce3f7e2c567efc
liuyifan <liuyifan@yunzshop.com>
2019-03-07 09:42:27 +0800

    修复

--- app/platform/modules/system/controllers/SiteController.php
+++ app/platform/modules/system/controllers/SiteController.php
@@ -15,7 +15,7 @@ class SiteController extends BaseController
     public function index()
     {
         $set_data = request()->setdata;
-        $copyright = SystemSetting::settingLoad('copyright', 'system_copyright');
+        $copyright = SystemSetting::settingLoad('kk', 'kk');
 
         if ($set_data) {
             $site = SystemSetting::settingSave($set_data, 'copyright', 'system_copyright');
@@ -34,32 +34,18 @@ class SiteController extends BaseController
             }
         }
 
-        return \Response::json([
-            'result' => 1,
-            'msg' => '成功',
-            'data' => $copyright
-        ]);
-
-//        return view('system.site', [
-//            'setdata' =>
-//        ]);
-
-        /* 站点设置字段名 */
-        // 是否关闭站点 status
-        // 站点名称 name
-        // 平台logo site_logo
-        // 浏览器标题图标 title_icon
-        // 登录页广告 advertisement
-        // 底部信息 information
-
-       /* $set = \Setting::get('system.site');
-        $set_data = request()->setdata;
-        if ($set_data) {
-            if (!\Setting::set('system.site', $set_data)) {
-                echo $this->errorJson('失败', '', 'error'); exit;
-            }
-            echo $this->successJson('成功', ''); exit;
+        if ($copyright) {
+            return \Response::json([
+                'result' => 1,
+                'msg' => '成功',
+                'data' => $copyright
+            ]);
+        } else {
+            return \Response::json([
+                'result' => 0,
+                'msg' => '失败',
+                'data' => ''
+            ]);
         }
-        echo $this->successJson('获取数据成功', $set); exit;*/
     }
 }
