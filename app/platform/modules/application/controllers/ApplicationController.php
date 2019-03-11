2c573087e8f4a59c99c67f2f315b1650c484efc6
zengxiangping <zengxiangping@yunzhong.com>
2019-03-11 16:22:21 +0800

    fix:修复图片上传返回路径

--- app/platform/modules/application/controllers/ApplicationController.php
+++ app/platform/modules/application/controllers/ApplicationController.php
@@ -16,83 +16,43 @@ class ApplicationController extends BaseController
 
     public function index()
     {
-        $search = request()->search;
-        
-        $app = new UniacidApp();
-
-        if ($search) {
+        if (request()->ajax()) {
             
-            $app = $app->search($search);
+            $search = request()->search;
+            
+            $app = new UniacidApp();
 
+            if ($search) {
+                
+                $app = $app->search($search);
 
-        } 
-        // else {
-            $list = $app->orderBy('id', 'desc')->paginate(5)->toArray();
+            } 
+            // else {
+                $list = $app->orderBy('id', 'desc')->paginate(5)->toArray();
 
-//            $total = Cache::get($this->key.'_num');
-//
-//            for($i = 1; $i< $total; $i++ ) {
-//
-//               $list[] = Cache::paginate(request()->input('pagesize',20))->get($this->key.':'.$i);
-//            }
-        // }
+    //            $total = Cache::get($this->key.'_num');
+    //
+    //            for($i = 1; $i< $total; $i++ ) {
+    //
+    //               $list[] = Cache::paginate(request()->input('pagesize',20))->get($this->key.':'.$i);
+    //            }
+            // }
 
-        return $this->successJson('获取成功',  $list);
+            return $this->successJson('获取成功',  $list);
+        }
     }
 
     public function add()
     {
-        $app = new UniacidApp();
-        // dd(request());
-        $data = $this->fillData(request()->input());
+        if (request()->ajax()) {
 
-        $app->fill($data);
-
-        $validator = $app->validator();
-
-        if ($validator->fails()) {
-
-            return $this->errorJson($validator->messages());
-        
-        } else {
-
-            if ($app->save()) {
-                
-                // $id = $app->insertGetId();
-                //更新缓存
-                // Cache::put($this->key.':'. $id, $app->find($id));
-                // Cache::put($this->key.'_num', $id);
-
-                return $this->successJson('添加成功');
-
-            } else {
-
-                return $this->errorJson('添加失败');
-            }
-        }
-    }
-
-    public function update()
-    {
-        $id = request()->id;
-        
-        $app = new UniacidApp();
-        
-        $info = $app->find($id);
-
-        if (!$id || !$info) {
-            return $this->errorJson('请选择应用');
-        }
-        
-        if (request()->input()) {
-           
+            $app = new UniacidApp();
+            // dd(request());
             $data = $this->fillData(request()->input());
-            $data['uniacid'] = $id;
-            $data['id'] = $id;
 
             $app->fill($data);
 
-            $validator = $app->validator($data);
+            $validator = $app->validator();
 
             if ($validator->fails()) {
 
@@ -100,104 +60,158 @@ class ApplicationController extends BaseController
             
             } else {
 
-                if ($app->where('id', $id)->update($data)) {
-                    //更新缓存
-                    Cache::put($this->key.':'. $id, $app->find($id), $data['validity_time']);
-
-                    return $this->successJson('修改成功');
-                } else {
+                if ($app->save()) {
                     
-                    return $this->errorJson('修改失败');
+                    // $id = $app->insertGetId();
+                    //更新缓存
+                    // Cache::put($this->key.':'. $id, $app->find($id));
+                    // Cache::put($this->key.'_num', $id);
+
+                    return $this->successJson('添加成功');
+
+                } else {
+
+                    return $this->errorJson('添加失败');
                 }
             }
         }
-        return $this->successJson('成功获取', $info);
+    }
+
+    public function update()
+    {
+        if (request()->ajax()) {
+
+            $id = request()->id;
+
+            $app = new UniacidApp();
+
+            $info = $app->find($id);
+
+            if (!$id || !$info) {
+                return $this->errorJson('请选择应用');
+            }
+
+            if (request()->input()) {
+
+                $data = $this->fillData(request()->input());
+                $data['uniacid'] = $id;
+                $data['id'] = $id;
+
+                $app->fill($data);
+
+                $validator = $app->validator($data);
+
+                if ($validator->fails()) {
+
+                    return $this->errorJson($validator->messages());
+
+                } else {
+
+                    if ($app->where('id', $id)->update($data)) {
+                        //更新缓存
+                        Cache::put($this->key . ':' . $id, $app->find($id), $data['validity_time']);
+
+                        return $this->successJson('修改成功');
+                    } else {
+
+                        return $this->errorJson('修改失败');
+                    }
+                }
+            }
+        }
     }
 
     //加入回收站 删除
     public function delete()
     {
-        $id = request()->id;
-        
-        $info = UniacidApp::withTrashed()->find($id);
+        if (request()->ajax()) {
 
-        if (!$id || !$info) {
-            return $this->errorJson('请选择要修改的应用');
-        }
-        // dd($info->deleted_at);
-        if ($info->deleted_at) {
-            
-           //强制删除
-            if (!$info->forceDelete()) {
-                return $this->errorJson('操作失败');            
+            $id = request()->id;
+
+            $info = UniacidApp::withTrashed()->find($id);
+
+            if (!$id || !$info) {
+                return $this->errorJson('请选择要修改的应用');
             }
-        
-            Cache::forget($this->key.':'.$id);
+            // dd($info->deleted_at);
+            if ($info->deleted_at) {
 
-        } else {
+                //强制删除
+                if (!$info->forceDelete()) {
+                    return $this->errorJson('操作失败');
+                }
 
-            if(!$info->delete()){
-                return $this->errorJson('操作失败');            
+                Cache::forget($this->key . ':' . $id);
+
+            } else {
+
+                if (!$info->delete()) {
+                    return $this->errorJson('操作失败');
+                }
+
+                Cache::put($this->key . ':' . $id, UniacidApp::find($id));
             }
 
-            Cache::put($this->key.':'.$id, UniacidApp::find($id));
+            return $this->successJson('操作成功');
         }
-
-        return $this->successJson('操作成功');
     }
 
     //启用禁用或恢复应用
     public function switchStatus()
     {
-        $id = request()->id;
-        
-        $info = UniacidApp::withTrashed()->find($id);
+        if (request()->ajax()) {
 
-        if (!$id || !$info) {
-            return $this->errorJson('请选择要修改的应用');
+            $id = request()->id;
+
+            $info = UniacidApp::withTrashed()->find($id);
+
+            if (!$id || !$info) {
+                return $this->errorJson('请选择要修改的应用');
+            }
+
+            if (request()->status) {
+                //修改状态
+                $res = UniacidApp::where('id', $id)->update(['status' => $info->status == 1 ? 0 : 1]);
+            }
+
+            if (request()->url) {
+                //修改应用跳转链接
+                $res = UniacidApp::where('id', $id)->update(['url' => filter_var(trim(request()->url), FILTER_VALIDATE_URL)]);
+            }
+
+            if ($info->deleted_at) {
+
+                //从回收站中恢复应用
+                $res = UniacidApp::withTrashed()->where('id', $id)->restore();
+                // dd('2');
+            }
+
+            if ($res) {
+                //更新缓存
+                Cache::put($this->key . ':' . $id, UniacidApp::find($id), $info->validity_time);
+
+                return $this->successJson('操作成功');
+            } else {
+                return $this->errorJson('操作失败');
+            }
         }
-
-        if (request()->status) {
-            //修改状态
-            $res = UniacidApp::where('id', $id)->update(['status' => $info->status == 1 ? 0 : 1]);
-        }
-        
-        if (request()->url) {
-            //修改应用跳转链接
-            $res = UniacidApp::where('id', $id)->update(['url' => filter_var(trim(request()->url), FILTER_VALIDATE_URL) ]);
-        }
-
-        if ($info->deleted_at) {
-
-            //从回收站中恢复应用
-            $res = UniacidApp::withTrashed()->where('id', $id)->restore();
-            // dd('2');
-        }
-
-        if ($res) {
-            //更新缓存
-            Cache::put($this->key.':'.$id, UniacidApp::find($id), $info->validity_time);
-
-            return $this->successJson('操作成功');
-        } else {
-            return $this->errorJson('操作失败');
-        }
-       
     }
 
     //回收站 列表
     public function recycle()
     {
-        $list = UniacidApp::onlyTrashed()
-            ->orderBy('id', 'desc')
-            ->paginate(20)
-            ->toArray();
+        if (request()->ajax()) {
 
-        if ($list) {
+            $list = UniacidApp::onlyTrashed()
+                ->orderBy('id', 'desc')
+                ->paginate(20)
+                ->toArray();
 
-            return $this->successJson('获取成功', $list);
-        } else {
-            return $this->errorJson('获取失败,暂无数据');
+            if ($list) {
+                return $this->successJson('获取成功', $list);
+            } else {
+                return $this->errorJson('获取失败,暂无数据');
+            }
         }
     }
 
@@ -278,38 +292,6 @@ class ApplicationController extends BaseController
             return $this->successJson('上传成功', asset($res.'app/public/'.$filename));
     }
 
-    public function uploadFile()
-    {
-        $file = request()->file('file');
-        \Log::info('file', $file);
-
-        if (!$file) {
-            return $this->errorJson('请传入正确参数.');
-        }
-        if ($file->isValid()) {
-            // 获取文件相关信息
-            $originalName = $file->getClientOriginalName(); // 文件原名
-            \Log::info('up_originalName', $originalName);
-
-            $realPath = $file->getRealPath();   //临时文件的绝对路径
-            \Log::info('up_realPath', $realPath);
-
-            $ext = $file->getClientOriginalExtension();
-            \Log::info('up_ext', $ext);
-
-            $path = config('filesystems.disks.public')['root'].'/';
-            \Log::info('up_path', $path);
-
-            $newOriginalName = md5($originalName . str_random(6)) . '.' . $ext;
-            \Log::info('up_newOriginalName', $newOriginalName);
-
-            \Storage::put($path.$newOriginalName, file_get_contents($realPath));
-
-            return $this->successJson('上传成功',\Storage::url($path.$newOriginalName) );
-            // return $this->successJson('上传成功', asset($path.'app/public/'.$newOriginalName));
-        }
-    }
-
     public function upload()
     {
         $file = request()->file('file');
@@ -329,14 +311,15 @@ class ApplicationController extends BaseController
             
             // $path = config('filesystems.disks.public')['root'].'/';   //后期存放路径
 
-            $newOriginalName = md5($originalName . str_random(6)) . '.' . $ext;
+            $newOriginalName = date('Ymd').md5($originalName . str_random(6)) . '.' . $ext;
             \Log::info('newOriginalName', $newOriginalName);
 
             // $res = \Storage::disk('image')->put($newOriginalName, file_get_contents($realPath));
             $res = \Storage::disk('public')->put($newOriginalName, file_get_contents($realPath));
             \Log::info('res-path', [$res, \Storage::disk('public')]);
 
-            return $this->successJson('上传成功', \Storage::disk('public')->url($newOriginalName));
+            // return $this->successJson('上传成功', \Storage::disk('public')->url('app/public/'.$newOriginalName));
+            return $this->successJson('上传成功', $_SERVER['HTTP_HOST'].'/storage/app/public/'.$newOriginalName);
         }
     }
 
