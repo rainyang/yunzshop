8a025946705809d1ef315b57af731c175ba7b541
dingran <dingran80s@gmail.com>
2019-03-12 15:59:59 +0800

    修复迁移文件

---
+++ database/migrations/2019_02_19_175336_create_admin_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('yz_admin_users')) {
+            Schema::create('yz_admin_users', function (Blueprint $table) {
+                $table->increments('id')->comment('管理员用户表ID');
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('yz_admin_users');
+    }
+}
