f6f8eca01960aa4f23d94552c74dfa5b32524473
dingran <dingran80s@gmail.com>
2019-03-18 14:08:47 +0800

    删除冗余代码

--- api.php
+++ api.php
@@ -19,13 +19,7 @@ if (!function_exists("getExitInfo")) {
     }
 }
 
-//getExitInfo();
-//define('IN_IA', true);
-
 require '../../framework/bootstrap.inc.php';
-//define('IA_ROOT', str_replace("\\", '/', dirname(dirname(dirname(__FILE__)))));
-
-//require IA_ROOT . '/framework/class/loader.class.php';
 
 include_once __DIR__ . '/app/laravel.php';
 
