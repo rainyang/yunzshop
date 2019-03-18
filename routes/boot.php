6a5892ecdf453d11aca2ae49510cd6f33af2d1f3
dingran <dingran80s@gmail.com>
2019-03-18 15:46:51 +0800

    安装引导

--- routes/boot.php
+++ routes/boot.php
@@ -1,17 +1,4 @@
 <?php
 Route::get('/', function () {
-    $path = 'addons/yun_shop';
-    $file = $path .  '/api.php';
-
-    if (!file_exists($file)) {
-        if (!is_dir($path)) {
-            mkdir($path, 0777, true);
-        }
-
-        $f_data = file_get_contents('api.php');
-
-        file_put_contents($file, $f_data);
-    }
-
     return redirect('/admin/index');
 });
