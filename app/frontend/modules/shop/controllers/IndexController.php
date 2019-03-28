ebaddd7dbfaa20d264e0e791e929796190820d39
linqinghui <linqinghui@yunzshop.com>
2019-03-28 14:08:59 +0800

    获取商品爱心值

--- app/frontend/modules/shop/controllers/IndexController.php
+++ app/frontend/modules/shop/controllers/IndexController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\DB;
 use app\common\services\goods\VideoDemandCourseGoods;
 use app\common\models\Adv;
 use app\common\helpers\Cache;
-
+use Yunshop\Love\Common\Services\SetService;
 /**
  * Created by PhpStorm.
  * Author: 芸众商城 www.yunzshop.com
@@ -107,6 +107,8 @@ class IndexController extends ApiController
                 $value->thumb = yz_tomedia($value->thumb);
             }
             if (app('plugins')->isEnabled('love')){
+                $love_basics_set = SetService::getLoveSet();//获取爱心值基础设置
+                $goodsList->love_name = $love_basics_set['name'];
                   foreach ($goodsList as &$goodsValue){
                       $love_value = \Yunshop\Love\Common\Models\GoodsLove::select('award_proportion')
                           ->where('uniacid',\Yunshop::app()->uniacid)
