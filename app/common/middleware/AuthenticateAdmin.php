8772616ff0570c7badca3b58a0f73a8c46bd4e13
liuyifan <liuyifan@yunzshop.com>
2019-03-30 16:16:14 +0800

    修复

--- app/common/middleware/AuthenticateAdmin.php
+++ app/common/middleware/AuthenticateAdmin.php
@@ -36,10 +36,10 @@ class AuthenticateAdmin
      */
     public function handle($request, Closure $next)
     {
-//        $check = $this->checkUserInfo();
-//        if (!$check['result']) {
-//            return $this->errorJson($check['msg'], ['status'=> -1]);
-//        }
+        $check = $this->checkUserInfo();
+        if (!$check['result']) {
+            return $this->errorJson($check['msg'], ['status'=> -1]);
+        }
         $cfg = \config::get('app.global');
 
         if (\Route::getCurrentRoute()->getUri() == 'admin/shop' && isset($cfg['uniacid'])) {
@@ -147,21 +147,21 @@ class AuthenticateAdmin
      */
     private function checkUserInfo()
     {
-//        $user = \Auth::guard('admin')->user();
-//        $result = 1;
-//
-//        if ($user->status == 3) {
-//            $result = 0;
-//            $msg = '您已被禁用，请联系管理员';
-//        }
-//        if ($user->endtime != 0 && $user->endtime <= time()) {
-//            $result = 0;
-//            $msg = '您的账号已过期，请联系管理员';
-//        }
-//
-//        return [
-//            'result' => $result,
-//            'msg' => $msg
-//        ];
+        $user = \Auth::guard('admin')->user();
+        $result = 1;
+
+        if ($user->status == 3) {
+            $result = 0;
+            $msg = '您已被禁用，请联系管理员';
+        }
+        if ($user->endtime != 0 && $user->endtime <= time()) {
+            $result = 0;
+            $msg = '您的账号已过期，请联系管理员';
+        }
+
+        return [
+            'result' => $result,
+            'msg' => $msg
+        ];
     }
 }
