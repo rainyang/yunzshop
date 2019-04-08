ecd86fbf1abcc06ee1353f8c9b53b3da53824c37
liuyifan <liuyifan@yunzshop.com>
2019-04-08 18:54:46 +0800

    no message

--- app/platform/modules/system/controllers/AttachmentController.php
+++ app/platform/modules/system/controllers/AttachmentController.php
@@ -350,7 +350,7 @@ class AttachmentController extends BaseController
         if (!$response) {
             return $this->errorJson('配置失败，腾讯cos访问url错误');
         }
-        $image = getimagesizefromstring($response['content']);
+        $image = getimagesizefromstring($response);
         if ($image && strexists($image['mime'], 'image')) {
             return $this->successJson('配置成功');
         } else {
