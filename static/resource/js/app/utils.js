3d65f33ccb003decfd1ce04cfc814851e08de11c
liuyifan <liuyifan@yunzshop.com>
2019-04-16 19:04:40 +0800

    no message

--- static/resource/js/app/utils.js
+++ static/resource/js/app/utils.js
@@ -127,7 +127,7 @@
         }),
             t = $.extend({},
                 a, t),
-            window.UEDITOR_HOME_URL = window.sysinfo.siteroot + "web/resource/components/ueditor/";
+            window.UEDITOR_HOME_URL = window.sysinfo.siteroot + "/static/resource/components/ueditor/";
         var r = function(a, r) {
             var l = {
                 autoClearinitialContent: !1,
