b36e58cc53e4f30d4a8f3c124c4511532314225d
liuyifan <liuyifan@yunzshop.com>
2019-04-16 10:03:18 +0800

    修复

--- database/migrations/2017_08_01_170424_update_engine_innodb_to_mc_members.php
+++ database/migrations/2017_08_01_170424_update_engine_innodb_to_mc_members.php
@@ -13,13 +13,8 @@ class UpdateEngineInnodbToMcMembers extends Migration
      */
     public function up()
     {
-        if (env('APP_Framework') == 'platform') {
-            $mc_members =  'yz_mc_members';
-        } else {
-            $mc_members = 'mc_members';
-        }
-        if (\Schema::hasTable($mc_members)) {
-            \Illuminate\Support\Facades\DB::statement("ALTER TABLE ".app('db')->getTablePrefix().$mc_members." engine = InnoDB");
+        if (\Schema::hasTable('mc_members')) {
+            \Illuminate\Support\Facades\DB::statement("ALTER TABLE ".app('db')->getTablePrefix()."mc_members engine = InnoDB");
         }
 
     }
