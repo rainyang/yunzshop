b2a7a1f22598efb88ee18b3d3bba50c5c83cc316
yangxing <1448557472@qq.com>
2019-04-16 15:19:42 +0800

    酒店自定义名称

--- app/frontend/modules/member/controllers/MemberController.php
+++ app/frontend/modules/member/controllers/MemberController.php
@@ -1634,7 +1634,7 @@ class MemberController extends ApiController
                 if ($hotel) {
                     $data[] = [
                         'name'  => 'hotel',
-                        'title' => '酒店管理',
+                        'title' => HOTEL_NAME.'管理',
                         'class' => 'icon-member_hotel',
                         'url'   => 'HotelManage'
                     ];
