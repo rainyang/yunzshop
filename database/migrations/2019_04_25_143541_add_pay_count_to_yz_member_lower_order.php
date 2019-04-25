9dc2b4cb914f4f2bbd1e79028c68291fe80828a0
yangxing <1448557472@qq.com>
2019-04-25 17:26:10 +0800

    no message

--- database/migrations/2019_04_25_143541_add_pay_count_to_yz_member_lower_order.php
+++ database/migrations/2019_04_25_143541_add_pay_count_to_yz_member_lower_order.php
@@ -16,10 +16,10 @@ class AddPayCountToYzMemberLowerOrder extends Migration
         if (Schema::hasTable('yz_member_lower_order')) {
             Schema::table('yz_member_lower_order', function (Blueprint $table) {
                 if (!Schema::hasColumn('yz_member_lower_order', 'pay_count')) {
-                    $table->integer('pay_count')->default(0)->comment('团队支付人数');
+                    $table->integer('pay_count')->default(0)->nullable()->comment('团队支付人数');
                 }
                 if (!Schema::hasColumn('yz_member_lower_order', 'team_count')) {
-                    $table->integer('team_count')->default(0)->comment('团队总人数');
+                    $table->integer('team_count')->default(0)->nullable()->comment('团队总人数');
                 }
             });
         }
