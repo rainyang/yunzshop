ab42a7e072bfd9c40eb793556ae31b9554693cd2
dingran <dingran80s@gmail.com>
2019-05-08 13:51:17 +0800

    文件更新

--- app/platform/modules/system/controllers/UpdateController.php
+++ app/platform/modules/system/controllers/UpdateController.php
@@ -133,6 +133,10 @@ class UpdateController extends BaseController
             return;
         }
 
+        //前端更新文件检测
+        $frontendUpgrad = $this->frontendUpgrad();
+
+        //后台更新文件检测
         $update = new AutoUpdate(null, null, 300);
         $update->setUpdateFile('backcheck_app.json');
         $update->setCurrentVersion(config('version'));
@@ -215,7 +219,8 @@ class UpdateController extends BaseController
                     'version' => $version,
                     'files' => $ret['files'],
                     'filecount' => count($files),
-                    'log' => nl2br(base64_decode($ret['log']))
+                    'log' => nl2br(base64_decode($ret['log'])),
+                    'frontendUpgrad' => $frontendUpgrad
                 ];
             } else {
                 preg_match('/"[\d\.]+"/', file_get_contents(base_path('config/') . 'version.php'), $match);
@@ -617,5 +622,25 @@ class UpdateController extends BaseController
         }
 
         \Artisan::call('migrate',['--force' => true]);
+    }
+
+    private function frontendUpgrad($key, $secret)
+    {
+        $update = new AutoUpdate(null, null, 300);
+
+        //前端更新文件检测
+        $update->setUpdateFile('check_app.json');
+        $update->setCurrentVersion(config('front-version'));
+        $update->setUpdateUrl(config('auto-update.checkUrl')); //Replace with your server update directory
+        $update->setBasicAuth($key, $secret);
+        $update->checkUpdate();
+
+        if ($update->newVersionAvailable()) {
+            $list = $update->getUpdates();
+        }
+
+        krsort($list);
+
+        return count($list);
     }
 }
