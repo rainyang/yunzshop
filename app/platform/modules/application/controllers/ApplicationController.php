c40d3621d4f8f75c7b590d3eefe3b3850c40d528
zengxiangping <zengxiangping@yunzhong.com>
2019-05-09 11:03:58 +0800

    fix

--- app/platform/modules/application/controllers/ApplicationController.php
+++ app/platform/modules/application/controllers/ApplicationController.php
@@ -57,7 +57,7 @@ class ApplicationController extends BaseController
                 } else {
         
                     //到期前一周的时间  当前+1 直到 +7 小于等于 $value['validity_time']
-                    if ((date('W', strtotime('+1 week')) == date('W', $value['validity_time'])) || (date('W') == date('W', $value['validity_time'])) && $value['validity_time'] >= mktime(0,0,0, date('m'), date('d'), date('Y')) ) {
+                    if ((date('W', strtotime('+1 week')) == date('W', $value['validity_time'])) || (date('W') == date('W', $value['validity_time'])) && $value['validity_time'] >= mktime(0,0,0, date('m'), date('d'), date('Y')) || ($value['validity_time'] < time())) {
                         //到期前一周
                         $list['data'][$key]['is_expire'] = 1;
 
