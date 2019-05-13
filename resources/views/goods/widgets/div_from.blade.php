fecd2fa12d018994a8210a883ce4ebec88429806
zengxiangping <zengxiangping@yunzhong.com>
2019-05-13 17:44:09 +0800

    fix

--- resources/views/goods/widgets/div_from.blade.php
+++ resources/views/goods/widgets/div_from.blade.php
@@ -16,6 +16,12 @@
                     </label>
                 </div>
             </div>
+            <div class="form-group">
+                <label class="col-xs-12 col-sm-3 col-md-2 control-label"></label>
+                <div class="col-sm-9 col-xs-12">
+                    <span>商品表单是针对跨境商品报税所开放使用的固定信息，非自定义表单信息</span>
+                </div>
+           </div>
         </div>
     </div>
 
