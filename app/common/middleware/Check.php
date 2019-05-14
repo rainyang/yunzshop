14cc2bd9fc29f4148ae590c1e9d8fc3f92c79ac9
liuyifan <liuyifan@yunzshop.com>
2019-05-14 15:13:57 +0800

    no message

---
+++ app/common/middleware/Check.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dingran
+ * Date: 2019/3/12
+ * Time: 下午5:42
+ */
+
+namespace app\common\middleware;
+
+
+use app\common\traits\JsonTrait;
+
+class Check
+{
+    use JsonTrait;
+
+    public function handle($request, \Closure $next)
+    {
+        $this->checkRegister();
+
+        return $next($request);
+    }
+
+    /**
+     * 检测是否注册
+     */
+    private function checkRegister()
+    {
+        $key = \Setting::get('shop.key')['key'];
+        $secret = \Setting::get('shop.key')['secret'];
+
+        if (!$key || !$secret) {
+            $this->errorJson('', [
+                'status' => -5
+            ])->send();
+            exit;
+        }
+    }
+}
