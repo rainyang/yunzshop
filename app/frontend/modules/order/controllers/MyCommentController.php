b380caba60d1661c86c7ec448a2ef550a180c43e
linqinghui <linqinghui@yunzshop.com>
2019-05-14 14:05:24 +0800

    评价分页处理

--- app/frontend/modules/order/controllers/MyCommentController.php
+++ app/frontend/modules/order/controllers/MyCommentController.php
@@ -26,6 +26,7 @@ class MyCommentController extends ApiController
     public function paging()
     {
         $page = \YunShop::request()->page?:1;
+        $page = ($page - 1) ? ($page - 1) *15 : 1;
         $list = Order::getMyCommentListPaginate( \YunShop::request()->status,$page,15);
         return $this->successJson('成功', [
             'list' => $list->toArray()
