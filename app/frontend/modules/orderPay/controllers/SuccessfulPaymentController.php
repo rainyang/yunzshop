a1e8d2a7fc13d5f9e7dad6f6deb3b2d7092743af
weijinsheng <weijinsheng>
2019-05-16 10:22:46 +0800

    支付跳转接口

--- app/frontend/modules/orderPay/controllers/SuccessfulPaymentController.php
+++ app/frontend/modules/orderPay/controllers/SuccessfulPaymentController.php
@@ -12,6 +12,7 @@ namespace app\frontend\modules\orderPay\controllers;
 use app\common\components\ApiController;
 use app\common\models\OrderPay;
 use app\common\models\Order;
+use app\common\services\finance\CalculationPointService;
 
 class SuccessfulPaymentController extends ApiController
 {
@@ -27,17 +28,24 @@ class SuccessfulPaymentController extends ApiController
         /**
          * 判断是余额还是第三方支付
          */
-//        if ($outtradeno){
-//            if (is_string($outtradeno)){
-//                $orderPay = OrderPay::where('pay_sn', $outtradeno)->first();
-//                $orders = Order::whereIn('id', $orderPay->order_ids)->get();
-//                dd($orders);
-//            }else{
-//                //余额
-//                $orderPay = OrderPay::find($outtradeno);
-//                $result = $orderPay->getPayResult(PayFactory::PAY_CREDIT);
-//            }
-//        }
+        if ($outtradeno){
+            if (is_string($outtradeno)){
+                $orderPay = OrderPay::where('pay_sn', $outtradeno)->first();
+                $orders = Order::whereIn('id', $orderPay->order_ids)->with('orderGoods')->get();
+//                $orders = Order::whereIn('id', $orderPay->order_ids)->with('orderGoods')->first();
+                foreach ($orders as $itme){
+                    foreach ($itme->orderGoods as $goods_model){
+                        $integral = $this::calcuationPointByGoods($goods_model);
+                        $data['integral'] += $integral['point'];  //$this::calcuationPointByGoods($goods_model);
+                    }
+                }
+
+            }else{
+                //余额
+                $orderPay = OrderPay::find($outtradeno);
+                $result = $orderPay->getPayResult(PayFactory::PAY_CREDIT);
+            }
+        }
 
 
         $data['app_links'] = '';
@@ -45,5 +53,46 @@ class SuccessfulPaymentController extends ApiController
             $set = \Setting::get('shop_app.pay');
             $data['app_links'] = $set['app_links'];
         }
+        dd($data);
+    }
+
+    public static function calcuationPointByGoods($order_goods_model)
+    {
+//        dd($order_goods_model->order_id);
+        $point_set = \Setting::get('point.set');
+
+
+        $order = Order::find($order_goods_model->order_id);
+        $order_set = $order->orderSettings->where('key', 'point')->first();
+        if ($order_set && $order_set->value['set']['give_point']) {
+            $point_set['give_point'] = $order_set->value['set']['give_point'] . '%';
+        }
+
+
+        $point_data = [];
+        //todo 如果等于0  不赠送积分
+        if (isset($order_goods_model->hasOneGoods->hasOneSale) && $order_goods_model->hasOneGoods->hasOneSale->point !== '' && intval($order_goods_model->hasOneGoods->hasOneSale->point) === 0) {
+            return $point_data;
+        }
+
+
+
+        //todo 如果不等于空，按商品设置赠送积分，否则按统一设置赠送积分
+        if (isset($order_goods_model->hasOneGoods->hasOneSale) && !empty($order_goods_model->hasOneGoods->hasOneSale->point)) {
+            if (strexists($order_goods_model->hasOneGoods->hasOneSale->point, '%')) {
+                $point_data['point'] = floatval(str_replace('%', '', $order_goods_model->hasOneGoods->hasOneSale->point) / 100 * $order_goods_model->payment_amount);
+            } else {
+                $point_data['point'] = $order_goods_model->hasOneGoods->hasOneSale->point * $order_goods_model->total;
+            }
+            $point_data['remark'] = '购买商品[' . $order_goods_model->hasOneGoods->title .'(比例:'. $order_goods_model->hasOneGoods->hasOneSale->point .')]赠送['.$point_data['point'].']积分！';
+        } else if (!empty($point_set['give_point'] && $point_set['give_point'])) {
+            if (strexists($point_set['give_point'], '%')) {
+                $point_data['point'] = floatval(str_replace('%', '', $point_set['give_point']) / 100 * $order_goods_model->payment_amount);
+            } else {
+                $point_data['point'] = $point_set['give_point'] * $order_goods_model->total;
+            }
+            $point_data['remark'] = "购买商品[统一设置(比例:". $point_set['give_point'] .")]赠送[{$point_data['point']}]积分！";
+        }
+        return $point_data;
     }
 }
