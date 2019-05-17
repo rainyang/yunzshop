4e9bf807d703182778493aa477437b4f29196967
linqinghui <linqinghui@yunzshop.com>
2019-05-17 11:05:55 +0800

    卖家收不到消息通知

--- app/frontend/modules/order/services/message/ShopMessage.php
+++ app/frontend/modules/order/services/message/ShopMessage.php
@@ -30,7 +30,7 @@ class ShopMessage extends Message
             return;
         }
         //客服发送消息通知
-        foreach (\Setting::get('shop.miniNotice.salers') as $saler) {
+        foreach (\Setting::get('shop.notice.salers') as $saler) {
                 $this->notice($this->templateId, $this->msg, $saler['uid']);
         }
     }
