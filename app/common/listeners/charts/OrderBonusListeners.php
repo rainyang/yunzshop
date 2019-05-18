c80eb4b133d9271add8311ac50807611993142a8
fbc <773928614@qq.com>
2019-05-18 17:19:05 +0800

    修复订单统计

--- app/common/listeners/charts/OrderBonusListeners.php
+++ app/common/listeners/charts/OrderBonusListeners.php
@@ -7,21 +7,25 @@
  */
 namespace app\common\listeners\charts;
 
+use app\backend\modules\charts\models\OrderIncomeCount;
 use app\common\events\order\AfterOrderCanceledEvent;
 use app\common\events\order\AfterOrderCreatedImmediatelyEvent;
+use app\common\events\order\AfterOrderPaidImmediatelyEvent;
 use app\common\events\order\AfterOrderReceivedEvent;
 use app\common\events\order\AfterOrderPaidEvent;
 use app\common\events\order\AfterOrderCreatedEvent;
+use app\common\events\order\AfterOrderReceivedImmediatelyEvent;
 use app\common\events\order\AfterOrderRefundedEvent;
 use app\common\models\order\OrderPluginBonus;
-use app\Jobs\OrderBonusContentJob;
 use app\Jobs\OrderBonusStatusJob;
-use app\Jobs\OrderBonusUpdateJob;
 use app\Jobs\OrderCountContentJob;
 use app\Jobs\OrderCountIncomeJob;
 use app\Jobs\OrderCountStatusJob;
 use app\Jobs\OrderMemberMonthJob;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Yunshop\StoreCashier\common\models\CashierOrder;
+use Yunshop\StoreCashier\common\models\StoreOrder;
+use Yunshop\Supplier\common\models\SupplierOrder;
 use app\common\models\Order;
 
 class OrderBonusListeners
@@ -33,13 +37,13 @@ class OrderBonusListeners
     {
         //下单
         $events->listen(AfterOrderCreatedImmediatelyEvent::class, OrderBonusListeners::class. '@addCount');
-//        $events->listen(AfterOrderCreatedEvent::class, OrderBonusListeners::class. '@addCount');
 
-        //todo 支付之后 统计订单详情
-        //$events->listen(AfterOrderPaidEvent::class, OrderBonusListeners::class . '@addBonus');
+        $events->listen(AfterOrderPaidImmediatelyEvent::class, OrderBonusListeners::class . '@orderPay');
 
         //收货之后 更改订单状态
         $events->listen(AfterOrderReceivedEvent::class, OrderBonusListeners::class . '@updateBonus');
+        //收货之后 更改订单状态
+        $events->listen(AfterOrderReceivedImmediatelyEvent::class, OrderBonusListeners::class . '@receivedBonus');
 
         //订单取消
         $events->listen(AfterOrderCanceledEvent::class, OrderBonusListeners::class. '@cancel');
@@ -49,36 +53,53 @@ class OrderBonusListeners
 
     }
 
-    //todo 订单支付后
-//    public function addBonus(AfterOrderPaidEvent $event)
-//    {
-//        $this->orderModel = Order::find($event->getOrderModel()->id);
-//        $this->dispatch(new OrderBonusContentJob($this->orderModel));
-//    }
+    public function orderPay(AfterOrderPaidImmediatelyEvent $event)
+    {
+        OrderIncomeCount::updateByOrderId($event->getOrderModel()->id, ['status' => $event->getOrderModel()->status]);
+    }
 
 
     public function addCount(AfterOrderCreatedImmediatelyEvent $event)
     {
-        $orderModel = Order::find($event->getOrderModel()->id);
+        $orderModel = $event->getOrderModel();
         $this->dispatch(new OrderCountContentJob($orderModel));
-//        $this->dispatch((new OrderCountContentJob($orderModel))->delay(10));
     }
 
     public function updateBonus(AfterOrderReceivedEvent $event)
     {
         $this->dispatch(new OrderMemberMonthJob($event->getOrderModel()));
         $this->dispatch(new OrderBonusStatusJob($event->getOrderModel()->id));
-        $this->dispatch((new OrderCountIncomeJob($event->getOrderModel()->id))->delay(10));
+//        $this->dispatch((new OrderCountIncomeJob($event->getOrderModel())));
+    }
+    
+    public function receivedBonus(AfterOrderReceivedImmediatelyEvent $event)
+    {
+        $orderModel = $event->getOrderModel();
+
+        $data = [];
+
+        if ($orderModel->is_plugin == 1 || $orderModel->plugin_id == 92) {
+            $data['supplier'] = SupplierOrder::where('order_id', $orderModel->id)->sum('supplier_profit');
+        }
+        if ($orderModel->plugin_id == 31) {
+            $data['cost_price'] = $data['cashier'] = CashierOrder::where('order_id', $orderModel->id)->sum('amount');
+        }
+        if ($orderModel->plugin_id == 32) {
+            $data['cost_price'] = $data['store'] = StoreOrder::where('order_id', $orderModel->id)->sum('amount');
+        }
+        $data['status'] = $orderModel->status;
+        OrderIncomeCount::updateByOrderId($event->getOrderModel()->id, $data);
     }
 
     public function cancel(AfterOrderCanceledEvent $event)
     {
-        $this->dispatch(new OrderCountStatusJob($event->getOrderModel()->id, -1));
+        OrderIncomeCount::updateByOrderId($event->getOrderModel()->id, ['status' => -1]);
     }
 
     public function refunded(AfterOrderRefundedEvent $event)
     {
-        $this->dispatch(new OrderCountStatusJob($event->getOrderModel()->id, -2));
+        OrderIncomeCount::updateByOrderId($event->getOrderModel()->id, ['status' => -2]);
     }
+
 
 }
