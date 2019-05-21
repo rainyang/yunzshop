626dbbdf1d1a3dab313ac52f68c8d8804467eab7
liuyifan <liuyifan@yunzshop.com>
2019-05-21 15:22:08 +0800

    no message

--- database/migrations/2019_05_20_151203_update_ims_yz_users_profile_mobile.php
+++ database/migrations/2019_05_20_151203_update_ims_yz_users_profile_mobile.php
@@ -16,7 +16,7 @@ class UpdateImsYzUsersProfileMobile extends Migration
         if (Schema::hasTable('yz_users_profile')) {
             if (Schema::hasColumn('yz_users_profile', 'mobile')) {
                 Schema::table('yz_users_profile', function (Blueprint $table) {
-                    $table->dropUnique('yz_users_profile_email_unique');
+                    $table->dropUnique('yz_users_profile_mobile_unique');
                 });
             }
         }
