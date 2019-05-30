89f44b2c37fc54abe8798969fc45266999a97d33
liuyifan <liuyifan@yunzshop.com>
2019-05-30 16:56:11 +0800

    no message

--- app/frontend/modules/member/controllers/MemberController.php
+++ app/frontend/modules/member/controllers/MemberController.php
@@ -1524,7 +1524,7 @@ class MemberController extends ApiController
                 'url'   => $url
             ];
         });
-        if (app('plugins')->isEnabled('asset') && (new \Yunshop\Asset\Common\Services\IncomeDigitizationService)->Memberlevel()) {
+        if (app('plugins')->isEnabled('asset') && (new \Yunshop\Asset\Common\Services\IncomeDigitizationService)->memberPermission()) {
             $data[] = [
                 'name'  => 'asset',
                 'title' => PLUGIN_ASSET_NAME,
