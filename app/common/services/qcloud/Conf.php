8ba5294f4d6991de8f82d64680eb969657d5be52
liuyifan <liuyifan@yunzshop.com>
2019-06-17 15:17:20 +0800

    no message

--- app/common/services/qcloud/Conf.php
+++ app/common/services/qcloud/Conf.php
@@ -6,21 +6,25 @@ use app\platform\modules\system\models\SystemSetting;
 
 class Conf {
 
-    // Please refer to http://console.qcloud.com/cos to fetch your app_id, secret_id and secret_key.
-    public static $APP_ID;
-    public static $SECRET_ID;
-    public static $SECRET_KEY;
 
     // Cos php sdk version number.
     const VERSION = 'v4.2.2';
     const API_COSAPI_END_POINT = 'http://region.file.myqcloud.com/files/v2/';
 
-    public function __construct()
+    /**
+     * Please refer to http://console.qcloud.com/cos to fetch your app_id, secret_id and secret_key.
+     *
+     * @return array
+     */
+    public static function config()
     {
         $remote = SystemSetting::settingLoad('remote', 'system_remote');
-        self::$APP_ID     = $remote['cos']['appid'];
-        self::$SECRET_ID  = $remote['cos']['secretid'];
-        self::$SECRET_KEY = $remote['cos']['secretkey'];
+
+        return [
+            'APP_ID'     => $remote['cos']['appid'],
+            'SECRET_ID'  => $remote['cos']['secretid'],
+            'SECRET_KEY' => $remote['cos']['secretkey']
+        ];
     }
 
     /**
