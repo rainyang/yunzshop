7b314aa67a99afed4a3e9e13dc3c8c9272323d0b
liuyifan <liuyifan@yunzshop.com>
2019-06-18 17:31:01 +0800

    修复安装报错

--- database/migrations/2019_06_16_102620_add_columns_to_ims_yz_goods_sales_table.php
+++ database/migrations/2019_06_16_102620_add_columns_to_ims_yz_goods_sales_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsToImsYzGoodsSaleTable extends Migration
+class AddColumnsToImsYzGoodsSalesTable extends Migration
 {
     /**
      * Run the migrations.
