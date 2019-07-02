fe0002b2cf4bdf273b98b6bd07562a88ec443a35
qinxinjin <553829274@qq.com>
2019-07-02 10:41:47 +0800

    字段修改

--- app/common/models/GoodsEditDisable.php
+++ app/common/models/GoodsEditDisable.php
@@ -10,11 +10,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * 例如，拼团活动使用了商品id:635,这时候需要禁止商品635编辑功能，否则可能出现下单出错(规格问题)
  *  于是拼团活动往该表插入一条数据，活动结束后将该数据删除
  * 当拼团活动在进行中而编辑该商品时，会查询该表是否有该商品记录，有则不允许编辑
- * 需要记录key字段，如拼团和抢购都设置了商品不可编辑，那么拼团不应该删除抢购写入的数据，所以要通过key区分
+ * 需要记录edit_key字段，如拼团和抢购都设置了商品不可编辑，那么拼团不应该删除抢购写入的数据，所以要通过edit_key区分
  * Class GoodsEditDisable
  * @property int goods_id 商品id
  * @property int message 信息
- * @property int key 关键字，通过该关键字，创建对应的记录和删除对应的记录
+ * @property int edit_key 关键字，通过该关键字，创建对应的记录和删除对应的记录
  * @package app\common\models
  */
 class GoodsEditDisable extends \app\common\models\BaseModel
@@ -34,7 +34,7 @@ class GoodsEditDisable extends \app\common\models\BaseModel
             'uniacid' => 'required|integer',
             'goods_id' => 'required|integer',
             'message' => 'required',
-            'key' => 'required'
+            'edit_key' => 'required'
         ];
     }
 
@@ -44,7 +44,7 @@ class GoodsEditDisable extends \app\common\models\BaseModel
             'uniacid' => '公众号ID',
             'goods_id' => '商品id',
             'message' => '提示信息',
-            'key' => '关键字'
+            'edit_key' => '关键字'
         ];
     }
 
