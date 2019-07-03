e8acbcbfaff5c232cf745ae6446c59421d97a5f6
崔松岩 <178125247@qq.com>
2019-07-03 15:50:57 +0800

    团队订单统计查询简化

---
+++ database/migrations/2019_07_03_153836_update_yz_member_month_order.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateYzMemberMonthOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        if (Schema::hasTable('yz_member_month_order')) {
+            if (Schema::hasColumn('yz_member_month_order', 'member_id')) {
+                Schema::table('yz_member_month_order', function (Blueprint $table) {
+                    $table->integer('member_id')->default(0)->index('idx_member_id')->change();
+                });
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
