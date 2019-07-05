5744048a37177455c206acaf5c4ac973fd347464
huapei <642739879@qq.com>
2019-07-05 10:51:04 +0800

    打印次数日志

--- app/frontend/modules/withdraw/services/StatisticalPresentationService.php
+++ app/frontend/modules/withdraw/services/StatisticalPresentationService.php
@@ -19,6 +19,7 @@ class StatisticalPresentationService
         $start = strtotime(date("Y-m-d"),time());
         $end = $start+60*60*24;
         $today_withdraw_count =  Withdraw::successfulWithdrawals($type,$start,$end);
+        \Log::debug($type.'收入提现次数',$today_withdraw_count);
         if(app('plugins')->isEnabled('supplier')){
         	\Log::debug($type.'供应商提现次数',\Yunshop\Supplier\supplier\models\SupplierWithdraw::successfulWithdrawals($type,$start,$end));
             $today_withdraw_count += \Yunshop\Supplier\supplier\models\SupplierWithdraw::successfulWithdrawals($type,$start,$end);
