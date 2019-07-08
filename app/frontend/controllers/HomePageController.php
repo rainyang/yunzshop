8a388a76281a93af91f8feb0c3c625d96986cb96
zengxiangping <zengxiangping@yunzhong.com>
2019-07-08 15:20:46 +0800

    fix

--- app/frontend/controllers/HomePageController.php
+++ app/frontend/controllers/HomePageController.php
@@ -52,6 +52,16 @@ class HomePageController extends ApiController
     ];
     private $pageSize = 16;
 
+    protected function jumpUrl($type, $mid)
+    {
+        if (empty($type) || $type == 'undefined') {
+            $type = Client::getType();
+        }
+
+        $queryString = ['type'=>$type,'i'=>\YunShop::app()->uniacid, 'mid'=>$mid];
+
+        return $queryString;
+    }
     /**
      * @return \Illuminate\Http\JsonResponse 当路由不包含page_id参数时,提供商城首页数据; 当路由包含page_id参数时,提供装修预览数据
      */
