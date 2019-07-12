10f6fa459388bb481b70d82aa142f2691f902d89
dingran <dingran80s@gmail.com>
2019-07-12 14:25:42 +0800

    小程序登录接口调整

--- app/common/components/ApiController.php
+++ app/common/components/ApiController.php
@@ -100,19 +100,24 @@ class ApiController extends BaseController
 
         $queryString = ['type'=>$type,'i'=>\YunShop::app()->uniacid, 'mid'=>$mid, 'scope' => $scope];
 
-        if ($this->controller == 'Login' && $this->action == 'checkLogin') {
-            if ($scope == 'home') {
-                if (!$mid && (!app('plugins')->isEnabled('designer')
-                    || (app('plugins')->isEnabled('designer')) && (new IndexPageService())->getIndexPage() == '')) {
-                    return;
-                }
-            }
 
-            if (self::MOBILE_TYPE == $type || self::WEB_APP == $type || self::NATIVE_APP == $type) {
-                throw new MemberNotLoginException('请登录', ['login_status' => 1, 'login_url' => '', 'type' => $type, 'i' => \YunShop::app()->uniacid, 'mid' => $mid, 'scope' => $scope]);
-            }
-
+        if ($type == 2) {
             throw new MemberNotLoginException('请登录', ['login_status' => 0, 'login_url' => Url::absoluteApi('member.login.index', $queryString)]);
+        } else {
+            if ($this->controller == 'Login' && $this->action == 'checkLogin') {
+                if ($scope == 'home') {
+                    if (!$mid && (!app('plugins')->isEnabled('designer')
+                            || (app('plugins')->isEnabled('designer')) && (new IndexPageService())->getIndexPage() == '')) {
+                        return;
+                    }
+                }
+
+                if (self::MOBILE_TYPE == $type || self::WEB_APP == $type || self::NATIVE_APP == $type) {
+                    throw new MemberNotLoginException('请登录', ['login_status' => 1, 'login_url' => '', 'type' => $type, 'i' => \YunShop::app()->uniacid, 'mid' => $mid, 'scope' => $scope]);
+                }
+
+                throw new MemberNotLoginException('请登录', ['login_status' => 0, 'login_url' => Url::absoluteApi('member.login.index', $queryString)]);
+            }
         }
     }
 }
