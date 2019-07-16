6fdeaff370788faabbe4014c8687408b1ab4e1cc
崔松岩 <178125247@qq.com>
2019-07-16 14:58:37 +0800

    会员中心装修

---
+++ app/frontend/modules/member/controllers/MemberDesignerController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: win 10
+ * Date: 2019/7/16
+ * Time: 14:53
+ */
+
+namespace app\frontend\modules\member\controllers;
+
+
+use app\common\components\ApiController;
+
+class MemberDesignerController extends ApiController
+{
+     public function index()
+     {
+
+     }
+}
