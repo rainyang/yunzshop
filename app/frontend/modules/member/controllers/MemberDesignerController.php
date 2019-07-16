a1ed80f1b8126adbf869e850249b1870328af8ea
崔松岩 <178125247@qq.com>
2019-07-16 15:06:20 +0800

    会员中心装修

--- app/frontend/modules/member/controllers/MemberDesignerController.php
+++ app/frontend/modules/member/controllers/MemberDesignerController.php
@@ -10,11 +10,24 @@ namespace app\frontend\modules\member\controllers;
 
 
 use app\common\components\ApiController;
+use Yunshop\Designer\models\MemberDesigner;
 
 class MemberDesignerController extends ApiController
 {
      public function index()
      {
+         $res = [];
+         $res['status'] = false;
+         $res['data'] = [];
+         if (app('plugins')->isEnabled('hotel')) {
+            $designer =  MemberDesigner::uniacid()->where('id',1)->first();
+            if($designer->datas)
+            {
+                $res['data'] = json_decode($designer->datas);
+                $res['status'] = true;
+            }
+         }
 
+         return $this->successJson('成功', $res);
      }
 }
