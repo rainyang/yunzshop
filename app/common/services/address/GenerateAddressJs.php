dd2b2746f000223d9a0d7c18ad04b7ad22aa9d5a
blank <baidu.com>
2019-07-17 10:16:33 +0800

    no message

---
+++ app/common/services/address/GenerateAddressJs.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/7/17
+ * Time: 10:10
+ */
+
+namespace app\common\services\address;
+
+use Illuminate\Support\Facades\DB;
+use app\common\models\Address;
+
+class GenerateAddressJs
+{
+    public function address()
+    {
+        if (env('APP_Framework') == 'platform') {
+            $file_dir = '';
+        } else {
+            $file_dir = '../addons/yun_shop';
+        }
+
+        $b = $this->abcd();
+        $b = json_encode($b, JSON_UNESCAPED_UNICODE);
+        $str = 'var district ='.$b.';if (typeof define === "function") {define(district)} else {window.YDUI_DISTRICT = district}';
+        $ccc =  file_put_contents($file_dir.'/gov_province_city_area_id.js', $str);
+
+        return $ccc;
+
+    }
+
+    public function abcd($parentId = 0, $level = 1)
+    {
+        $a = Address::where('parentid', $parentId)->where('level', $level)->get()->toArray();
+        if (empty($a)) return [];
+
+        foreach ($a as $key => $value) {
+            $address[$key]['v'] = $value['id'];
+            $address[$key]['n'] = $value['areaname'];
+            $address[$key]['c'] = $this->abcd($value['id'], ($value['level'] + 1));
+        }
+
+        return $address;
+    }
+}
