168d3dd19985afb134311bb8a6d89441fefa7c11
huapei <642739879@qq.com>
2019-07-18 12:52:15 +0800

    debug

--- app/common/models/Withdraw.php
+++ app/common/models/Withdraw.php
@@ -529,6 +529,7 @@ class Withdraw extends BaseModel
     public static function SuccessfulCashWithdrawal($pay_type,$start,$end){
 
         return self::where([
+                ['member_id',\YunShop::app()->getMemberId()],
                 ['status','=',self::STATUS_PAY],
                 ['pay_way','=',$pay_type],
                 ['pay_at','>=',$start],
