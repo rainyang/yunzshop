92d3ac34b9c686d2d44f02c73b724aacacd91b5a
崔松岩 <178125247@qq.com>
2019-07-18 09:58:19 +0800

    后台样式

--- app/frontend/modules/member/controllers/MemberDesignerController.php
+++ app/frontend/modules/member/controllers/MemberDesignerController.php
@@ -20,14 +20,83 @@ class MemberDesignerController extends ApiController
          $res['status'] = false;
          $res['data'] = [];
          if (app('plugins')->isEnabled('designer')) {
-            $designer =  MemberDesigner::uniacid()->where('id',19)->first();
+            $designer =  MemberDesigner::uniacid()->where('id',3)->first();
             if($designer->datas)
             {
-                $res['data'] = json_decode($designer->datas);
+                $datas = json_decode($designer->datas);
+                $memberData = $this->getMemberData();
+                foreach ($datas as $dkey=>$design)
+                {
+                    if($design['temp'] == 'membertool')
+                    {
+                         foreach ($design['data']['part'] as $pkey=>$par)
+                         {
+                             if(!in_array($par['name'],$memberData['tools'])){
+                                 unset($datas[$dkey]['data']['part'][$pkey]);
+                             }
+                         }
+                    }
+                    if($design['temp'] == 'membermerchant')
+                    {
+                        foreach ($design['data']['part'] as $pkey=>$par)
+                        {
+                            if(!in_array($par['name'],$memberData['merchants'])){
+                                unset($datas[$dkey]['data']['part'][$pkey]);
+                            }
+                        }
+                    }
+                    if($design['temp'] == 'membermarket')
+                    {
+                        foreach ($design['data']['part'] as $pkey=>$par)
+                        {
+                            if(!in_array($par['name'],$memberData['markets'])){
+                                unset($datas[$dkey]['data']['part'][$pkey]);
+                            }
+                        }
+                    }
+                    if($design['temp'] == 'memberasset')
+                    {
+                        foreach ($design['data']['part'] as $pkey=>$par)
+                        {
+                            if(!in_array($par['name'],$memberData['assets'])){
+                                unset($datas[$dkey]['data']['part'][$pkey]);
+                            }
+                        }
+                    }
+                }
+                $res['data'] = $datas;
                 $res['status'] = true;
             }
          }
 
          return $this->successJson('成功', $res);
+     }
+
+     private function getMemberData()
+     {
+         $arr = (new \app\common\services\member\MemberDesigner())->getMemberData();
+         $tools = [];
+         $merchants = [];
+         $markets = [];
+         $assets = [];
+         foreach ($arr['tool'] as $v){
+            $tools[] = $v['name'];
+         }
+         foreach ($arr['merchant'] as $v){
+             $merchants[] = $v['name'];
+         }
+         foreach ($arr['market'] as $v){
+             $markets[] = $v['name'];
+         }
+         foreach ($arr['asset_equity'] as $v){
+             $assets[] = $v['name'];
+         }
+
+         return [
+             'tools'=>$tools,
+             'merchants'=>$merchants,
+             'markets'=>$markets,
+             'assets'=>$assets,
+         ];
      }
 }
