42523bdb873b9b6b714f7f8eca9e9337d363e8b2
qinxinjin <553829274@qq.com>
2019-07-24 19:50:54 +0800

    easywechat第三库的curl超时配置，改为10秒

--- app/common/modules/wechat/Config.php
+++ app/common/modules/wechat/Config.php
@@ -97,6 +97,10 @@ class Config
                 'permission' => 0777,
                 'file'       => '/tmp/easywechat.log',
             ],
+            'guzzle' => [
+                'timeout' => 10.0, // 超时时间（秒）
+                //'verify' => false, // 关掉 SSL 认证（强烈不建议！！！）
+            ],
         ];
     }
 }
