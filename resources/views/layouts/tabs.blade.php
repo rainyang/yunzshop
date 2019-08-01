0468abc1e37ffdef51fb77c6a8565a0d0776820d
李宝佳 <livsyitian@163.com>
2019-08-01 15:06:45 +0800

    代码格式化

--- resources/views/layouts/tabs.blade.php
+++ resources/views/layouts/tabs.blade.php
@@ -1,19 +1,13 @@
 <div class="panel panel-info">
     <ul class="add-shopnav">
-
         @foreach(config('menu')[\Yunshop::$currentItems[0]]['child'][\Yunshop::$currentItems[1]]['child'] as $key=>$value)
-
             @if(isset($value['menu']) && $value['menu'] == 1 && $value['can'])
                 @if(isset($value['child']) && array_child_kv_exists($value['child'],'menu',1))
-
-
                     <li @if(\YunShop::request()->route == $value['url'] || \YunShop::request()->route . '.index' == $value['url']) class="active" @endif>
                         <a href="{{isset($value['url']) ? yzWebFullUrl($value['url']):''}}{{$value['url_params'] or ''}}">
                             {{$value['name']}}
                         </a>
                     </li>
-
-
                 @elseif($value['menu'] == 1)
                     <li @if(\YunShop::request()->route == $value['url'] || \YunShop::request()->route . '.index' == $value['url']) class="active" @endif>
                         <a href="{{isset($value['url']) ? yzWebFullUrl($value['url']):''}}{{$value['url_params'] or ''}}">
@@ -23,6 +17,5 @@
                 @endif
             @endif
         @endforeach
-
     </ul>
 </div>
