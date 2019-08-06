98f0b197dd075d5f599b5bf26279d48978980815
fbc <773928614@qq.com>
2019-08-06 14:44:33 +0800

    优化统计

---
+++ database/migrations/2019_08_05_161833_drop_index_in_order_income_count.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropIndexInOrderIncomeCount extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('yz_order_income_count')) {
+            Schema::table('yz_order_income_count', function (Blueprint $table) {
+                $sm = Schema::getConnection()->getDoctrineSchemaManager();
+                $preFix = Schema::getConnection()->getTablePrefix();
+                $indexesFound = $sm->listTableIndexes($preFix . 'yz_order_income_count');
+
+                if (array_key_exists("idx_order", $indexesFound)) {
+                    $table->dropIndex('idx_order');
+                }
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+
+    public function hasIndex($table, $name)
+    {
+        $conn = Schema::getConnection();
+        $dbSchemaManager = $conn->getDoctrineSchemaManager();
+        $doctrineTable = $dbSchemaManager->listTableDetails($table);
+        return $doctrineTable->hasIndex($name);
+    }
+
+
+}
