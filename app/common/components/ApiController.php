e1386d8cf30416379b759f457aad75c52fa3f567
dingran <dingran80s@gmail.com>
2019-08-14 18:00:21 +0800

    重新规划

--- app/common/components/ApiController.php
+++ app/common/components/ApiController.php
@@ -66,9 +66,7 @@ class ApiController extends BaseController
         $member = MemberFactory::create($type);
 
         if (!$member->checkLogged($this)) {
-            if ($mid && $this->controller == 'HomePage'
-                && ($this->action == 'index' || $this->action == 'getParams')
-            ) {
+            if ($mid) {
                 return $this->jumpUrl($type, $mid);
             }
 
@@ -108,24 +106,10 @@ class ApiController extends BaseController
 
         $queryString = ['type'=>$type,'i'=>\YunShop::app()->uniacid, 'mid'=>$mid, 'scope' => $scope];
 
-
-        if ($type == 2) {
-            throw new MemberNotLoginException('请登录', ['login_status' => 0, 'login_url' => Url::absoluteApi('member.login.index', $queryString)]);
-        } else {
-            if (($this->controller == 'Login' && $this->action == 'checkLogin') || $this->jump) {
-                if ($scope == 'home') {
-                    if (!$mid && (!app('plugins')->isEnabled('designer')
-                            || (app('plugins')->isEnabled('designer')) && (new IndexPageService())->getIndexPage() == '')) {
-                        return;
-                    }
-                }
-
-                if (self::MOBILE_TYPE == $type || self::WEB_APP == $type || self::NATIVE_APP == $type) {
-                    throw new MemberNotLoginException('请登录', ['login_status' => 1, 'login_url' => '', 'type' => $type, 'i' => \YunShop::app()->uniacid, 'mid' => $mid, 'scope' => $scope]);
-                }
-
-                throw new MemberNotLoginException('请登录', ['login_status' => 0, 'login_url' => Url::absoluteApi('member.login.index', $queryString)]);
-            }
+        if (self::MOBILE_TYPE == $type || self::WEB_APP == $type || self::NATIVE_APP == $type) {
+            throw new MemberNotLoginException('请登录', ['login_status' => 1, 'login_url' => '', 'type' => $type, 'i' => \YunShop::app()->uniacid, 'mid' => $mid, 'scope' => $scope]);
         }
+
+        throw new MemberNotLoginException('请登录', ['login_status' => 0, 'login_url' => Url::absoluteApi('member.login.index', $queryString)]);
     }
 }
